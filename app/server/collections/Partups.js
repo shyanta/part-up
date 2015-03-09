d29b9697cc215faf3bbb14b783fa74051d96ab24
Peter Peerdeman <peter@lifely.nl>
2015-03-09 23:54:23 +0100

    layouts, routing and scaffolding

---
+++ app/server/collections/Partups.js
@@ -0,0 +1,14 @@
+/*
+Partups.allow({
+    insert: function() { return true; },
+    update: function() { return true; },
+    remove: function() { return true; },
+});
+
+Partups.deny({
+
+});
+Meteor.publish('partups-all', function() {
+    return Partups.findAll();
+});
+*/
---
+++ bin/migration-1.js
@@ -0,0 +1,1 @@
+// we can house migrations and other scripts in this folder
