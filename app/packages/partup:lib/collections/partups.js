18b69e83bbc7027883732ff8c6c29ce5057388df
Bryan te Beek <bryantebeek@gmail.com>
2015-03-14 10:45:48 +0100

    Authorisation should be done in the Meteor.calls

--- app/packages/partup:lib/collections/partups.js
+++ app/packages/partup:lib/collections/partups.js
@@ -1,18 +1,1 @@
 Partups = new Meteor.Collection('partups');
-
-Partups.allow({
-    insert: function(userId, doc) { 
-        return userId; 
-    },
-    update: function(userId, doc, fieldNames, modifier) { 
-        return userId === doc.userId; 
-    },
-    remove: function(userId, doc) { 
-        return userId === doc.userId; 
-    }
-});
-
-Partups.deny({
-
-});
-
--- app/packages/partup:server-base/methods/collections/partups.js
+++ app/packages/partup:server-base/methods/collections/partups.js
@@ -1,6 +1,8 @@
 Meteor.methods({
 
     'collections.partups.insert': function (data) {
+	// Authorized?
+
         Event.emit('collections.partups.insert', data);
     }
 
