bd3c61572bbbb1bdde29446b496daf8c606cf10b
Bryan te Beek <bryantebeek@gmail.com>
2015-03-14 10:45:58 +0100

    Add code to remove a partup

--- app/packages/partup:server-base/methods/collections/partups.js
+++ app/packages/partup:server-base/methods/collections/partups.js
@@ -1,9 +1,15 @@
 Meteor.methods({
 
     'collections.partups.insert': function (data) {
-	// Authorized?
+        // Authorized?
 
         Event.emit('collections.partups.insert', data);
+    },
+
+    'collections.partups.remove': function (partupId) {
+        // Authorized?
+
+        Event.emit('collections.partups.remove', partupId);
     }
 
 });
--- app/packages/partup:server-base/test.js
+++ app/packages/partup:server-base/test.js
@@ -1,1 +1,5 @@
 Meteor.call('collections.partups.insert', { name: 'New Partup!' });
+
+var partup = Partups.findOne();
+
+Meteor.call('collections.partups.remove', partup._id);
