63bea6877c68d9ad50dfe26082c7af342ec264b2
Bryan te Beek <bryantebeek@gmail.com>
2015-03-14 12:20:13 +0100

    Add some jsdocs

--- app/packages/partup:server-base/event.js
+++ app/packages/partup:server-base/event.js
@@ -1,43 +1,57 @@
 var equal = Npm.require('equals');
 var events = Npm.require('eventemitter2');
 
-
-//
-// Global
-//
-
+/**
+ * The global event emitter instance.
+ *
+ * @type {EventEmitter2}
+ */
 Event = new events.EventEmitter2({
     wildcard: true,
     delimiter: '.'
 });
 
-
-//
-// Collection Event Helpers
-//
-
+/**
+ * Helper to handle the event emitting for an insertion of a collection document.
+ *
+ * @param collection (Mongo.Collection)
+ * @param fields
+ */
 Event.emitCollectionInsert = function (collection, fields) {
     Event.emit('collections.' + collection._name + '.insert', fields);
 };
 
-Event.emitCollectionUpdate = function (collection, entity, fields) {
-    if (equal(entity, fields)) return;
+/**
+ * Helper to handle the event emitting for an update of a collection document.
+ *
+ * @param collection (Mongo.Collection)
+ * @param document
+ * @param fields
+ */
+Event.emitCollectionUpdate = function (collection, document, fields) {
+    if (equal(document, fields)) return;
 
-    Event.emit('collections.' + collection._name + '.update', entity._id, fields);
+    Event.emit('collections.' + collection._name + '.update', document._id, fields);
 
     Object.keys(fields).forEach(function (key) {
         var value = {
             'name': key,
-            'old': entity[key],
+            'old': document[key],
             'new': fields[key]
         };
 
         if (equal(value.old, value.new)) return;
 
-        Event.emit('collections.' + collection._name + '.' + key + '.update', entity._id, value);
+        Event.emit('collections.' + collection._name + '.' + key + '.update', document._id, value);
     });
 };
 
-Event.emitCollectionRemove = function (collection, entity) {
-    Event.emit('collections.' + collection._name + '.remove', entity._id);
+/**
+ * Helper to handle the event emitting for a removal in a collection document.
+ *
+ * @param collection
+ * @param document
+ */
+Event.emitCollectionRemove = function (collection, document) {
+    Event.emit('collections.' + collection._name + '.remove', document._id);
 };
