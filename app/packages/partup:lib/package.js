9be9e14286c5918307c7eba7f047cb4709a28142
Bryan te Beek <bryantebeek@gmail.com>
2015-03-14 10:18:40 +0100

    Move collections to partup:lib package

---
+++ app/packages/partup:lib/package.js
@@ -0,0 +1,17 @@
+Package.describe({
+    name: 'partup:lib',
+    version: '0.0.1',
+    summary: '',
+    git: '',
+    documentation: null
+});
+
+Package.onUse(function(api) {
+    api.use(['mongo']);
+
+    api.addFiles([
+        'collections/partups.js'
+    ]);
+
+    api.export('Partups');
+});
