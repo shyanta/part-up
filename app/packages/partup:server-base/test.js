177e1a6055f68c1676643442cc2e57c8551b9af4
Jesse de Vries <jessedvrs@gmail.com>
2015-03-18 15:34:48 +0100

    Commented test.js (because mongo database is updated on every server restart)

--- app/packages/partup:server-base/test.js
+++ app/packages/partup:server-base/test.js
@@ -1,6 +1,8 @@
-Meteor.call('collections.partups.insert', { name: 'New Partup!' });
+// Meteor.call('collections.partups.insert', { name: 'New Partup!' });
 
 var partup = Partups.findOne();
 
-Meteor.call('collections.partups.update', partup._id, { name: 'Changed Partup!' });
-Meteor.call('collections.partups.remove', partup._id);
+if(partup) {
+    // Meteor.call('collections.partups.update', partup._id, { name: 'Changed Partup!' });
+    // Meteor.call('collections.partups.remove', partup._id);
+}
