f80ae24835831ec90d9f4966fafcb64df5039555
Laurens Bruijn <laurens@lifely.nl>
2015-03-18 13:46:38 +0100

    active Page check

--- app/client/templates/pages/start-partup/StartPartup.js
+++ app/client/templates/pages/start-partup/StartPartup.js
@@ -2,30 +2,17 @@
 /* Page helpers */
 /*************************************************************/
 Template.PagesStartPartup.helpers({
-    currentPage: {
-        start: function() {
-            var router = Router.current().route.path(this);
-            if (router == '/startpartup/details') {
+    currentPage: function(page) {
+        var router = Router.current().route.path(this);
+        if (page) {
+            var page = page.hash.samePage;
+            if (page == router) {
                 return true;
+            } else {
+                return false;
             }
-        },
-        activities: function() {
-            var router = Router.current().route.path(this);
-            if (router == '/startpartup/activities') {
-                return true;
-            }
-        },
-        contribute: function() {
-            var router = Router.current().route.path(this);
-            if (router == '/startpartup/contribute') {
-                return true;
-            }
-        },
-        promote: function() {
-            var router = Router.current().route.path(this);
-            if (router == '/startpartup/promote') {
-                return true;
-            }
+        } else {
+            return router;
         }
     }
 });
