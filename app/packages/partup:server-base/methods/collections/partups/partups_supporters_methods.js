d7ca155bfef0de16a3af1057540feea945c350a6
Bryan te Beek <bryantebeek@gmail.com>
2015-03-20 09:56:55 +0100

    Check if a user is already a supporter before inserting

--- app/packages/partup:server-base/methods/collections/partups/partups_supporters_methods.js
+++ app/packages/partup:server-base/methods/collections/partups/partups_supporters_methods.js
@@ -6,16 +6,19 @@ Meteor.methods({
      * @param {integer} partupId
      */
     'collections.partups.supporters.insert': function (partupId) {
-        // TODO: Validation
-
         var upper = Meteor.user();
         if (! upper) throw new Meteor.Error(401, 'Unauthorized.');
 
         var partup = Partups.findOneOrFail(partupId);
 
         try {
-            Partups.update(partupId, { $addToSet: { 'supporters': upper._id } });
-            Event.emit('collections.partups.supporters.inserted', partup, upper);
+	    var supporters = partup.supporters || [];
+	    var isAlreadySupporter = !! (supporters.indexOf(upper._id) > -1);
+
+	    if (! isAlreadySupporter) {
+		Partups.update(partupId, { $push: { 'supporters': upper._id } });
+		Event.emit('collections.partups.supporters.inserted', partup, upper);
+	    }
         } catch (error) {
             Log.error(error);
             throw new Meteor.Error(400, 'Upper [' + upper._id + '] could not be added as a supporter to Partup [' + partupId + '].');
@@ -28,16 +31,19 @@ Meteor.methods({
      * @param {integer} partupId
      */
     'collections.partups.supporters.remove': function (partupId) {
-        // TODO: Validation
-
         var upper = Meteor.user();
         if (! upper) throw new Meteor.Error(401, 'Unauthorized.');
 
         var partup = Partups.findOneOrFail(partupId);
 
         try {
-            Partups.update(partupId, { $pull: { 'supporters': upper._id } });
-            Event.emit('collections.partups.supporters.removed', partup, upper);
+	    var supporters = partup.supporters || [];
+	    var isAlreadySupporter = !! (supporters.indexOf(upper._id) > -1);
+
+	    if (! isAlreadySupporter) {
+		Partups.update(partupId, { $pull: { 'supporters': upper._id } });
+		Event.emit('collections.partups.supporters.removed', partup, upper);
+	    }
         } catch (error) {
             Log.error(error);
             throw new Meteor.Error(400, 'Upper [' + upper._id + '] could not be remove as a supporter from Partup [' + partupId + '].');
