b7e52f7cf906413cdd8ed245ded08e983fe4e2e8
Nick Koster <nick.koster@lifely.nl>
2015-03-20 11:31:18 +0100

    Cleanup

--- app/packages/partup:client-widgets-startdetails/package.js
+++ app/packages/partup:client-widgets-startdetails/package.js
@@ -5,7 +5,7 @@ Package.describe({
     documentation: null
 });
 
-Package.onUse(function(api) {
+Package.onUse(function (api) {
 
     api.use([
         'templating',
@@ -17,7 +17,7 @@ Package.onUse(function(api) {
     api.addFiles([
         'package-tap.i18n',
         'startdetails-placeholders.js',
-    	'startdetails.html',
+        'startdetails.html',
         'startdetails.js',
         'i18n/en.i18n.json',
         'i18n/nl.i18n.json'
