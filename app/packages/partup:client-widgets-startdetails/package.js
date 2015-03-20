755fbf1ecfeacbc4c6bfc08b10cf0b167cee85c7
Léon Smit <leonsmit@Leons-MacBook-Pro.local>
2015-03-20 16:16:47 +0100

    fixed startdetails translation issue

--- app/packages/partup:client-widgets-startdetails/package.js
+++ app/packages/partup:client-widgets-startdetails/package.js
@@ -5,21 +5,31 @@ Package.describe({
     documentation: null
 });
 
-Package.onUse(function (api) {
+Package.onUse(function(api) {
+    api.use([
+        'tap:i18n'
+    ], ['client', 'server']);
 
     api.use([
         'templating',
         'aldeed:autoform',
-        'partup:lib',
-        'tap:i18n'
+        'partup:lib'
     ], 'client');
 
     api.addFiles([
         'package-tap.i18n',
+
         'WidgetStartDetailsPlaceholders.js',
         'WidgetStartDetails.html',
         'WidgetStartDetails.js',
+
         'i18n/en.i18n.json',
         'i18n/nl.i18n.json'
     ], 'client');
+
+    api.addFiles([
+        'package-tap.i18n',
+        'i18n/en.i18n.json',
+        'i18n/nl.i18n.json'
+    ], 'server');
 });
