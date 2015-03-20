da65716c172d416c6fa18c03d91ac4fed4a76273
Léon Smit <leonsmit@Leons-MacBook-Pro.local>
2015-03-20 16:17:12 +0100

    translation buttons in home

--- app/client/templates/pages/home/Home.js
+++ app/client/templates/pages/home/Home.js
@@ -14,5 +14,14 @@ Template.PagesHome.helpers({
 /* Page events */
 /*************************************************************/
 Template.PagesHome.events({
-    //
+    'click [data-translate]': function EventClickTranslate (event, template) {
+        var language = $(event.currentTarget).data("translate");
+        TAPi18n.setLanguage(language).done(function () {
+            // it worked
+            console.log('Work work')
+        }).fail(function (error_message) {
+            // Handle the situation
+            console.log(error_message);
+        });
+    }
 });
--- app/client/templates/partials/app-header/AppHeader.js
+++ app/client/templates/partials/app-header/AppHeader.js
@@ -15,16 +15,6 @@ Template.PartialsAppHeader.helpers({
 /*************************************************************/
 Template.PartialsAppHeader.events({
 
-    'click [data-translate]': function EventClickTranslate (event, template) {
-        var language = $(event.currentTarget).data("translate");
-        TAPi18n.setLanguage(language).done(function () {
-            // it worked
-        }).fail(function (error_message) {
-            // Handle the situation
-            console.log(error_message);
-        });
-    },
-
     'click [data-login]': function EventClickLogin (event, template) {
         Meteor.loginWithLinkedin();
     }
