b260fd078eef0586fe96363f7de06477e413fbfb
Jesse de Vries <jessedvrs@gmail.com>
2015-03-20 16:37:05 +0100

    Added part supporters call

--- app/client/templates/pages/partup-detail/PartupDetail.js
+++ app/client/templates/pages/partup-detail/PartupDetail.js
@@ -1,11 +1,23 @@
 /*************************************************************/
+/* Local functions */
+/*************************************************************/
+var getPartup = function getPartup () {
+    var partupId = Router.current().params._id;
+    return Partups.findOne({ _id: partupId });
+};
+
+/*************************************************************/
 /* Page helpers */
 /*************************************************************/
 Template.PagesPartupDetail.helpers({
 
     partup: function () {
-        var partupId = Router.current().params._id;
-        return Partups.findOne({ _id: partupId });
+        return getPartup;
+    },
+
+    partupSupporters: function () {
+        var partup = getPartup();
+        return partup ? Meteor.users.find({ _id: { $in: partup.supporters} }) : [];
     }
 
 });
