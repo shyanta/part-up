b0ac9c50f2975481d22a6eb7a775469c90f633e7
Jesse de Vries <jessedvrs@gmail.com>
2015-03-23 16:54:07 +0100

    Added timeline and update components

--- app/client/templates/pages/partup-detail/updates/Updates.js
+++ app/client/templates/pages/partup-detail/updates/Updates.js
@@ -2,7 +2,46 @@
 /* Page helpers */
 /*************************************************************/
 Template.PagesPartupDetailUpdates.helpers({
-    //
+
+    'updates': function helperUpdates () {
+
+        // Stub data:
+        return [
+            {
+                id: 'j1hb233hb2j3h4',
+                user: {
+                    fullname: 'Jesse de Vries',
+                    profile_url: '',
+                    avatar: 'https://fbcdn-profile-a.akamaihd.net/hprofile-ak-xfa1/v/t1.0-1/p320x320/10372513_10152630117689315_2823570313206588958_n.jpg?oh=e76400243d7ed2678ba0d74edd6640b1&oe=55B4CF02&__gda__=1437886258_2a6463042ac4dcef71cdbee34d6c55c7'
+                },
+                title: 'Anti-contract getekend',
+                data: {
+                    //
+                },
+                time: '2015-03-23T14:39:41.711Z',
+                comments: [
+                    //
+                ]
+            },
+            {
+                id: '987asd987asda2',
+                user: {
+                    fullname: 'Jesse de Vries',
+                    profile_url: '',
+                    avatar: 'https://fbcdn-profile-a.akamaihd.net/hprofile-ak-xfa1/v/t1.0-1/p320x320/10372513_10152630117689315_2823570313206588958_n.jpg?oh=e76400243d7ed2678ba0d74edd6640b1&oe=55B4CF02&__gda__=1437886258_2a6463042ac4dcef71cdbee34d6c55c7'
+                },
+                title: 'Anti-contract getekend',
+                data: {
+                    //
+                },
+                time: '2015-03-23T14:39:41.711Z',
+                comments: [
+                    //
+                ]
+            }
+        ];
+    }
+
 });
 
 
