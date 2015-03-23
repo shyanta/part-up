aedab6e54371073bfe32e59daa2592e0c00ffde7
Jesse de Vries <jessedvrs@gmail.com>
2015-03-23 10:25:39 +0100

    js cleanup

--- app/client/templates/pages/home/Home.js
+++ app/client/templates/pages/home/Home.js
@@ -14,14 +14,14 @@ Template.PagesHome.helpers({
 /* Page events */
 /*************************************************************/
 Template.PagesHome.events({
-    'click [data-translate]': function EventClickTranslate (event, template) {
+
+    'click [data-translate]': function clickTranslate (event, template) {
         var language = $(event.currentTarget).data("translate");
         TAPi18n.setLanguage(language).done(function () {
-            // it worked
-            console.log('Work work')
+            // success
         }).fail(function (error_message) {
-            // Handle the situation
-            console.log(error_message);
+            // failed to switch language
         });
     }
+
 });
