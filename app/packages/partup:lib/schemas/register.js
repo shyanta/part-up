83c7bac7101be5138a73f2dd278766ab242d8c4a
Peter Peerdeman <peter@lifely.nl>
2015-03-24 12:02:21 +0100

    added register optional schema

--- app/packages/partup:lib/schemas/register.js
+++ app/packages/partup:lib/schemas/register.js
@@ -28,5 +28,44 @@ Partup.schemas.forms.registerRequired = new SimpleSchema({
  * @memberOf partup.schemas.forms
  */
 Partup.schemas.forms.registerOptional = new SimpleSchema({
-
+    location_input: {
+        type:String,
+        max: 255
+    },
+    description: {
+        type:String,
+        max: 255
+    },
+    tags_input: {
+        type:String,
+        max: 255
+    },
+    facebook: {
+        type:String,
+        max: 255
+    },
+    twitter: {
+        type:String,
+        max: 255
+    },
+    instagram: {
+        type:String,
+        max: 255
+    },
+    linkedin: {
+        type:String,
+        max: 255
+    },
+    website: {
+        type:String,
+        max: 255
+    },
+    phonenumber: {
+        type:String,
+        max: 255
+    },
+    skype: {
+        type:String,
+        max: 255
+    }
 });
