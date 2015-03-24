4b2bd798f908c5b60cc1e54500a9e4c5d18c9e21
Jesse de Vries <jessedvrs@gmail.com>
2015-03-24 15:15:19 +0100

    Added client-base package

---
+++ app/packages/partup:client-base/package.js
@@ -0,0 +1,19 @@
+Package.describe({
+    name: 'partup:client-base',
+    version: '0.0.1',
+    summary: '',
+    git: '',
+    documentation: null
+});
+
+Package.onUse(function(api) {
+    api.use('check');
+
+    api.use([
+        'tap:i18n',
+        'momentjs:moment'
+    ], ['client']);
+
+    api.addFiles([
+    ], ['client']);
+});
