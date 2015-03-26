13f22e1ccd2e3a6662420e6108863cc371de8cbe
Jesse de Vries <jessedvrs@gmail.com>
2015-03-26 15:21:54 +0100

    Improved update anotherDay function, added update-item widget

---
+++ app/packages/partup:client-widgets-partupdetail-update-item/WidgetPartupdetailUpdateItem.js
@@ -0,0 +1,18 @@
+/*************************************************************/
+/* Widget helpers */
+/*************************************************************/
+Template.WidgetPartupdetailUpdateItem.helpers({
+
+    'titleKey': function titleKey () {
+        return 'partupdetail-update-item-type-' + this.update.type + '-title';
+    }
+
+});
+
+
+/*************************************************************/
+/* Widget events */
+/*************************************************************/
+Template.WidgetPartupdetailUpdateItem.events({
+    //
+});
--- app/packages/partup:client-widgets-partupdetail-updates/WidgetPartupdetailUpdates.js
+++ app/packages/partup:client-widgets-partupdetail-updates/WidgetPartupdetailUpdates.js
@@ -1,43 +1,110 @@
 /*************************************************************/
+/* Widget functions */
+/*************************************************************/
+var getUpdates = function getUpdates () {
+    // Stub data
+    return [
+        {
+            user: {
+                avatar: 'https://media.licdn.com/mpr/mpr/shrinknp_200_200/p/3/005/0b2/1b0/1d80474.jpg',
+                fullname: 'Jesse de Vries',
+            },
+            created_at: new Date(),
+            type: 'anticontract_signed',
+            data: {
+                //
+            },
+            comments: [
+                {
+                    user: {
+                        fullname: 'Leon Smit'
+                    },
+                    content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet obcaecati cumque aliquam, corporis dolorum ab, quam totam recusandae culpa hic!'
+                },
+                {
+                    user: {
+                        fullname: 'Jesse de Vries'
+                    },
+                    content: 'Lorem ipsum dolor sit amet'
+                }
+            ]
+        },
+        {
+            user: {
+                avatar: 'https://media.licdn.com/mpr/mpr/shrink_200_200/p/7/005/0a7/057/33f4a9e.jpg',
+                fullname: 'Leon Smit',
+            },
+            created_at: new Date(1426949113548),
+            type: 'anticontract_signed',
+            data: {
+                //
+            },
+            comments: [
+                {
+                    user: {
+                        fullname: 'Jesse de Vries'
+                    },
+                    content: 'Lorem ipsum dolor sit amet'
+                }
+            ]
+        },
+        {
+            user: {
+                avatar: 'https://media.licdn.com/mpr/mpr/shrink_200_200/p/7/005/0a7/057/33f4a9e.jpg',
+                fullname: 'Leon Smit',
+            },
+            created_at: new Date(1426949113549),
+            type: 'anticontract_signed',
+            data: {
+                //
+            },
+            comments: []
+        },
+        {
+            user: {
+                avatar: 'https://media.licdn.com/mpr/mpr/shrink_200_200/p/7/005/0a7/057/33f4a9e.jpg',
+                fullname: 'Leon Smit',
+            },
+            created_at: new Date(1426949113550),
+            type: 'anticontract_signed',
+            data: {
+                //
+            },
+            comments: [
+                {
+                    user: {
+                        fullname: 'Jesse de Vries'
+                    },
+                    content: 'Lorem ipsum dolor sit amet'
+                }
+            ]
+        }
+    ];
+
+    // Real data
+    var partupId = Router.current().params._id;
+
+    return Updates.find({ partup_id: partupId }).map(function (update, idx) {
+        update.arrayIndex = idx;
+        return update;
+    });
+};
+
+/*************************************************************/
 /* Widget helpers */
 /*************************************************************/
 Template.WidgetPartupdetailUpdates.helpers({
 
     'updates': function helperUpdates () {
-        // Stub data
-        return [
-            {
-                arrayIndex: 0,
-                user: {
-                    avatar: 'https://media.licdn.com/mpr/mpr/shrinknp_200_200/p/3/005/0b2/1b0/1d80474.jpg',
-                    fullname: 'Jesse de Vries',
-                },
-                created_at: new Date(),
-                type: 'henk'
-            },
-            {
-                arrayIndex: 1,
-                user: {
-                    avatar: 'https://media.licdn.com/mpr/mpr/shrink_200_200/p/7/005/0a7/057/33f4a9e.jpg',
-                    fullname: 'Leon Smit',
-                },
-                created_at: new Date(1426949113548),
-                type: 'henk'
-            }
-        ];
-
-        // Real data
-        var partupId = Router.current().params._id;
-
-        return Updates.find({ partup_id: partupId }).map(function (update, idx) {
-            update.arrayIndex = idx;
-            return update;
-        });
+        return getUpdates();
     },
 
-    'anotherDay': function helperAnotherday (update, updates) {
+    'anotherDay': function helperAnotherday (update) {
         var TIME_FIELD = 'created_at';
-        var previousUpdate = updates[update.arrayIndex - 1];
+        
+        var updates = getUpdates();
+        var currentIndex = lodash.findIndex(updates, update);
+        var previousUpdate = updates[currentIndex - 1];
         var previousMoment = moment();
 
         if (previousUpdate) {
