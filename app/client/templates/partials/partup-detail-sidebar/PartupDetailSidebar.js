940baa56bc593cbd85ffd19148b92786ca60d931
Jesse de Vries <jessedvrs@gmail.com>
2015-03-26 11:16:55 +0100

    Fixed partup-detail page cutting

--- app/client/templates/partials/partup-detail-sidebar/PartupDetailSidebar.js
+++ app/client/templates/partials/partup-detail-sidebar/PartupDetailSidebar.js
@@ -2,18 +2,24 @@
 /* Partial rendered */
 /*************************************************************/
 Template.PartialsPartupDetailSidebar.rendered = function() {
+    // Offset to improve window resizing behaviour
     var OFFSET = 100;
-    if(!document) return;
-    var pageElm = document.querySelector('.pu-layout > .pu-sub-page');
+
+    // Find page element
+    var pageElm = $('.pu-layout > .pu-sub-pagecontainer');
     if(!pageElm) return;
-    var rightElm = pageElm.querySelector('.pu-sub-partupdetail-right');
+
+    // Find left side element
+    var rightElm = $('> .pu-sub-partupdetail-right', pageElm);
     if(!rightElm) return;
 
+    // Calculate navigation background width
     var calculateBackgroundWidth = function calculateBackgroundWidth () {
-        var backgroundWidth = (window.innerWidth - pageElm.offsetWidth) / 2 + rightElm.offsetWidth + OFFSET;
+        var backgroundWidth = (window.innerWidth - pageElm.width()) / 2 + rightElm.width() + OFFSET;
         Session.set('partials.partup-detail-sidebar.background-width', backgroundWidth);
     };
 
+    // Trigger calculations
     window.addEventListener('resize', calculateBackgroundWidth);
     calculateBackgroundWidth();
 };
