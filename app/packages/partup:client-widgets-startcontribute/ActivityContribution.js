787d7bd628e3c5dd3b93645f4a79ff4259e03477
Nick Koster <nick.koster@lifely.nl>
2015-03-30 11:46:29 +0200

    Contribute page (work in progress)

--- app/packages/partup:client-widgets-startcontribute/ActivityContribution.js
+++ app/packages/partup:client-widgets-startcontribute/ActivityContribution.js
@@ -1,26 +1,24 @@
 Template.ActivityContribution.helpers({
     'formSchema': Partup.schemas.forms.contribute,
-    'formCollection': Activities.contributions,
     'placeholders': Partup.services.placeholders.startcontribute,
-    'activityId': function () {
-        return this._id;
+    'formId': function () {
+        return Template.instance().contributionId.get() ? Template.instance().contributionId.get() : this._id;
     },
-    'newContribution': function () {
-        return Template.instance().newContribution.get();
+    'contributionId': function () {
+        return Template.instance().contributionId.get() ? Template.instance().contributionId.get() : null;
     },
     'fieldsFromContributionActivity': function () {
-        var contributionId = Template.instance().contributionId.get();
-        if (contributionId) {
-            var activity = Activities.findOne({_id: this._id}, {
-                contributions: {
-                    $elemMatch: {
-                        _id: contributionId
-                    }
-                }
-            });
-            return Partup.transformers.activity.contribution.toFormActivityContribution(activity);
-        }
-        return null;
+        //var contributionId = Template.instance().contributionId.get();
+        //if (contributionId) {
+        //    console.log(contributionId);
+            //var contribution = Activities.findOne({"contribution._id": contributionId});
+            //return Template.instance().contributionId.get() ? Partup.transformers.activity.contribution.toFormActivityContribution(contribution) : null;
+        //} else {
+        //    return null;
+        //}
+    },
+    'checkContributionUser': function(upper_id) {
+        return upper_id == Meteor.user()._id;
     }
 });
 
@@ -28,21 +26,54 @@ Template.ActivityContribution.events({
     //
 });
 
+Template.ActivityContribution.created = function () {
+    var contribution = Activities.findOne({
+            contributions: {
+                $elemMatch: {
+                    upper_id: Meteor.user()._id
+                }
+            }
+        }, {
+            "contributions.$": 1
+        }
+    );
+
+    if (contribution) {
+        this.contributionId = new ReactiveVar(contribution._id);
+    } else {
+        this.contributionId = new ReactiveVar(null);
+    }
+};
+
 AutoForm.addHooks(
     null, {
         onSubmit: function (insertDoc, updateDoc, currentDoc) {
             this.event.preventDefault();
             var self = this;
 
-            var activityId = self.formId;
-            Meteor.call('activities.contributions.insert', activityId, insertDoc, function (error, contributionId) {
-                if (error) {
-                    console.log('something went wrong', error);
-                    return false;
-                }
-            });
+            if (self.template.parent().contributionId.get()) {
+                console.log('update');
+                var contributionId = self.formId;
+                Meteor.call('activities.contributions.update', contributionId, insertDoc, function (error, result) {
+                    if (error) {
+                        console.log('something went wrong', error);
+                        return false;
+                    }
+                    console.log(result);
+                });
+            } else {
+                console.log('insert');
+                var activityId = self.formId;
+                Meteor.call('activities.contributions.insert', activityId, insertDoc, function (error, contributionId) {
+                    if (error) {
+                        console.log('something went wrong', error);
+                        return false;
+                    }
+                    self.template.parent().contributionId.set(contributionId);
+                });
+            }
 
-            AutoForm.resetForm(self.formId);
+            AutoForm.resetForm(this.formId);
             this.done();
 
             return false;
