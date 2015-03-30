6a32c6b654275e8c9333521f330c573f11ba136d
Nick Koster <nick.koster@lifely.nl>
2015-03-30 11:44:04 +0200

    Renamed contribute schema file

---
+++ app/packages/partup:lib/schemas/contribution.js
@@ -0,0 +1,158 @@
+/**
+ * Base Contribution schema
+ * @name contributionBaseSchema
+ * @memberOf partup.schemas
+ * @private
+ */
+var activityBaseSchema = new SimpleSchema({
+    description: {
+        type: String,
+        max: 500
+    },
+    end_date: {
+        type: Date,
+        optional: true
+    },
+    name: {
+        type: String,
+        max: 250
+    },
+    start_date: {
+        type: Date,
+        optional: true
+    }
+});
+
+/**
+ * Activity entity schema
+ * @name activity
+ * @memberOf partup.schemas.entities
+ */
+Partup.schemas.entities.activity = new SimpleSchema([activityBaseSchema, {
+    _id: {
+        type: String,
+        regEx: SimpleSchema.RegEx.Id
+    },
+    contributions: {
+        type: [String],
+        optional: true
+    },
+    "contributions.$._id": {
+        type: String,
+        regEx: SimpleSchema.RegEx.Id
+    },
+    "contributions.$.created_at": {
+        type: Date,
+        defaultValue: Date.now()
+    },
+    "contributions.$.feedbacks": {
+        type: [Object],
+        optional: true
+    },
+    "contributions.feedbacks.$.description": {
+        type: String,
+        optional: true
+    },
+    "contributions.feedbacks.$.rating": {
+        type: Number,
+        decimal: true,
+        optional: true
+    },
+    "contributions.feedbacks.$.upper": {
+        type: Object
+    },
+    "contributions.feedbacks.$.upper._id": {
+        type: String,
+        regEx: SimpleSchema.RegEx.Id
+    },
+    "contributions.feedbacks.$.upper.image": {
+        type: Object,
+        optional: true
+    },
+    "contributions.feedbacks.$.upper.name": {
+        type: String,
+        regEx: SimpleSchema.RegEx.Id
+    },
+    "contributions.$.types": {
+        type: [Object]
+    },
+    "contributions.$.types.$.type": {
+        type: String
+    },
+    "contributions.$.types.$.type_data": {
+        type: Object
+    },
+    "contributions.$.types.$.type_data.amount": {
+        type: Number,
+        min: 0,
+        optional: true
+    },
+    "contributions.$.types.$.type_data.description": {
+        type: String,
+        optional: true
+    },
+    "contributions.$.updated_at": {
+        type: Date,
+        defaultValue: Date.now()
+    },
+    "contributions.$.upper_id": {
+        type: String,
+        regEx: SimpleSchema.RegEx.Id
+    },
+    created_at: {
+        type: Date,
+        defaultValue: Date.now()
+    },
+    creator_id: {
+        type: String,
+        regEx: SimpleSchema.RegEx.Id
+    },
+    partup_id: {
+        type: String,
+        regEx: SimpleSchema.RegEx.Id
+    },
+    updated_at: {
+        type: Date,
+        defaultValue: Date.now()
+    }
+}]);
+
+/**
+ * Activity form schema
+ * @name startActivities
+ * @memberOf partup.schemas.forms
+ */
+Partup.schemas.forms.startActivities = new SimpleSchema([activityBaseSchema, {
+    //
+}]);
+
+
+
+/**
+ * Contribute Form
+ * @name contribute
+ * @memberOf partup.schemas.forms
+ */
+Partup.schemas.forms.contribute = new SimpleSchema({
+    type_want: {
+        type: Boolean,
+        optional: true
+    },
+    type_can_amount: {
+        type: Number,
+        optional: true
+    },
+    type_have_amount: {
+        type: Number,
+        optional: true
+    },
+    type_have_description: {
+        type: String,
+        max: 255,
+        optional: true
+    },
+    type_donate_amount: {
+        type: Number,
+        optional: true
+    }
+});
