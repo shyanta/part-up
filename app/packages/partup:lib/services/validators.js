856dfe9ec0b397ddeaf4c351a58f38af7885f170
Peter Peerdeman <peter@lifely.nl>
2015-03-30 18:00:00 +0200

    added validators and tags services

---
+++ app/packages/partup:lib/services/validators.js
@@ -0,0 +1,8 @@
+/**
+ @namespace Validators helper service
+ @name partup.services.validators
+ @memberOf partup.services
+ */
+Partup.services.validators = {
+    tagsSeparatedByComma: /^\s*\w(\s*,?\s*\w)*\s*$/
+};
