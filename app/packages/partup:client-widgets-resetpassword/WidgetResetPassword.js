5d739e4fdd19ecd7040ba6ba87516bec18b038b2
Peter Peerdeman <peter@lifely.nl>
2015-04-01 16:19:27 +0200

    finished resetpassword functionality

--- app/packages/partup:client-widgets-resetpassword/WidgetResetPassword.js
+++ app/packages/partup:client-widgets-resetpassword/WidgetResetPassword.js
@@ -1,6 +1,6 @@
 Template.WidgetResetPassword.helpers({
     formSchema: Partup.schemas.forms.resetPassword,
-    placeholders: Partup.services.placeholders.resetPassword
+    placeholders: Partup.services.placeholders.resetPassword,
 });
 
 Template.WidgetResetPassword.events({
@@ -10,16 +10,20 @@ Template.WidgetResetPassword.events({
 AutoForm.hooks({
     resetPasswordForm: {
         onSubmit: function(insertDoc, updateDoc, currentDoc) {
-            event.preventDefault();
-
-            Meteor.resetPassword(Router.current().params.token, insertDoc.password, function(error) {
+            var self = this;
+            self.event.preventDefault();
+            var token = Router.current().params.token;
+            Accounts.resetPassword(token, insertDoc.password, function(error) {
 
                 if(error) {
                     Partup.ui.notify.error(error.reason);
-                    this.done(new Error(error.reason));
+                    self.done(new Error(error.reason));
                 }
 
+                self.done();
+
                 Router.go('home');
+
             });
         }
     }
