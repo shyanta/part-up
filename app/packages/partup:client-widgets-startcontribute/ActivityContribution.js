117e7d9dc8152d27e915506e20f31bf3f3df92cf
Léon Smit <leonsmit@Leons-MacBook-Pro.local>
2015-04-01 11:59:53 +0200

    optimised the popover handler code some more and inputs now automatically get focussed on click(open), also the value for the contributions are updated on keyup

--- app/packages/partup:client-widgets-startcontribute/ActivityContribution.js
+++ app/packages/partup:client-widgets-startcontribute/ActivityContribution.js
@@ -1,7 +1,13 @@
 Template.ActivityContribution.created = function () {
-    this.showContributeHave = new ReactiveVar(false);
+
+    // contribute can init values
     this.showContributeCan = new ReactiveVar(false);
     this.contributeCanValue = new ReactiveVar(0);
+
+    // contribute have init values
+    this.showContributeHave = new ReactiveVar(false);
+    this.contributeHaveValue = new ReactiveVar(0);
+    this.contributeHaveExtraValue = new ReactiveVar("");
 };
 
 Template.ActivityContribution.helpers({
@@ -20,46 +26,58 @@ Template.ActivityContribution.helpers({
     },
     'contributionCanChecked': function(){
         return (Template.instance().contributeCanValue.get() > 0) ? 'checked' : '';
+    },
+    'contributionHaveChecked': function(){
+        return (Template.instance().contributeCanValue.get() > 0) ? 'checked' : '';
     }
 });
 
 Template.ActivityContribution.events({
-    'click [data-click-contribution]': function clickContribution(event, template){
+    'click [data-open-contribution]': function clickContribution(event, template){
         // show the popover on click
 
         // prevent any form actions and checkbox selections
         event.preventDefault();
 
+        // focus on the first input field
+        $(event.currentTarget).find('input[data-input-contribution]:first').focus();
+
         // get the key for the boolean wich is associated with this field
-        var booleanKey = $(event.currentTarget).data("click-contribution");
+        var booleanKey = $(event.currentTarget).data("open-contribution");
 
         // set reactive var boolean to true to open popover
         template[booleanKey].set(true)
     },
-    'mouseenter [data-hover-contribution]': function hoverContribution(event, template){
+    'mouseenter [data-open-contribution]': function mouseEnterContribution(event, template){
         // this event handler does almost the same as the 
         // click handler, except it's on hover and with a delay
-        var booleanKey = $(event.currentTarget).data("hover-contribution");
+        var booleanKey = $(event.currentTarget).data("open-contribution");
         if(!template[booleanKey].get()){
             template.showTimeout = Meteor.setTimeout(function(){
                 template[booleanKey].set(true);
             },1000);
         }
     },
-    'mouseleave [data-hover-contribution]': function mouseleaveContributionCan(event, template){
+    'mouseleave [data-open-contribution]': function mouseLeaveContribution(event, template){
         // hide the popover on mouse leave
 
+        // blur input fields to prevent accidental input change by user
+        $(event.currentTarget).find('input[data-input-contribution]').blur();
+
         // get the key for the boolean wich is associated with this field
-        var booleanKey = $(event.currentTarget).data("hover-contribution");
+        var booleanKey = $(event.currentTarget).data("open-contribution");
 
         // cancel any timeout that will show the current popover
         Meteor.clearTimeout(template.showTimeout);
 
         // set reactive var boolean to false to hide popover
         template[booleanKey].set(false);
+
     },
-    'change [data-input-contribution-can]': function changeContrinutionCan(event, template){
-        template.contributeCanValue.set(event.target.value);
+    'keyup [data-input-contribution]': function changeContrinutionCan(event, template){
+        var valueKey = $(event.currentTarget).data("input-contribution");
+        console.log(valueKey,event.target.value)
+        template[valueKey].set(event.target.value);
     }
 });
 
