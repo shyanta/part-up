be37fd6bf579f6659b68c1431c314b7d9b19afe3
Jesse de Vries <jessedvrs@gmail.com>
2015-04-01 13:26:02 +0200

    Added own version of af input types

---
+++ app/packages/partup:client-form/inputTypes/month/month.js
@@ -0,0 +1,11 @@
+AutoForm.addInputType("month", {
+  template: "PartupFormAfInputMonth",
+  valueConverters: {
+    "stringArray": function (val) {
+      if (typeof val === "string" && val.length > 0) {
+        return [val];
+      }
+      return val;
+    }
+  }
+});
---
+++ app/packages/partup:client-form/inputTypes/time/time.js
@@ -0,0 +1,11 @@
+AutoForm.addInputType("time", {
+  template: "PartupFormAfInputTime",
+  valueConverters: {
+    "stringArray": function (val) {
+      if (typeof val === "string" && val.length > 0) {
+        return [val];
+      }
+      return val;
+    }
+  }
+});
