e29cd3709f338f0f3d57b96b09a41c70e2c92930
Léon Smit <leonsmit@Leons-MacBook-Pro.local>
2015-04-02 11:59:12 +0200

    activity edit template and logic

--- app/packages/partup:client-widgets-startactivities/Activity.js
+++ app/packages/partup:client-widgets-startactivities/Activity.js
@@ -3,13 +3,58 @@ Template.Activity.onCreated(function(){
 })
 
 Template.Activity.helpers({
+    'Partup': Partup,
+    'placeholders': Partup.services.placeholders.startactivities,
     editMode: function(){
         return Template.instance().editMode.get();
     }
 })
 
 Template.Activity.events({
-    'click [data-toggle-edit-mode]': function editMode(event, template){
-        template.editMode.set(!template.editMode.get());
+    'click [data-enable-edit-mode]': function editMode(event, template){
+        template.editMode.set(true);
+        // defer is nessesary to wait for template re rendering
+        lodash.defer(function(){
+
+            // init datepicker
+            template.$('.pu-datepicker').datepicker({
+                language: moment.locale(),
+                format: "yyyy-mm-dd",
+            });
+        })
+    },
+    'click [data-save-and-disable-edit-mode]': function saveActivity(event, template){
+
+        template.editMode.set(false);
+    },
+    'click [data-remove-activity]': function saveActivity(event, template){
+        alert('Why not Zoidberg?\n(V)_|•,,,•|_(V)');
     }
 })
+
+AutoForm.hooks({
+    activityEditForm: {
+        onSubmit: function (insertDoc, updateDoc, currentDoc) {
+            event.preventDefault();
+
+            // update activity
+
+
+            // var partupId = Session.get('partials.start-partup.current-partup');
+            // var self = this;
+
+            // Meteor.call('activities.update', partupId, insertDoc, function (error, result) {
+            //     if (error) {
+            //         console.log('something went wrong', error);
+            //         return false;
+            //     }
+
+            //     // Session.set('showDateButton', true);
+            //     AutoForm.resetForm('activityEditForm');
+            //     self.done();
+            // });
+
+            return false;
+        }
+    }
+});
