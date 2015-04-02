5f9677cf8331591b3ac097a6c7d20fd57c73d91d
Léon Smit <leonsmit@Leons-MacBook-Pro.local>
2015-04-02 14:49:04 +0200

    basic helpers

--- app/packages/partup:client-widgets-startpromote/WidgetStartPromote.js
+++ app/packages/partup:client-widgets-startpromote/WidgetStartPromote.js
@@ -1,1 +1,11 @@
-// Write your package code here!
+Template.WidgetStartPromote.helpers({
+    Partup: Partup,
+    placeholders: Partup.services.placeholders.startdetails,
+    partup: function () {
+        var partupId = Session.get('partials.start-partup.current-partup');
+        return Partups.findOne({ _id: partupId });
+    },
+    uploadedImage: function() {
+        return Images.findOne({_id:Session.get('partials.start-partup.uploaded-image')});
+    }
+});
