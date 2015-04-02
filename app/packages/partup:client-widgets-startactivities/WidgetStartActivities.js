1b748073d7f010ed4bbadd79676a02cf423f6ec8
Peter Peerdeman <peter@lifely.nl>
2015-04-02 17:53:01 +0200

    refactoring startactivities, fixing problems along the way (translations, button refactoring, avoiding use of session variables)

--- app/packages/partup:client-widgets-startactivities/WidgetStartActivities.js
+++ app/packages/partup:client-widgets-startactivities/WidgetStartActivities.js
@@ -1,56 +1,58 @@
+var showDatePicker = new ReactiveVar(false);
+
 Template.WidgetStartActivities.helpers({
-    'Partup': Partup,
+    'formSchema': Partup.schemas.forms.startActivities,
     'placeholders': Partup.services.placeholders.startactivities,
     'partupActivities': function () {
         var partupId = Session.get('partials.start-partup.current-partup');
         return Activities.find({ partup_id: partupId }, {sort: { created_at: -1 }});
     },
-    'showDateButton': function () {
-        return Session.get('showDateButton');
+    'currentPartupId': function() {
+        return Session.get('partials.start-partup.current-partup');
+    },
+    'showDatePicker': function() {
+        return showDatePicker.get();
     }
 });
 
 Template.WidgetStartActivities.events({
-    'click #nextPage': function () {
-        Router.go('start-contribute', {_id: Session.get('partials.start-partup.current-partup')});
-    },
     'click [data-end-date-button]': function(event, template) {
         event.preventDefault();
-        Session.set('showDateButton', false);
+        showDatePicker.set(true);
 
         // defer is nessesary to wait for template re rendering
-        lodash.defer(function(){
+        Meteor.defer(function(){
 
             // init datepicker
             template.$('.pu-datepicker').datepicker({
                 language: moment.locale(),
-                format: "yyyy-mm-dd",
+                format: "yyyy-mm-dd"
             });
         })
         
     }
 });
 
-Template.WidgetStartActivities.rendered = function () {
-    Session.set('showDateButton', true);
-};
+Template.WidgetStartActivities.onCreated(function () {
+
+});
 
 AutoForm.hooks({
     activityForm: {
-        onSubmit: function (insertDoc, updateDoc, currentDoc) {
-            event.preventDefault();
-            var partupId = Session.get('partials.start-partup.current-partup');
+        onSubmit: function (insertDoc) {
             var self = this;
+            self.event.preventDefault();
+            var partupId = Session.get('partials.start-partup.current-partup');
 
-            Meteor.call('activities.insert', partupId, insertDoc, function (error, result) {
+            Meteor.call('activities.insert', partupId, insertDoc, function (error) {
                 if (error) {
-                    console.log('something went wrong', error);
-                    return false;
+                    Partup.ui.notify.error(error.reason);
+                    self.done(error);
+                } else {
+                    showDatePicker.set(false);
+                    AutoForm.resetForm('activityForm');
+                    self.done();
                 }
-
-                Session.set('showDateButton', true);
-                AutoForm.resetForm('activityForm');
-                self.done();
             });
 
             return false;
--- app/packages/partup:client-widgets-startactivities/package.js
+++ app/packages/partup:client-widgets-startactivities/package.js
@@ -7,20 +7,33 @@ Package.describe({
 
 Package.onUse(function (api) {
     api.use([
+        'tap:i18n'
+    ], ['client', 'server']);
+
+    api.use([
         'templating',
         'aldeed:autoform',
         'partup:lib',
-        'tap:i18n'
+        'reactive-var'
     ], 'client');
 
     api.addFiles([
         'package-tap.i18n',
+
         'WidgetStartActivitiesPlaceholders.js',
         'WidgetStartActivities.html',
         'WidgetStartActivities.js',
+
         'Activity.html',
         'Activity.js',
+
         'i18n/en.i18n.json',
         'i18n/nl.i18n.json'
     ], 'client');
+
+    api.addFiles([
+        'package-tap.i18n',
+        'i18n/en.i18n.json',
+        'i18n/nl.i18n.json'
+    ], 'server');
 });
