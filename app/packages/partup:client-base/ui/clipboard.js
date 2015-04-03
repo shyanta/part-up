5874b1e00333d1075d5ff1ade05ae0b1aa7d6381
Léon Smit <leonsmit@Leons-MacBook-Pro.local>
2015-04-03 10:18:33 +0200

    global helper for copy-to-clipboard

---
+++ app/packages/partup:client-base/ui/clipboard.js
@@ -0,0 +1,19 @@
+Partup.ui.clipboard = {
+    applyToElement: function(element, copyCallBack){
+        // Disables other default handlers on click (avoid issues)
+        element.on('click', function(e) {
+            e.preventDefault();
+        });
+
+        // Apply clipboard click event
+        element.clipboard({
+            path: '/extra/jquery.clipboard.swf',
+
+            copy: function() {
+                var copyText = $(this).text();
+                copyCallBack();
+                return copyText;
+            }
+        });
+    }
+};
