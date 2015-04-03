6bcf83f7a8c91012fca3e5de1a9a7fbd8489e505
Léon Smit <leonsmit@Leons-MacBook-Pro.local>
2015-04-03 10:40:20 +0200

    jsdocs

--- app/packages/partup:client-base/ui/clipboard.js
+++ app/packages/partup:client-base/ui/clipboard.js
@@ -1,4 +1,12 @@
 Partup.ui.clipboard = {
+
+    /**
+     * Create a copy to clipboard element width callback
+     *
+     * @memberOf partup.ui
+     * @param {Element} target element to serve as the clickable copy to clipboard
+     * @param {function} Callback after copy
+     */
     applyToElement: function(element, copyCallBack){
         // Disables other default handlers on click (avoid issues)
         element.on('click', function(e) {
