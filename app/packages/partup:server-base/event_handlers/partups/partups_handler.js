551b742dc8f0e6c64c84a30d3898d5753e641154
Bryan te Beek <bryantebeek@gmail.com>
2015-04-03 10:42:48 +0200

    Update message on partup field changes debug

--- app/packages/partup:server-base/event_handlers/partups/partups_handler.js
+++ app/packages/partup:server-base/event_handlers/partups/partups_handler.js
@@ -11,5 +11,5 @@ Event.on('partups.removed', function (partup) {
 });
 
 Event.on('partups.*.updated', function (partup, value) {
-   Log.debug('Partup [' + partup._id + '] had it\'s value for \'' + value.name + '\' change from ' + value.old + ' to ' + value.new);
+   Log.debug('Partup [' + partup._id + '] had it\'s value for [' + value.name + '] change from [' + value.old + '] to [' + value.new + ']');
 });
