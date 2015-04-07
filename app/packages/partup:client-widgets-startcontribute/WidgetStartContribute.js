11c12cdf35be675c20daf60578b6095173836ec0
Jesse de Vries <jessedvrs@gmail.com>
2015-04-07 14:41:45 +0200

    Code cleanup & styling fixes

--- app/packages/partup:client-widgets-startcontribute/WidgetStartContribute.js
+++ app/packages/partup:client-widgets-startcontribute/WidgetStartContribute.js
@@ -1,5 +1,7 @@
+/*************************************************************/
+/* Widget helpers */
+/*************************************************************/
 Template.WidgetStartContribute.helpers({
-    'Partup': Partup,
     'partupActivities': function () {
         var partupId = Session.get('partials.start-partup.current-partup');
         return Activities.find({ partup_id: partupId }, {sort: { created_at: -1 }});
@@ -8,7 +10,3 @@ Template.WidgetStartContribute.helpers({
         return Session.get('partials.start-partup.current-partup');
     }
 });
-
-Template.WidgetStartContribute.rendered = function () {
-    //
-};
