d94c4fd2aa53538ac8ddcb9fad9b38a496619051
Jesse de Vries <jessedvrs@gmail.com>
2015-04-07 13:46:06 +0200

    Fixed reset password form validation

--- app/packages/partup:client-widgets-resetpassword/WidgetResetPassword.js
+++ app/packages/partup:client-widgets-resetpassword/WidgetResetPassword.js
@@ -1,30 +1,56 @@
+/*************************************************************/
+/* Widget initial */
+/*************************************************************/
+Template.WidgetRegisterOptional.onRendered(function() {
+    AutoForm.resetForm("registerOptionalForm");
+});
+
+/*************************************************************/
+/* Widget helpers */
+/*************************************************************/
 Template.WidgetResetPassword.helpers({
     formSchema: Partup.schemas.forms.resetPassword,
     placeholders: Partup.services.placeholders.resetPassword
 });
 
+/*************************************************************/
+/* Widget events */
+/*************************************************************/
 Template.WidgetResetPassword.events({
 
 });
 
+/*************************************************************/
+/* Widget form hooks */
+/*************************************************************/
 AutoForm.hooks({
     resetPasswordForm: {
+        beginSubmit: function() {
+            Partup.ui.forms.removeAllStickyFieldErrors(this);
+        },
         onSubmit: function(insertDoc, updateDoc, currentDoc) {
             var self = this;
-            self.event.preventDefault();
+
             var token = Router.current().params.token;
             Accounts.resetPassword(token, insertDoc.password, function(error) {
-
-                if(error) {
-                    Partup.ui.notify.error(error.reason);
-                    self.done(new Error(error.reason));
+                if(error && error.message) {
+                    switch (error.message) {
+                        // case 'User not found [403]':
+                        //     Partup.ui.forms.addStickyFieldError(self, 'email', 'emailNotFound');
+                        //     break;
+                        default:
+                            Partup.ui.notify.error(error.reason);
+                    }
+                    AutoForm.validateForm(self.formId);
+                    self.done(new Error(error.message));
+                    return;
                 }
 
                 self.done();
-
                 Router.go('home');
-
             });
+
+            return false;
         }
     }
 });
