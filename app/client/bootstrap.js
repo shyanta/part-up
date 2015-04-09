b7ad59920f4084e30dd2cc681448a7563fe6a381
Peter Peerdeman <peter@lifely.nl>
2015-04-09 15:46:02 +0200

    detect language based on browserdefault, fallback to 'nl'

--- app/client/bootstrap.js
+++ app/client/bootstrap.js
@@ -4,7 +4,12 @@ Meteor.startup(function() {
     /* Language configuration */
     /*************************************************************/
     if(TAPi18n && Partup) {
-        var language = TAPi18n.getLanguage() || 'en';
+        var detectedLocale = navigator.language || navigator.userLanguage;
+        if(detectedLocale && detectedLocale.match(/^[a-z]{2}-[A-Z]{2}$/)) {
+            var detectedLanguage = detectedLocale.split('-')[0];
+        }
+        var language = detectedLanguage || 'nl';
+
         Partup.ui.language.changeLanguage(language);
     }
 
