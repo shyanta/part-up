0cd72768f38d33f05ba272b1af0b164368a13d46
Léon Smit <leonsmit@Leons-MacBook-Pro.local>
2015-04-09 17:36:00 +0200

    Social username validators

--- app/packages/partup:lib/services/validators.js
+++ app/packages/partup:lib/services/validators.js
@@ -8,6 +8,20 @@ Partup.services.validators = {
 
     // minimum 8 characters, at least 1 number, at least 1 capital letter
     password: /(?=^.{8,}$)(?=.*\d)(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/,
-    facebookUsername: /^[a-zA-Z\d.]{1,}$/
+
+    // Facebook usernames can only contain a-z A-Z 0-9 and .
+    // limit is 0 (not validated)
+    facebookUsername: /^[a-zA-Z0-9.]{1,}$/,
+
+    // Instagram usernames can only contain a-z A-Z . and _
+    // limit is 0 (not validated)
+    instagramUsername: /^[a-zA-Z._]{1,}$/,
+
+    // Linkedin usernames can only contain a-z A-Z 0-9
+    // min is 5 chars and max is 30 chars according to linkedin guidelines
+    linkedinUsername: /^[a-zA-Z0-9]{5,30}$/,
+
+    // Twitter usernames can only contain a-z A-Z 0-9 and have a limit of 15 chars
+    twitterUsername: /^[A-Za-z0-9_]{1,15}$/,
 
 };
