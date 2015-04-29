50c75ed545012128816ea699ade90a6d4e9577cd
Jesse de Vries <jessedvrs@gmail.com>
2015-04-29 14:05:14 +0200

    Fixed activity-contributions page language switch problems

--- app/packages/partup:client-widgets-startcontribute/package.js
+++ app/packages/partup:client-widgets-startcontribute/package.js
@@ -7,25 +7,36 @@ Package.describe({
 
 Package.onUse(function (api) {
     api.use([
+        'tap:i18n'
+    ], ['client', 'server']);
+
+    api.use([
         'templating',
         'aldeed:autoform',
         'aldeed:template-extension',
         'partup:lib',
         'partup:client-base',
-        'partup:client-widgets-dropdowns',
-        'tap:i18n',
         'reactive-var',
         'reactive-dict'
     ], 'client');
 
     api.addFiles([
         'package-tap.i18n',
+
         'WidgetStartContributePlaceholders.js',
         'WidgetStartContribute.html',
         'WidgetStartContribute.js',
+        
         'ActivityContribution.html',
         'ActivityContribution.js',
+
         'i18n/en.i18n.json',
         'i18n/nl.i18n.json'
     ], 'client');
+
+    api.addFiles([
+        'package-tap.i18n',
+        'i18n/en.i18n.json',
+        'i18n/nl.i18n.json'
+    ], 'server');
 });
