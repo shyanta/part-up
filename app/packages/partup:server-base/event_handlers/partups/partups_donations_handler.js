5f85d7bb9deb7cea6d7e66b017277bd19ee15f3c
Peter Peerdeman <peter@lifely.nl>
2015-04-30 17:22:13 +0200

    default to dutch language for user test

--- app/packages/partup:server-base/event_handlers/partups/partups_donations_handler.js
+++ app/packages/partup:server-base/event_handlers/partups/partups_donations_handler.js
@@ -4,9 +4,9 @@
 Event.on('partups.inserted', function (userId, partup) {
     var donationActivity = {
         created_at: new Date(),
-        description: __('activities-donation-description'),
+        description: __('activities-donation-description', { lng: 'nl'}),
         donation: true,
-        name: __('activities-donation-name'),
+        name: __('activities-donation-name', { lng: 'nl'}),
         partup_id: partup._id,
         upper_id: userId
     };
