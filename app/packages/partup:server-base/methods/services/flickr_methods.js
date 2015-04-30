474e535fa934a682ac457ea5cc55c47d8bb5420c
Nick Koster <nick.koster@lifely.nl>
2015-04-30 15:30:28 +0200

    Flickr methods overhaul

--- app/packages/partup:server-base/methods/services/flickr_methods.js
+++ app/packages/partup:server-base/methods/services/flickr_methods.js
@@ -13,27 +13,41 @@ Meteor.methods({
      *
      * @param {string[]} tags
      * @param {number} count Number of results to return
+     * @param {string[]} fallbackTags Tags
      */
-    'partups.images.tags.search': function (tags, count, fallbackTags) {
+    'partups.services.flickr.search': function (tags, count, fallbackTags) {
+        // Set default values
+        count = count || 5;
+        fallbackTags = fallbackTags || ['teamwork', 'group', 'team'];
 
         var lookupTags = Meteor.wrapAsync(function (tags, count, callback) {
-
             var searchByTags = function (tags, count, photos) {
+                var searchableTags = tags.join(' ');
 
-                var searchableTags = tags.join(',');
                 flickr.get('photos.search', {
-                    'tags': searchableTags,
-                    'tag_mode': 'all',
-                    'media': 'photos',
-                    'sort': 'interestingness-desc',
-                    'license': 4, // Attribution License
+                    'text': encodeURIComponent(searchableTags), // Search for text instead of tags (huge difference)
+                    'extras': 'url_l', // Only search for large images
+                    'per_page': count * 5, // Get more results then needed because we will be filtering them
+                    'sort': 'relevance',
+                    'license': [9, 4], // CC0 & Attribution License
                     'content_type': 1 // Photos only
-                }, function (result) {
+                }, function (result, error) {
+                    if (error) {
+                        Log.error(error);
+                        throw new Meteor.Error(400, 'Error while getting photos from Flickr: ' + error);
+                    }
+
                     result.photos.photo.forEach(function (photo) {
+                        if (!photo.url_l || (photo.height_l < photo.width_l)) return; // Only landscape photos
                         photos.push({
-                            'imageUrl': 'https://farm' + photo.farm + '.staticflickr.com/' + photo.server + '/' + photo.id + '_' + photo.secret + '_b.jpg',
-                            'authorUrl': 'https://www.flickr.com/photos/' + photo.owner
+                            'imageUrl': photo.url_l,
+                            'authorUrl': 'https://www.flickr.com/people/' + photo.owner
                         });
+                    });
+
+                    // We don't want all photos to be from 1 Flickr user/album
+                    photos = lodash.unique(photos, function(item, key, a) {
+                        return item.authorUrl;
                     });
 
                     if (photos.length < count) {
