4a36b070cb054f41aa804864a1eb4cdb87e6c08f
Jesse de Vries <jessedvrs@gmail.com>
2015-04-30 14:13:09 +0200

    Showing full date when hovering over time in updates timeline

--- app/packages/partup:client-base/helpers/dateFormatters.js
+++ app/packages/partup:client-base/helpers/dateFormatters.js
@@ -28,6 +28,14 @@ Template.registerHelper('partupDateNormal', function(date) {
     return moment(date).format('LL');
 });
 
+Template.registerHelper('partupDateFull', function(date) {
+    return moment(date).format('LLL');
+});
+
+Template.registerHelper('partupDateCustom', function(date, format) {
+    return moment(date).format(format);
+});
+
 Template.registerHelper('partupDateISO', function(date) {
     return moment(date).toISOString();
 });
