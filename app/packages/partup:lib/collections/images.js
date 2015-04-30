d30eeb5e9cf8790ac0c6cdf94a08c79a0e7eaf32
Bryan te Beek <bryantebeek@gmail.com>
2015-04-30 13:40:22 +0200

    Always (server+client) use the correct storage adapters on each environment

--- app/packages/partup:lib/collections/images.js
+++ app/packages/partup:lib/collections/images.js
@@ -1,44 +1,65 @@
 var stores = [];
 
-if (Meteor.isServer && (process.env.NODE_ENV.match(/staging|acceptance|production/)) ) {
-    console.log('Creating Image store with S3');
+if (process.env.NODE_ENV.match(/development/)) {
+    if (Meteor.isServer) {
+        console.log('Creating Image store with GridFS');
 
-    stores.push(new FS.Store.S3('original', {
-        region: process.env.AWS_BUCKET_REGION,
-        bucket: process.env.AWS_BUCKET_NAME
-    }));
-
-    stores.push(new FS.Store.S3('1200x520', {
-        region: process.env.AWS_BUCKET_REGION,
-        bucket: process.env.AWS_BUCKET_NAME,
-        transformWrite: function (image, readStream, writeStream) {
-            gm(readStream, image.name()).resize(1200, 520).stream().pipe(writeStream);
-        }
-    }));
-
-    stores.push(new FS.Store.S3('360x360', {
-        region: process.env.AWS_BUCKET_REGION,
-        bucket: process.env.AWS_BUCKET_NAME,
-        transformWrite: function (image, readStream, writeStream) {
-            gm(readStream, image.name()).resize(360, 360).stream().pipe(writeStream);
-        }
-    }));
-} else {
-    console.log('Creating Image store with gridfs');
         stores.push(new FS.Store.GridFS('original', {
-    }));
+            region: process.env.AWS_BUCKET_REGION,
+            bucket: process.env.AWS_BUCKET_NAME
+        }));
 
-    stores.push(new FS.Store.GridFS('1200x520', {
-        transformWrite: function (image, readStream, writeStream) {
-            gm(readStream, image.name()).resize(1200, 520).stream().pipe(writeStream);
-        }
-    }));
+        stores.push(new FS.Store.GridFS('1200x520', {
+            region: process.env.AWS_BUCKET_REGION,
+            bucket: process.env.AWS_BUCKET_NAME,
+            transformWrite: function (image, readStream, writeStream) {
+                gm(readStream, image.name()).resize(1200, 520).stream().pipe(writeStream);
+            }
+        }));
 
-    stores.push(new FS.Store.GridFS('360x360', {
-        transformWrite: function (image, readStream, writeStream) {
-            gm(readStream, image.name()).resize(360, 360).stream().pipe(writeStream);
-        }
-    }));
+        stores.push(new FS.Store.GridFS('360x360', {
+            region: process.env.AWS_BUCKET_REGION,
+            bucket: process.env.AWS_BUCKET_NAME,
+            transformWrite: function (image, readStream, writeStream) {
+                gm(readStream, image.name()).resize(360, 360).stream().pipe(writeStream);
+            }
+        }));
+    } else {
+        stores.push(new FS.Store.GridFS('original'));
+        stores.push(new FS.Store.GridFS('1200x520'));
+        stores.push(new FS.Store.GridFS('360x360'));
+    }
+}
+
+if (process.env.NODE_ENV.match(/staging|acceptance|production/)) ) {
+    if (Meteor.isServer) {
+        console.log('Creating Image store with S3');
+
+        stores.push(new FS.Store.S3('original', {
+            region: process.env.AWS_BUCKET_REGION,
+            bucket: process.env.AWS_BUCKET_NAME
+        }));
+
+        stores.push(new FS.Store.S3('1200x520', {
+            region: process.env.AWS_BUCKET_REGION,
+            bucket: process.env.AWS_BUCKET_NAME,
+            transformWrite: function (image, readStream, writeStream) {
+                gm(readStream, image.name()).resize(1200, 520).stream().pipe(writeStream);
+            }
+        }));
+
+        stores.push(new FS.Store.S3('360x360', {
+            region: process.env.AWS_BUCKET_REGION,
+            bucket: process.env.AWS_BUCKET_NAME,
+            transformWrite: function (image, readStream, writeStream) {
+                gm(readStream, image.name()).resize(360, 360).stream().pipe(writeStream);
+            }
+        }));
+    } else {
+        stores.push(new FS.Store.S3('original'));
+        stores.push(new FS.Store.S3('1200x520'));
+        stores.push(new FS.Store.S3('360x360'));
+    }
 }
 
 Images = new FS.Collection('images', {
