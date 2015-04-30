d29e3594ee6316c36fa734e0a8a89f73b8516df4
Peter Peerdeman <peter@lifely.nl>
2015-04-30 17:35:12 +0200

    Revert "Change to filesystem adapter"
    This reverts commit 944b9e1fd617fac0e3925e8b9888a13ec621a414.

--- app/packages/partup:lib/collections/images.js
+++ app/packages/partup:lib/collections/images.js
@@ -1,37 +1,25 @@
 var stores = [];
 
 if (Meteor.isServer) {
-    console.log('Creating Image store with FileSystem');
+    console.log('Creating Image store with GridFS');
 
-    if (process.env.NODE_ENV.match(/staging|acceptance|production/)) {
-        FS.TempStore.Storage = new FS.Store.FileSystem('_tempstore', {
-            internal :  true,
-            path : '/app-storage/tmp',
-        });
-    } else {
-        FS.TempStore.Storage = new FS.Store.FileSystem('_tempstore', {
-            internal :  true,
-            path : '/tmp',
-        });
-    }
+    stores.push(new FS.Store.GridFS('original'));
 
-    stores.push(new FS.Store.FileSystem('original'));
-
-    stores.push(new FS.Store.FileSystem('1200x520', {
+    stores.push(new FS.Store.GridFS('1200x520', {
         transformWrite: function (image, readStream, writeStream) {
             gm(readStream, image.name()).resize(1200, 520).stream().pipe(writeStream);
         }
     }));
 
-    stores.push(new FS.Store.FileSystem('360x360', {
+    stores.push(new FS.Store.GridFS('360x360', {
         transformWrite: function (image, readStream, writeStream) {
             gm(readStream, image.name()).resize(360, 360).stream().pipe(writeStream);
         }
     }));
 } else {
-    stores.push(new FS.Store.FileSystem('original'));
-    stores.push(new FS.Store.FileSystem('1200x520'));
-    stores.push(new FS.Store.FileSystem('360x360'));
+    stores.push(new FS.Store.GridFS('original'));
+    stores.push(new FS.Store.GridFS('1200x520'));
+    stores.push(new FS.Store.GridFS('360x360'));
 }
 
 Images = new FS.Collection('images', {
