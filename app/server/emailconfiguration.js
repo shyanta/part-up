0f09e2a20d379d522f5bb0682f7325ea4132f1a3
Bryan te Beek <bryantebeek@gmail.com>
2015-05-01 11:59:12 +0200

    Add basic implementation of the email html code

--- app/server/emailconfiguration.js
+++ app/server/emailconfiguration.js
@@ -1,16 +1,17 @@
 Accounts.emailTemplates.from = 'Part-up <noreply@part-up.com>';
+
 Accounts.emailTemplates.resetPassword.subject = function(user) {
     return 'Reset Part-up password';
 }
+
 Accounts.emailTemplates.resetPassword.text = function(user, url) {
-    return [
-        'Hello ' + user.profile.name + '\n',
-        '\n',
-        'To reset your password, click the link below\n',
-        '\n',
-        url.replace('/#', '') + '\n',
-        '\n',
-        'Regards,\n',
-        'Part-up'
-    ].join();
+    return SSR.render('emailText', { user: user, url: url.replace('/#', '') });
 }
+
+SSR.compileTemplate('emailText', Assets.getText('emails/ResetPassword.html'));
+
+// Template.emailText.helpers({
+//     time: function() {
+//         return new Date().toString();
+//     }
+// });
