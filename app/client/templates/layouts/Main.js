bdd66e3288d5df6dbccd89e13a3349b8f7346e47
Léon Smit <leonsmit@Leons-MacBook-Pro.local>
2015-05-06 10:31:27 +0200

    removed popup logic from main template

--- app/client/templates/layouts/Main.js
+++ app/client/templates/layouts/Main.js
@@ -2,10 +2,8 @@
 /* Layout helpers */
 /*************************************************************/
 Template.LayoutsMain.helpers({
-    
-    popupOpen: function(){
-        return Session.get('main.popup-open');
-    }
+
+    //
     
 });
 
@@ -15,13 +13,7 @@ Template.LayoutsMain.helpers({
 /*************************************************************/
 Template.LayoutsMain.events({
     
-    'click [data-close-popup]': function closePopup(event, template){
-        var overlay = $('[data-close-popup]'),
-            clickedElement = $(event.target);
-
-        // close popup if overlay is clicked
-        if(clickedElement[0] === overlay[0]) Session.set('main.popup-open', false);
-    }
+    //
     
 });
 
