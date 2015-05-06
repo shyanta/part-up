66e3a874e157a915b5ee3b7c63bacfde0871711d
Léon Smit <leonsmit@Leons-MacBook-Pro.local>
2015-05-06 14:31:58 +0200

    popup for new activity

--- app/packages/partup:client-widgets-partupdetail-activities/WidgetPartupdetailActivities.js
+++ app/packages/partup:client-widgets-partupdetail-activities/WidgetPartupdetailActivities.js
@@ -1,1 +1,8 @@
 // Write your package code here!
+
+Template.WidgetPartupdetailActivities.events({
+    'click [data-newactivity]': function openNewActivityPopup(event, template){
+        Partup.ui.popup.open();
+        console.log("opening popup")
+    }
+})
