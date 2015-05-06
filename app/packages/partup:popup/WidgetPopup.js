2ea48ea284519f3aca7c2ea78f0ae6545a3866ee
Léon Smit <leonsmit@Leons-MacBook-Pro.local>
2015-05-06 10:32:18 +0200

    popup logic moved to package

--- app/packages/partup:popup/WidgetPopup.js
+++ app/packages/partup:popup/WidgetPopup.js
@@ -1,5 +1,18 @@
+Template.WidgetPopup.helpers({
+    popupOpen: function(){
+        return Session.get('main.popup-open');
+    }
+});
+
 Template.WidgetPopup.events({
     'click [data-close]': function closePopup(event, template){
         Partup.ui.popup.close();
+    },
+    'click [data-close-popup]': function closePopup(event, template){
+        var overlay = $('[data-close-popup]'),
+            clickedElement = $(event.target);
+
+        // close popup if overlay is clicked
+        if(clickedElement[0] === overlay[0]) Session.set('main.popup-open', false);
     }
 });
