4cac9e67d3a6a95938a322c48d6246133aa617e9
Chiel Kunkels <chiel@kunkels.me>
2015-05-06 09:53:28 +0200

    pu detail: add scrolling columns behaviour

--- app/client/templates/partials/partup-detail-sidebar/PartupDetailSidebar.js
+++ app/client/templates/partials/partup-detail-sidebar/PartupDetailSidebar.js
@@ -1,27 +1,150 @@
+var getScrollTop = function(){
+    return (window.pageYOffset !== undefined) ? window.pageYOffset : (document.documentElement || document.body.parentNode || document.body).scrollTop;
+};
+
+var partupDetailLayout = {
+
+    init: function(){
+        this.container = document.querySelector('.pu-sub-pagecontainer');
+        if (!this.container) return;
+
+        this.left = this.container.querySelector('.pu-sub-partupdetail-left');
+        this.right = this.container.querySelector('.pu-sub-partupdetail-right');
+        if (!this.left || !this.right) return;
+
+        this.initialRect = this.getRects();
+        this.setContainerHeight();
+        this.preScroll(); // actually needs to be called ONCE on scroll start
+        this.checkScroll();
+        this.setEvents();
+    },
+
+    setEvents: function(){
+        var self = this;
+        window.addEventListener('resize', function(){
+            self.setContainerHeight();
+            self.preScroll();
+        });
+
+        // fake onscrollstart
+        window.addEventListener('scroll', mout.function.debounce(function(){
+            self.setContainerHeight();
+            self.preScroll();
+            self.scrolling = true;
+            self.checkInterval();
+        }, 100, true));
+
+        // fake onscrollend
+        window.addEventListener('scroll', mout.function.debounce(function(){
+            self.scrolling = false;
+        }, 100));
+    },
+
+    getRects: function(){
+        var lr = this.left.getBoundingClientRect();
+        var rr = this.right.getBoundingClientRect();
+
+        if (!lr.width) lr.width = lr.right - lr.left;
+        if (!lr.height) lr.height = lr.bottom - lr.top;
+        if (!rr.width) rr.width = rr.right - rr.left;
+        if (!rr.height) rr.height = rr.bottom - rr.top;
+
+        return { left: lr, right: rr };
+    },
+
+    setContainerHeight: function(){
+        var r = this.getRects();
+        this.container.style.height = Math.max(r.left.height, r.right.height) + 'px';
+    },
+
+    checkInterval: function(){
+        var self = this;
+        requestAnimationFrame(function(){
+            self.checkScroll();
+            if (self.scrolling) self.checkInterval();
+        });
+    },
+
+    preScroll: function(){
+        var r = this.getRects();
+        var br = document.body.getBoundingClientRect();
+        var scol, lcol;
+
+        if (r.left.height > r.right.height){
+            scol = 'right';
+            lcol = 'left';
+        } else {
+            scol = 'left';
+            lcol = 'right';
+        }
+
+        this.lastScrollTop = getScrollTop();
+        this.constrainScroll = [
+            0,
+            r[lcol].bottom - br.top - window.innerHeight
+        ];
+        this.constrainPos = [
+            this.initialRect[scol].top,
+            r[lcol].bottom - br.top - r[scol].height
+        ];
+
+        if (r[scol].height < window.innerHeight - r[scol].top){
+            this.constrainScroll[1] += (window.innerHeight - r[scol].height - r[scol].top);
+        }
+    },
+
+    checkScroll: function(){
+        var scrollTop = getScrollTop();
+        var r = this.getRects();
+        var br = document.body.getBoundingClientRect();
+        var iH = window.innerHeight;
+        var direction = scrollTop > this.lastScrollTop ? 'down' : 'up';
+        var top, pos, scol, lcol;
+
+        if (r.left.height > r.right.height){
+            scol = 'right';
+            lcol = 'left';
+        } else {
+            scol = 'left';
+            lcol = 'right';
+        }
+
+        if (direction === this.lastDirection){
+            if (direction === 'down' && r[scol].bottom - iH < 0){
+                pos = 'fixed';
+                if (r[scol].height < (iH - this.initialRect[scol].top)){
+                    top = this.initialRect[scol].top;
+                } else {
+                    top = iH - r[scol].height;
+                }
+            } else if (direction === 'up' && r[scol].top > this.initialRect[scol].top){
+                pos = 'fixed';
+                top = this.initialRect[scol].top;
+            }
+        } else {
+            pos = 'absolute';
+            top = r[scol].top - br.top;
+        }
+
+        if (scrollTop >= this.constrainScroll[1]){
+            pos = 'absolute';
+            top = this.constrainPos[1];
+        }
+
+        this[scol].style.position = pos;
+        this[scol].style.top = top + 'px';
+
+        this.lastDirection = direction;
+        this.lastScrollTop = scrollTop;
+    }
+};
+
+
 /*************************************************************/
 /* Partial rendered */
 /*************************************************************/
 Template.PartialsPartupDetailSidebar.rendered = function () {
-    // Offset to improve window resizing behaviour
-    var OFFSET = 100;
-
-    // Find page element
-    var pageElm = $('.pu-layout > .pu-sub-pagecontainer');
-    if (!pageElm) return;
-
-    // Find left side element
-    var rightElm = $('> .pu-sub-partupdetail-right', pageElm);
-    if (!rightElm) return;
-
-    // Calculate navigation background width
-    var calculateBackgroundWidth = function calculateBackgroundWidth() {
-        var backgroundWidth = (window.innerWidth - pageElm.width()) / 2 + rightElm.width() + OFFSET;
-        Session.set('partials.partup-detail-sidebar.background-width', backgroundWidth);
-    };
-
-    // Trigger calculations
-    window.addEventListener('resize', calculateBackgroundWidth);
-    calculateBackgroundWidth();
+    partupDetailLayout.init();
 };
 
 
