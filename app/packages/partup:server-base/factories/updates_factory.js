c80b305db8748c222918627ed460e04a5b109ecf
Bryan te Beek <bryantebeek@gmail.com>
2015-05-07 15:35:35 +0200

    Clean up update factory code

--- app/packages/partup:server-base/factories/updates_factory.js
+++ app/packages/partup:server-base/factories/updates_factory.js
@@ -1,6 +1,6 @@
 /**
  @namespace Partup updates factory
- @name Partup.factories.location
+ @name Partup.factories.updatesFactory
  @memberOf partup.factories
  */
 Partup.factories.updatesFactory = {
@@ -10,23 +10,23 @@ Partup.factories.updatesFactory = {
      *
      * @param  {int} userId
      * @param  {int} partupId
-     * @param  {string} type
-     * @param  {mixed} typeData
+     * @param  {string} updateType
+     * @param  {mixed} updateTypeData
      *
      * @return {Update}
      */
-    make: function(userId, partupId, type, typeData) {
+    make: function(userId, partupId, updateType, updateTypeData) {
         var update = { };
 
         if (! userId) throw new Meteor.Error(500, 'Required argument [userId] is missing for method [Partup.factories.updatesFactory::make]');
         if (! partupId) throw new Meteor.Error(500, 'Required argument [partupId] is missing for method [Partup.factories.updatesFactory::make]');
-        if (! type) throw new Meteor.Error(500, 'Required argument [type] is missing for method [Partup.factories.updatesFactory::make]');
-        if (! typeData) throw new Meteor.Error(500, 'Required argument [typeData] is missing for method [Partup.factories.updatesFactory::make]');
+        if (! updateType) throw new Meteor.Error(500, 'Required argument [updateType] is missing for method [Partup.factories.updatesFactory::make]');
+        if (! updateTypeData) throw new Meteor.Error(500, 'Required argument [updateTypeData] is missing for method [Partup.factories.updatesFactory::make]');
 
         update.upper_id = userId;
         update.partup_id = partupId;
-        update.type = type;
-        update.type_data = typeData;
+        update.type = updateType;
+        update.type_data = updateTypeData;
         update.created_at = new Date();
         update.updated_at = new Date();
 
