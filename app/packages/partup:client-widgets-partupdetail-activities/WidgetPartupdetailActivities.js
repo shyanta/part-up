6e3ecbe13859fb79f5478470bbfb4b2b4b94a137
Bryan te Beek <bryantebeek@gmail.com>
2015-05-07 14:56:28 +0200

    Render the activities in de partup detail activities overview

--- app/packages/partup:client-widgets-partupdetail-activities/WidgetPartupdetailActivities.js
+++ app/packages/partup:client-widgets-partupdetail-activities/WidgetPartupdetailActivities.js
@@ -1,5 +1,26 @@
-// Write your package code here!
+/*************************************************************/
+/* Widget functions */
+/*************************************************************/
+var getActivities = function getActivities () {
+    var partupId = Router.current().params._id;
 
+    return Activities.find({ partup_id: partupId }, { sort: { updated_at: -1 } });
+};
+
+/*************************************************************/
+/* Widget helpers */
+/*************************************************************/
+Template.WidgetPartupdetailActivities.helpers({
+
+    'activities': function helperActivities () {
+        return getActivities();
+    }
+
+});
+
+/*************************************************************/
+/* Widget events */
+/*************************************************************/
 Template.WidgetPartupdetailActivities.events({
     'click [data-newactivity]': function openNewActivityPopup(event, template){
         Partup.ui.popup.open();
