a98ee4e6e9bafc1bf0933ffe4a16e74497cf87e9
Nick Koster <nick.koster@lifely.nl>
2015-05-07 11:21:01 +0200

    Added update message method

--- app/packages/partup:server-base/methods/updates/updates_messages_methods.js
+++ app/packages/partup:server-base/methods/updates/updates_messages_methods.js
@@ -24,4 +24,47 @@ Meteor.methods({
         }
     },
 
+    /**
+     * Update a Message
+     *
+     * @param {string} updateId
+     * @param {mixed[]} fields
+     */
+    'updates.messages.update': function (updateId, fields) {
+        var upper = Meteor.user();
+        if (! upper) throw new Meteor.Error(401, 'Unauthorized.');
+
+
+        message.type = 'partups_message_updated';
+        message.type_data = {
+            old_value: message.new_value,
+            new_value: fields.content
+        };
+
+        try {
+            var update = Updates.findOneOrFail(updateId);
+
+            Updates.update({
+                    _id: update._id
+                },
+                {
+                    $set: {
+                        type: 'partups_message_updated',
+                        type_data: {
+                            old_value: update.new_value,
+                            new_value: fields.content
+                        }
+                    }
+                }
+            );
+
+            return {
+                _id: update._id
+            }
+        } catch (error) {
+            Log.error(error);
+            throw new Meteor.Error(400, 'error-method-updates-messages-insert-failure');
+        }
+    }
+
 });
