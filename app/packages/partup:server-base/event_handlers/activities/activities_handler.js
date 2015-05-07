a0598765eb14d7af9d81b7a40296b18f5b74cff8
Bryan te Beek <bryantebeek@gmail.com>
2015-05-07 11:21:26 +0200

    Update the activity update when an activity is updated

--- app/packages/partup:server-base/event_handlers/activities/activities_handler.js
+++ app/packages/partup:server-base/event_handlers/activities/activities_handler.js
@@ -8,9 +8,23 @@ Event.on('partups.activities.inserted', function (userId, activity) {
         type_data: {
             activity_id: activity._id
         },
-        created_at: new Date()
+        created_at: new Date(),
+        updated_at: new Date()
     };
 
     var updateId = Updates.insert(update);
     Activities.update({ _id: activity._id }, { $set: { update_id: updateId }});
+});
+
+Event.on('partups.activities.updated', function (userId, activity, oldActivity) {
+    if (! userId) return;
+    if (! oldActivity.update_id) return;
+
+    var set = {
+        upper_id: userId,
+        type: 'partups_activities_changed',
+        updated_at: new Date(),
+    };
+
+    Updates.update({ _id: activity.update_id }, { $set: set });
 });
