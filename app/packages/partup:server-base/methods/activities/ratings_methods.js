2693dff999821611fe8cf9845387504d8960e674
Nick Koster <nick.koster@lifely.nl>
2015-05-11 18:08:32 +0200

    Added rating upsert method (work in progress)

---
+++ app/packages/partup:server-base/methods/activities/ratings_methods.js
@@ -0,0 +1,52 @@
+Meteor.methods({
+
+    /**
+     * Insert or update a rating for a specific contribution
+     *
+     * @param {string} contributionId
+     * @param {mixed[]} fields
+     */
+    'contribution.rating.upsert': function (contributionId, fields) {
+        var upper = Meteor.user();
+        var contribution = Contributions.findOne(contributionId);
+        var rating = Ratings.findOne(ratingId);
+        var isUpperInPartup = Partups.findOne({ _id: contribution.partup_id, uppers: { $in: [upper._id] } }) ? true : false;
+
+        if (!upper || !isUpperInPartup) throw new Meteor.Error(401, 'Unauthorized.');
+
+        check(fields, Partup.schemas.forms.rating);
+
+        try {
+            var newRating = {};
+
+            if (rating) {
+                // Update rating
+                newRating = fields;
+                newRating.updated_at = new Date();
+
+                Ratings.update(rating, { $set: newRating });
+            } else {
+                // Insert rating
+                newRating = {
+                    _id: Random.id(),
+                    created_at: new Date(),
+                    //partup_id & activity_id
+                    contribution_id: contributionId,
+                    rating: fields.rating,
+                    feedback: fields.feedback,
+                    upper_id: fields.upper_id
+                };
+
+                newRating = Ratings.insert(newRating);
+                Contributions.insert(contributionId, { $push: { 'ratings': newRating._id } });
+            }
+
+            return newRating;
+
+        } catch (error) {
+            Log.error(error);
+            throw new Meteor.Error(400, 'Contribution could not be updated.');
+        }
+    }
+
+});
