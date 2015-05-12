dbceffbb3979718640e552fad6f56eb3b7b77094
Nick Koster <nick.koster@lifely.nl>
2015-05-12 11:52:44 +0200

    Updated contribution transformers

--- app/packages/partup:lib/transformers/contributions.js
+++ app/packages/partup:lib/transformers/contributions.js
@@ -12,15 +12,7 @@ Partup.transformers.contribution = {
      * @param {object} contribution
      */
     'toFormContribution': function (contribution) {
-        if(!contribution) return undefined;
-        return contributionValues = {
-            _id: contribution._id,
-
-            types_want_enabled: contribution.types.want.enabled,
-            types_can_amount: contribution.types.can.amount,
-            types_have_amount: contribution.types.have.amount,
-            types_have_description: contribution.types.have.description
-        };
+        return contribution ? contribution : undefined;
     },
 
     /**
@@ -30,26 +22,9 @@ Partup.transformers.contribution = {
      * @param {mixed[]} fields
      */
     'fromFormContribution': function (fields) {
-        var contribution = {
-            types: {}
+        return {
+            hours: fields.hours,
+            rate: fields.rate
         };
-
-        // create "want" field
-        contribution.types.want = {
-            enabled: fields.types_want_enabled
-        };
-        
-        // create "can" field
-        contribution.types.can = {
-            amount: fields.types_can_amount
-        };
-
-        // create "have" field
-        contribution.types.have = {
-            amount: fields.types_have_amount,
-            description: fields.types_have_description
-        };
-
-        return contribution;
     }
 };
