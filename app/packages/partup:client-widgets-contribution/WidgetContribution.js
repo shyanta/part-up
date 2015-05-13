a1a4bdcce40344eb31ca06d258d8660dfe0230cc
Nick Koster <nick.koster@lifely.nl>
2015-05-13 16:58:58 +0200

    Remove contribution method

--- app/packages/partup:client-widgets-contribution/WidgetContribution.js
+++ app/packages/partup:client-widgets-contribution/WidgetContribution.js
@@ -53,7 +53,7 @@ Template.WidgetContribution.events({
         template.showForm.set(true);
     },
     'click [data-contribution-remove]': function(event, template){
-        Meteor.call('activity.contribution.remove', template.data.contribution._id, function(){
+        Meteor.call('contributions.remove', template.data.contribution._id, function(){
             if (error){
                 console.error(err);
             }
