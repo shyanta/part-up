86214361d796528ee8c81d18aa978ffd4e436290
Léon Smit <leonsmit@Leons-MacBook-Pro.local>
2015-05-15 16:35:09 +0200

    moved comment code to its own package

--- app/packages/partup:client-widgets-partupdetail-activities-detail/WidgetPartupdetailActivitiesDetail.js
+++ app/packages/partup:client-widgets-partupdetail-activities-detail/WidgetPartupdetailActivitiesDetail.js
@@ -1,1 +1,10 @@
 // Write your package code here!
+Template.WidgetPartupdetailActivitiesDetail.helpers({
+    activityUpdates: function(){
+        if(!this.activity)
+            return;
+        if(this.activity.update_id) {
+            return Updates.findOne({ _id: this.activity.update_id });
+        }
+    }
+})
--- app/packages/partup:client-widgets-partupdetail-update-item/WidgetPartupdetailUpdateItem.js
+++ app/packages/partup:client-widgets-partupdetail-update-item/WidgetPartupdetailUpdateItem.js
@@ -1,29 +1,4 @@
 /*************************************************************/
-/* Widget constants */
-/*************************************************************/
-var MAX_COLLAPSED_COMMENTS = 2;
-
-/*************************************************************/
-/* Widget reactives */
-/*************************************************************/
-var commentsExpandedDict = new ReactiveDict;
-var commentInputFieldExpandedDict = new ReactiveDict;
-var commentPostButtonActiveDict = new ReactiveDict;
-
-
-/*************************************************************/
-/* Widget rendered */
-/*************************************************************/
-Template.WidgetPartupdetailUpdateItem.onRendered(function () {
-    var update = this.data.update;
-    var comments = update.comments || [];
-    commentsExpandedDict.set(update._id, false);
-    commentInputFieldExpandedDict.set(update._id, comments.length > 0);
-    commentPostButtonActiveDict.set(update._id, false);
-});
-
-
-/*************************************************************/
 /* Widget helpers */
 /*************************************************************/
 Template.WidgetPartupdetailUpdateItem.helpers({
@@ -37,42 +12,6 @@ Template.WidgetPartupdetailUpdateItem.helpers({
     },
     'titleKey': function helperTitleKey() {
         return 'partupdetail-update-item-type-' + this.update.type + '-title';
-    },
-
-    'generateFormId': function () {
-        return 'commentForm-' + this.update._id;
-    },
-
-    'formSchema': Partup.schemas.forms.updateComment,
-
-    // 'commentInputFieldExpanded': function helperCommentInputFieldExpanded() {
-    //     return commentInputFieldExpandedDict.get(this.update._id) ? 'pu-state-expanded' : '';
-    // },
-
-    'commentPostButtonActive': function helperCommentPostButtonActive() {
-        return commentPostButtonActiveDict.get(this.update._id);
-    },
-
-    'shownComments': function helperShownComments() {
-        var allComments = this.update.comments || [];
-        var commentsExpanded = commentsExpandedDict.get(this.update._id);
-        if (commentsExpanded || Template.instance().data.expandedComments)
-            return allComments;
-        else
-            return allComments.slice(-MAX_COLLAPSED_COMMENTS);
-    },
-
-    'showExpandButton': function helperShowExpandButton() {
-        var hiddenComments = 0;
-        if(this.update && this.update.comments && this.update.comments.length) {
-            hiddenComments = this.update.comments.length - MAX_COLLAPSED_COMMENTS > 0;
-        }
-        var commentsExpanded = commentsExpandedDict.get(this.update._id);
-        return Template.instance().data.expandedComments ? false : hiddenComments && !commentsExpanded;
-    },
-
-    'currentUser': function helperUser() {
-        return Meteor.user();
     },
 
     'updateUpper': function getUpdateUpper() {
@@ -92,55 +31,3 @@ Template.WidgetPartupdetailUpdateItem.helpers({
     }
 
 });
-
-
-/*************************************************************/
-/* Widget events */
-/*************************************************************/
-Template.WidgetPartupdetailUpdateItem.events({
-
-    'click [data-expand-comment-field]': function eventClickExpandCommentField(event, template) {
-        commentInputFieldExpandedDict.set(template.data.update._id, true);
-
-        // focus on input
-        var input = template.find('[data="commentfield"]');
-        Meteor.defer(function(){
-            $(input).focus();
-        });
-    },
-
-    'click [data-expand-comments]': function eventClickExpandComments(event, template) {
-        commentsExpandedDict.set(template.data.update._id, true);
-    },
-
-    'input [data=commentfield]': function eventChangeCommentField(event, template) {
-        var hasValue = event.currentTarget.value ? true : false;
-        commentPostButtonActiveDict.set(template.data.update._id, hasValue);
-    }
-
-});
-
-AutoForm.addHooks(
-    null, {
-        onSubmit: function (insertDoc) {
-            var self = this;
-            self.event.preventDefault();
-
-            var formNameParts = self.formId.split('-');
-            if (formNameParts.length !== 2 || formNameParts[0] !== 'commentForm') return;
-            var updateId = formNameParts[1];
-
-            Meteor.call('updates.comments.insert', updateId, insertDoc, function (error, result) {
-                if (error) {
-                    return Partup.ui.notify.iError('error-method-' + error.reason);
-                } else {
-                    commentPostButtonActiveDict.set(updateId, false);
-                    AutoForm.resetForm(self.formId);
-                }
-            });
-
-            self.done();
-            return false;
-        }
-    }
-);
