dcfd27cc3f769da7a5e361862bdd640951947223
Léon Smit <leonsmit@Leons-MacBook-Pro.local>
2015-05-15 13:45:56 +0200

    activity data helper

--- app/packages/partup:client-widgets-partupdetail-update-item/WidgetPartupdetailUpdateItem.js
+++ app/packages/partup:client-widgets-partupdetail-update-item/WidgetPartupdetailUpdateItem.js
@@ -30,6 +30,11 @@ Template.WidgetPartupdetailUpdateItem.helpers({
     'partupId': function(){
         return Router.current().params._id;
     },
+    'activityData': function(){
+        var activityId = Template.instance().data.update.type_data.activity_id;
+        // debugger;
+        return Activities.findOne({_id: activityId});
+    },
     'titleKey': function helperTitleKey() {
         return 'partupdetail-update-item-type-' + this.update.type + '-title';
     },
