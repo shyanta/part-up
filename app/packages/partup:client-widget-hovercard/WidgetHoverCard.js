4eb5707c96c6d35832bbf1b456aff28b556151da
Léon Smit <leonsmit@Leons-MacBook-Pro.local>
2015-05-19 17:37:11 +0200

    rename

--- app/packages/partup:client-widget-hovercard/WidgetHoverCard.js
+++ app/packages/partup:client-widget-hovercard/WidgetHoverCard.js
@@ -1,14 +1,17 @@
-Template.WidgetProfileHoverCard.helpers({
+Template.WidgetHoverCard.helpers({
     cardOpen: function(){
+        // if there are settings, that means the card must be visible
         return Session.get('partup.hover-card.settings') ? true : false;
     },
     settings: function(){
-        var template = Template.instance();
+        // get hovercard settings
         var settings = Session.get('partup.hover-card.settings');
         if(!settings) return {};
+
         // check if it's below the middle of the screen
         var positionTop = (100 / settings.windowHeight * settings.y) > 50 ? true : false;
 
+        // calculate top and left styling
         return {
             top: function(){
                 if(positionTop){
