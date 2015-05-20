9f2f892b562697a0e2b11cb4fbf4bafefcd29c28
Léon Smit <leonsmit@Leons-MacBook-Pro.local>
2015-05-20 17:28:58 +0200

    invite upper schema

---
+++ app/packages/partup:lib/schemas/inviteUpper.js
@@ -0,0 +1,19 @@
+/**
+ * New message Form
+ * @name inviteUpper
+ * @memberOf partup.schemas.forms
+ */
+Partup.schemas.forms.inviteUpper = new SimpleSchema({
+    name: {
+        type: String
+    },
+    email: {
+        type: String,
+        max: 255,
+        regEx: SimpleSchema.RegEx.Email
+    },
+    // message: {
+    //     type: String,
+    //     max: 250
+    // },
+});
