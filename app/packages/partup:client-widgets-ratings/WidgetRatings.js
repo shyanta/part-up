09e86152faff8e48a0b147c9457b91db0003cba8
Chiel Kunkels <chiel@kunkels.me>
2015-05-26 10:27:00 +0200

    widget-ratings: add icon for new rating

--- app/packages/partup:client-widgets-ratings/WidgetRatings.js
+++ app/packages/partup:client-widgets-ratings/WidgetRatings.js
@@ -1,3 +1,9 @@
+/**
+ * A widget that will render all given ratings
+ *
+ * @param {Cursor} contribution   The contribution which is being rated
+ */
+
 /*************************************************************/
 /* Widget initial */
 /*************************************************************/
@@ -8,6 +14,15 @@ Template.WidgetRatings.onCreated(function(){
 /* Widget helpers */
 /*************************************************************/
 Template.WidgetRatings.helpers({
+    canRate: function(){
+        var user = Meteor.user();
+        if (!user) return false;
+
+        var partup = Partups.findOne({_id: this.contribution.partup_id});
+        if (!partup) return false;
+
+        return mout.array.contains(partup.uppers, user._id);
+    }
 });
 
 /*************************************************************/
