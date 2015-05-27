972678291b4915914cdebdffb96dda8b4a3331b6
Nick Koster <nick.koster@lifely.nl>
2015-05-27 12:10:54 +0200

    Added AutoForm to copy form

--- app/packages/partup:client-widgets-startactivities/CopyActivityPopup.js
+++ app/packages/partup:client-widgets-startactivities/CopyActivityPopup.js
@@ -1,25 +1,44 @@
-Template.CopyActivityPopup.onRendered(function() {
+Template.CopyActivityPopup.onRendered(function () {
     Meteor.typeahead.inject();
 });
-
 
 /*************************************************************/
 /* Widget helpers */
 /*************************************************************/
 Template.CopyActivityPopup.helpers({
+    formSchema: Partup.schemas.forms.copyActivities,
+    placeholders: Partup.services.placeholders.startactivities,
+    generateFormId: function () {
+        return 'activityCopyForm-' + Router.current().params._id;
+    },
     partups: function () {
         return Partups.find({}).map(function (partup) {
-            console.log(partup);
             return { id: partup._id, value: partup.name };
         });
     }
 });
 
 /*************************************************************/
-/* Widget events */
+/* Widget form hooks */
 /*************************************************************/
-Template.CopyActivityPopup.events({
-    'click [data-copyactivities]': function clickCopyActivities(event, template) {
-        Meteor.call('activities.copy', template.find('[name=partup]').value, Router.current().params._id);
+AutoForm.addHooks(null, {
+    onSubmit: function (doc) {
+        var partupId = Router.current().params._id;
+        var self = this;
+
+        Meteor.call('activities.copy', doc, partupId, function (error) {
+            if (error && error.message) {
+                Partup.ui.notify.error(error.reason);
+
+                AutoForm.validateForm(self.formId);
+                self.done(new Error(error.message));
+                return;
+            }
+
+            Partup.ui.popup.close();
+            self.done();
+        });
+
+        return false;
     }
 });
