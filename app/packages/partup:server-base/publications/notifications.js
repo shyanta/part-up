b6698ca57c7d9ee89c0a5db4eb78570103b8b901
Bryan te Beek <bryantebeek@gmail.com>
2015-05-28 14:13:16 +0200

    Publish images with new partups supporters notifications

--- app/packages/partup:server-base/publications/notifications.js
+++ app/packages/partup:server-base/publications/notifications.js
@@ -1,3 +1,22 @@
-Meteor.publish('notifications.user', function () {
-    return Notifications.find({ for_upper_id: this.userId }, { limit: 20 });
+Meteor.publishComposite('notifications.user', function() {
+    var self = this;
+
+    return {
+        find: function() {
+            return Notifications.find({ for_upper_id: self.userId }, { limit: 20 });
+        },
+        children: [
+            {
+                find: function(notification) {
+                    var images = [];
+
+                    if (notification.type === 'partups_supporters_added') {
+                        images.push(type_data.supporter.image);
+                    }
+
+                    return Images.find({ _id: { $in: images } });
+                }
+            }
+        ]
+    };
 });
