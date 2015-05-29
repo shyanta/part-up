3b5c272a27c36aaae89434f3b8e8cd5bf740b737
Bryan te Beek <bryantebeek@gmail.com>
2015-05-29 12:07:15 +0200

    Add an update for the partup creation

--- app/packages/partup:server-base/event_handlers/partups/partups_handler.js
+++ app/packages/partup:server-base/event_handlers/partups/partups_handler.js
@@ -1,5 +1,11 @@
 Event.on('partups.inserted', function (userId, partup) {
     Log.debug('Partup [' + partup._id + '] inserted.');
+
+    if (! userId) return;
+
+    var update = Partup.factories.updatesFactory.make(userId, partup._id, 'partups_created', {});
+
+    Updates.insert(update);
 });
 
 Event.on('partups.updated', function (userId, partup, fields) {
