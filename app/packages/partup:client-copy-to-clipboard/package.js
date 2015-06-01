36dc8af071b179f90197b856fa6476cd9542b19b
Chiel Kunkels <chiel@kunkels.me>
2015-06-01 16:21:48 +0200

    rename: copy-to-clipboard to client-copy-to-clipboard

--- app/packages/partup:client-copy-to-clipboard/package.js
+++ app/packages/partup:client-copy-to-clipboard/package.js
@@ -1,5 +1,5 @@
 Package.describe({
-    name: 'partup:copy-to-clipboard',
+    name: 'partup:client-copy-to-clipboard',
     version: '0.0.1',
     summary: "jQuery Clipboard plugin packaged for meteor. Official repo: https://github.com/pix/meteor-jquery-clipboard.git, repackaged by Lifely for newer versions of Meteor",
     documentation: null
