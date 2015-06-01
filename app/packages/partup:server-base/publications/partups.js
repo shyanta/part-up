15c5ea2bd80864796f572d55285fe11bc716973f
Nick Koster <nick.koster@lifely.nl>
2015-06-01 17:36:17 +0200

    Added search publication for Partups to allow full text search

--- app/packages/partup:server-base/publications/partups.js
+++ app/packages/partup:server-base/publications/partups.js
@@ -14,6 +14,36 @@ Meteor.publish('partups.list', function () {
     return Partups.find({}, { _id: 1, name: 1 });
 });
 
+Meteor.publish('partups.search', function (searchValue) {
+    if (!searchValue) {
+        return Partups.find({});
+    }
+
+    console.log('Searching for ', searchValue);
+
+    return Partups.find(
+        { $text: { $search: searchValue } },
+        {
+            /*
+             * `fields` is where we can add MongoDB projections. Here we're causing
+             * each document published to include a property named `score`, which
+             * contains the document's search rank, a numerical value, with more
+             * relevant documents having a higher score.
+             */
+            fields: {
+                score: { $meta: 'textScore' }
+            },
+            /*
+             * This indicates that we wish the publication to be sorted by the
+             * `score` property specified in the projection fields above.
+             */
+            sort: {
+                score: { $meta: 'textScore' }
+            }
+        }
+    );
+});
+
 Meteor.publishComposite('partups.one.activities', function (partupId) {
     return {
         find: function() {
