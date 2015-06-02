de816853b7a8911e26c44eba7bac5b50e1463aa0
Jesse de Vries <jessedvrs@gmail.com>
2015-06-02 09:44:11 +0200

    Modulized forgot & reset password modules

---
+++ app/packages/partup:client-forgotpassword/package.js
@@ -0,0 +1,35 @@
+Package.describe({
+    name: 'partup:client-forgotpassword',
+    version: '0.0.1',
+    summary: 'The forgot password module',
+    documentation: null
+});
+
+Package.onUse(function (api) {
+    api.use([
+        'tap:i18n'
+    ], ['client', 'server']);
+
+    api.use([
+        'templating',
+        'partup:lib',
+        'aldeed:autoform',
+        'reactive-var'
+    ], ['client']);
+
+    api.addFiles([
+        'package-tap.i18n',
+
+        'Forgotpassword.html',
+        'Forgotpassword.js',
+
+        'i18n/en.i18n.json',
+        'i18n/nl.i18n.json'
+    ], 'client');
+
+    api.addFiles([
+        'package-tap.i18n',
+        'i18n/en.i18n.json',
+        'i18n/nl.i18n.json'
+    ]);
+});
--- app/packages/partup:client-pages/modal/forgotpassword/forgotpassword.js
+++ app/packages/partup:client-pages/modal/forgotpassword/forgotpassword.js
@@ -1,26 +1,3 @@
-var placeholders = {
-    'email': function () {
-        return __('pages-modal-forgotpassword-form-email-placeholder');
-    }
-};
-
-/*************************************************************/
-/* Widget initial */
-/*************************************************************/
-var resetSentSuccessful = new ReactiveVar(false);
-
-/*************************************************************/
-/* Page helpers */
-/*************************************************************/
-Template.modal_forgotpassword.helpers({
-    formSchema: Partup.schemas.forms.forgotPassword,
-    placeholders: placeholders,
-    resetSentSuccessful: function() {
-        return resetSentSuccessful.get();
-    }
-});
-
-
 /*************************************************************/
 /* Page events */
 /*************************************************************/
@@ -28,40 +5,5 @@ Template.modal_forgotpassword.events({
     'click [data-closepage]': function (event, template) {
         event.preventDefault();
         Partup.ui.intent.executeIntentCallback('login');
-    }
-});
-
-/*************************************************************/
-/* Widget form hooks */
-/*************************************************************/
-AutoForm.hooks({
-    forgotPasswordForm: {
-        onSubmit: function(insertDoc, updateDoc, currentDoc) {
-            var self = this;
-
-            Accounts.forgotPassword({email: insertDoc.email}, function(error) {
-
-                // Error cases
-                if(error && error.message) {
-                    switch (error.message) {
-                        case 'User not found [403]':
-                            Partup.ui.forms.addStickyFieldError(self, 'email', 'emailNotFound');
-                            break;
-                        default:
-                            Partup.ui.notify.error(error.reason);
-                    }
-                    AutoForm.validateForm(self.formId);
-                    self.done(new Error(error.message));
-                    return false;
-                }
-
-                // Success
-                self.done();
-                resetSentSuccessful.set(true);
-
-            });
-
-            return false;
-        }
     }
 });
