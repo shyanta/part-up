4933b5a7ba170e8b1c72ba140b5693af12b90366
Bryan te Beek <bryantebeek@gmail.com>
2015-06-04 11:53:32 +0200

    Remove event handlers that debug partup information, use the any handler for that

--- app/packages/partup:server/event_handlers/updates/updates_handler.js
+++ app/packages/partup:server/event_handlers/updates/updates_handler.js
@@ -1,15 +1,3 @@
-Event.on('partups.updates.inserted', function(userId, update) {
-    Log.debug('Update [' + update._id + '] with Partup [' + update.partup_id + '] inserted.');
-});
-
-Event.on('partups.updates.updated', function(userId, update, fields) {
-    Log.debug('Update [' + update._id + '] with Partup [' + update.partup_id + '] updated.');
-});
-
-Event.on('partups.updates.removed', function(userId, update) {
-    Log.debug('Update [' + update._id + '] with Partup [' + update.partup_id + '] removed.');
-});
-
 /**
  * Generate a Notification for each upper in a Partup when a new message is posted.
  */
