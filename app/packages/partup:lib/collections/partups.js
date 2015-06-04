e58be44d56d0c4e25875fcd7e43bdb4937aef17e
Bryan te Beek <bryantebeek@gmail.com>
2015-06-04 13:06:53 +0200

    Add a Partup model with isEditableBy and isRemovableBy methods

--- app/packages/partup:lib/collections/partups.js
+++ app/packages/partup:lib/collections/partups.js
@@ -1,5 +1,26 @@
 /**
+ * Partup model
+ */
+Partup = function(document) {
+    _.extend(this, document);
+};
+
+Partup.prototype.isEditableBy = function(user) {
+    var uppers = this.uppers || [];
+
+    return user && uppers.indexOf(user._id) > -1;
+};
+
+Partup.prototype.isRemovableBy = function(user) {
+    return user && partup.creator_id === user._id;
+};
+
+/**
  @namespace Partups
  @name Partups
  */
-Partups = new Mongo.Collection('partups');
+Partups = new Mongo.Collection('partups', {
+    transform: function(document) {
+        return new Partup(document);
+    }
+});
