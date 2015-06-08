bfccd3a3d767223104a9ed51e6fdfc2351647ce9
Nick Koster <nick.koster@lifely.nl>
2015-06-08 15:07:18 +0200

    Calculate profile completeness percentage on profile update

--- app/packages/partup:server/methods/users/users_methods.js
+++ app/packages/partup:server/methods/users/users_methods.js
@@ -19,6 +19,13 @@ Meteor.methods({
 
             var newProfile = _.extend(upper.profile, profileFields);
 
+            // Calculate new registration percentage
+            var providedValues = 0;
+            _.each(newProfile, function(value, key) {
+                if (value !== undefined && value !== '' && value !== null) providedValues++;
+            });
+            newProfile.completeness = Math.round(providedValues / Object.keys(newProfile).length * 100);
+
             Meteor.users.update(upper._id, {$set:newProfile});
             Event.emit('users.updated', upper._id, newProfile);
 
