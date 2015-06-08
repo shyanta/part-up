d018dd581f76c6f6eb66bca2d1b1f9869e2eec79
Nick Koster <nick.koster@lifely.nl>
2015-06-08 15:06:30 +0200

    Reordered register schema and added completeness property

--- app/packages/partup:lib/schemas/register.js
+++ app/packages/partup:lib/schemas/register.js
@@ -4,20 +4,6 @@
  * @memberOf partup.schemas.forms
  */
 Partup.schemas.forms.registerRequired = new SimpleSchema({
-    name: {
-        type: String,
-        max: 255
-    },
-    email: {
-        type: String,
-        max: 255,
-        regEx: SimpleSchema.RegEx.Email
-    },
-    password: {
-        type: String,
-        max: 255,
-        regEx: Partup.services.validators.password
-    },
     confirmPassword: {
         type: String,
         custom: function() {
@@ -25,6 +11,20 @@ Partup.schemas.forms.registerRequired = new SimpleSchema({
                 return 'passwordMismatch';
             }
         }
+    },
+    email: {
+        type: String,
+        max: 255,
+        regEx: SimpleSchema.RegEx.Email
+    },
+    name: {
+        type: String,
+        max: 255
+    },
+    password: {
+        type: String,
+        max: 255,
+        regEx: Partup.services.validators.password
     }
     //network: {
     //    type: String,
@@ -38,26 +38,17 @@ Partup.schemas.forms.registerRequired = new SimpleSchema({
  * @memberOf partup.schemas.forms
  */
 Partup.schemas.forms.registerOptional = new SimpleSchema({
-    image: {
-        type: String,
-        max: 255,
-        optional: true
-    },
-    location_input: {
-        type: String,
-        max: 255,
-        optional: true
+    completeness: {
+        type: Number,
+        min: 0,
+        max: 100,
+        optional: true,
+        defaultValue: 0
     },
     description: {
         type: String,
         max: 650,
         optional: true
-    },
-    tags_input: {
-        type: String,
-        max: 255,
-        optional: true,
-        regEx: Partup.services.validators.tagsSeparatedByComma
     },
     facebook: {
         type: String,
@@ -65,11 +56,10 @@ Partup.schemas.forms.registerOptional = new SimpleSchema({
         optional: true,
         regEx: Partup.services.validators.facebookUsername
     },
-    twitter: {
+    image: {
         type: String,
         max: 255,
-        optional: true,
-        regEx: Partup.services.validators.twitterUsername
+        optional: true
     },
     instagram: {
         type: String,
@@ -83,11 +73,10 @@ Partup.schemas.forms.registerOptional = new SimpleSchema({
         optional: true,
         regEx: Partup.services.validators.linkedinUsername
     },
-    website: {
+    location_input: {
         type: String,
         max: 255,
-        optional: true,
-        regEx: Partup.services.validators.simpleSchemaUrlWithoutProtocol
+        optional: true
     },
     phonenumber: {
         type: String,
@@ -98,5 +87,23 @@ Partup.schemas.forms.registerOptional = new SimpleSchema({
         type: String,
         max: 255,
         optional: true
+    },
+    tags_input: {
+        type: String,
+        max: 255,
+        optional: true,
+        regEx: Partup.services.validators.tagsSeparatedByComma
+    },
+    twitter: {
+        type: String,
+        max: 255,
+        optional: true,
+        regEx: Partup.services.validators.twitterUsername
+    },
+    website: {
+        type: String,
+        max: 255,
+        optional: true,
+        regEx: Partup.services.validators.simpleSchemaUrlWithoutProtocol
     }
 });
