6536dabc24143b0e2ee307c8b0453919b047ce60
Chiel Kunkels <chiel@kunkels.me>
2015-06-09 13:16:08 +0200

    partup-tile: add styling for stats

--- app/packages/partup:client-partup-tile/package.js
+++ app/packages/partup:client-partup-tile/package.js
@@ -7,12 +7,21 @@ Package.describe({
 
 Package.onUse(function(api) {
     api.use([
+        'tap:i18n'
+    ], ['client', 'server']);
+
+    api.use([
         'templating',
         'partup:lib'
     ], 'client');
 
     api.addFiles([
+        'package-tap.i18n',
+
         'PartupTile.html',
-        'PartupTile.js'
+        'PartupTile.js',
+
+        'i18n/en.i18n.json',
+        'i18n/nl.i18n.json'
     ], 'client');
 });
