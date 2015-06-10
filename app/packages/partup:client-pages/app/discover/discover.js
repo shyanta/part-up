fbc0a470a8739741691121fe79d10f02e206ff72
Léon Smit <leonsmit@Leons-MacBook-Pro.local>
2015-06-10 16:21:14 +0200

    removed the temporary interval and set limit to 10

--- app/packages/partup:client-pages/app/discover/discover.js
+++ app/packages/partup:client-pages/app/discover/discover.js
@@ -1,4 +1,3 @@
-var limit = new ReactiveVar(20);
 Template.app_discover.onCreated(function() {
     var template = this;
     // template.limit = new ReactiveVar(10);
@@ -6,21 +5,15 @@ Template.app_discover.onCreated(function() {
 
 Template.app_discover.onRendered(function() {
     var template = this;
-    Meteor.setInterval(function() {
-        var l = limit.get();
-        l++;
-        limit.set(l);
-        // console.log(l);
-    }, 10000);
 });
 
 /*************************************************************/
 /* Page initial */
 /*************************************************************/
-Template.app_discover.onCreated(function() {
-    this.partups = Partups.find();
-    this.subscription = Meteor.subscribe('partups.all');
-});
+// Template.app_discover.onCreated(function() {
+//     this.partups = Partups.find();
+//     this.subscription = Meteor.subscribe('partups.all');
+// });
 
 /*************************************************************/
 /* Page helpers */
@@ -28,13 +21,10 @@ Template.app_discover.onCreated(function() {
 Template.app_discover.helpers({
     partups: function() {
         var self = this;
-        var l = limit.get();
-        console.log('run', limit.get());
         var partups = Partups.find({}, {
-            limit: l,
+            limit: 10,
             sort: {created_at: -1}
         }).fetch();
-        // console.log(partups);
         return partups;
     }
 });
