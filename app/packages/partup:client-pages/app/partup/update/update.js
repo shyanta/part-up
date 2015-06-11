6cd04e114f9f40464ccdcf44a4baca46522bd123
Jesse de Vries <jessedvrs@gmail.com>
2015-06-11 12:05:24 +0200

    Made an update retrieve its own data

--- app/packages/partup:client-pages/app/partup/update/update.js
+++ app/packages/partup:client-pages/app/partup/update/update.js
@@ -1,9 +1,5 @@
-// Write your package code here!
 Template.app_partup_update.helpers({
-    update: function() {
-        var update_id = Router.current().params.update_id;
-        if (update_id) {
-            return Updates.findOne({_id: Router.current().params.update_id});
-        }
+    update_id: function() {
+        return Router.current().params.update_id;
     }
 });
--- app/packages/partup:client-update/Update.js
+++ app/packages/partup:client-update/Update.js
@@ -1,47 +1,67 @@
 /*************************************************************/
 /* Widget onCreated */
 /*************************************************************/
-Template.Update.onCreated(function () {
-    this.commentInputFieldExpanded = new ReactiveVar(false);
+Template.Update.onCreated(function() {
+    var template = this;
+    template.commentInputFieldExpanded = new ReactiveVar(false);
 
-    var update = this.data.update;
-    if (update.type === 'partups_budget_changed') {
-        update.type_data.old_value = update.type_data.old_value || 0;
-        update.type_data.new_value = update.type_data.new_value || 0;
-    }
+    template.update = false;
+    template.autorun(function() {
+        var updates = Updates.find({_id: template.data.updateId});
+        var update = updates.fetch()[0];
+        template.update = update;
+    });
 });
 
 /*************************************************************/
 /* Widget helpers */
 /*************************************************************/
 Template.Update.helpers({
+    update: function helperUpdate() {
+        return Template.instance().update;
+    },
     partupId: function helperPartupId () {
         return Router.current().params._id;
     },
     activityData: function helperActivityData () {
-        var activityId = Template.instance().data.update.type_data.activity_id;
+        var update = Template.instance().update;
+        if (!update) return;
+
+        var activityId = update.type_data.activity_id;
         return Activities.findOne({_id: activityId});
     },
-    isActivityUpdate: function(){
-        return /^partups_activities/.test(this.update.type) ||
-            (this.update.type === 'partups_comments_added' && !this.update.type_data.contribution_id);
+    isActivityUpdate: function() {
+        var update = Template.instance().update;
+        if (!update) return;
+
+        return /^partups_activities/.test(update.type) ||
+            (update.type === 'partups_comments_added' && !update.type_data.contribution_id);
     },
-    isContributionUpdate: function(){
-        return /^partups_(contributions|ratings)/.test(this.update.type) ||
-            (this.update.type === 'partups_comments_added' && this.update.type_data.contribution_id);
+    isContributionUpdate: function() {
+        var update = Template.instance().update;
+        if (!update) return;
+
+        return /^partups_(contributions|ratings)/.test(update.type) ||
+            (update.type === 'partups_comments_added' && update.type_data.contribution_id);
     },
-    isDetail: function helperIsDetail (){
+    isDetail: function helperIsDetail () {
         return !!Router.current().params.update_id;
     },
-    isNotDetail: function helperIsDetail (){
+    isNotDetail: function helperIsDetail () {
         return !Router.current().params.update_id;
     },
     titleKey: function helperTitleKey() {
-        return 'partupdetail-update-item-type-' + this.update.type + '-title';
+        var update = Template.instance().update;
+        if (!update) return;
+
+        return 'partupdetail-update-item-type-' + update.type + '-title';
     },
 
     updateUpper: function helperUpdateUpper() {
-        var user = Meteor.users.findOne({_id: this.update.upper_id});
+        var update = Template.instance().update;
+        if (!update) return;
+
+        var user = Meteor.users.findOne({_id: update.upper_id});
 
         if (user.profile && user.profile.image) {
             user.profile.image = Images.findOne({_id: user.profile.image});
@@ -57,8 +77,11 @@ Template.Update.helpers({
     },
 
     commentInputFieldExpanded: function helperCommentInputFieldExpanded () {
-        var commentsPresent = this.update.comments && this.update.comments.length > 0;
-        var commentButtonPressed = Template.instance().commentInputFieldExpanded.get();
+        var template = Template.instance();
+        if (!template.update) return;
+
+        var commentsPresent = template.update.comments && template.update.comments.length > 0;
+        var commentButtonPressed = template.commentInputFieldExpanded.get();
         return commentsPresent || commentButtonPressed;
     },
 
