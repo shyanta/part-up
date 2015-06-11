8fa1b5fc07a075a2a6cfa1b533692ac800cd00f8
Jesse de Vries <jessedvrs@gmail.com>
2015-06-11 16:29:15 +0200

    Fixed update metadata content for updatedetail page

--- app/packages/partup:client-pages/app/partup/update/update.js
+++ app/packages/partup:client-pages/app/partup/update/update.js
@@ -1,5 +1,27 @@
 Template.app_partup_update.helpers({
     update_id: function() {
         return Router.current().params.update_id;
+    },
+    metaDataForUpdate: function() {
+        var update = Updates.findOne({_id: Router.current().params.update_id});
+        if (!update) return {};
+
+        var updateUpper = Meteor.users.findOne({_id: update.upper_id});
+
+        var path = '';
+        if (update.type === 'partups_newuser') {
+            path = Router.path('profile', {_id: update.upper_id});
+        } else {
+            path = Router.path('partup-update', {_id: update.partup_id, update_id: update._id});
+        }
+
+        return {
+            updateUpper: updateUpper,
+            updateUpperImage: Images.findOne({_id: updateUpper.profile.image}),
+            updated_at: update.updated_at,
+            path: path,
+            update_type: update.type,
+            invited_name: update.type_data.invited_name
+        };
     }
 });
