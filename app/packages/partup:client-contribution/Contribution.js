64575a7eef0607c41848fcf54496070dd88047ac
Peter Peerdeman <peter@lifely.nl>
2015-06-15 16:15:50 +0200

    fixed "FE: remove contribution comments links"

--- app/packages/partup:client-contribution/Contribution.js
+++ app/packages/partup:client-contribution/Contribution.js
@@ -10,11 +10,6 @@ Template.Contribution.onCreated(function() {
 /* Widget helpers */
 /*************************************************************/
 Template.Contribution.helpers({
-    commentsCount: function() {
-        var update = Updates.findOne({_id: this.contribution.update_id});
-        if (!update) return;
-        return update.comments_count;
-    },
     formSchema: Partup.schemas.forms.contribution,
     placeholders: Partup.services.placeholders.contribution,
     generateFormId: function() {
