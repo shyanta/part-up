385e89d7ad9331e3e8f5963fdade690be2f52a8a
Léon Smit <leonsmit@Leons-MacBook-Pro.local>
2015-06-17 16:23:46 +0200

    add class on render for transition

--- app/packages/partup:client-reactive-layout/ReactiveTile.js
+++ app/packages/partup:client-reactive-layout/ReactiveTile.js
@@ -1,7 +1,9 @@
 Template.ReactiveTile.onRendered(function() {
+    var template = this;
     if (typeof this.data.onRenderCallback === 'function') {
         this.data.onRenderCallback();
     }
+    $(template.find('li.pu-reactivetile')).addClass('pu-reactivetile-rendered');
 });
 Template.ReactiveTile.helpers({
     templateName: function() {
