723c40ea66870715f8c73448ea6c9a85c4fb42a2
Nick Koster <nick.koster@lifely.nl>
2015-06-18 18:31:11 +0200

    Privacy type determination in partup creation

--- app/packages/partup:server/methods/partups/partups_methods.js
+++ app/packages/partup:server/methods/partups/partups_methods.js
@@ -6,17 +6,35 @@ Meteor.methods({
      * @param {mixed[]} extraFields
      */
     'partups.insert': function(fields, extraFields) {
-
-        check(fields, Partup.schemas.forms.startPartup);
-
         var user = Meteor.user();
         if (!user) throw new Meteor.Error(401, 'Unauthorized.');
+
+        check(fields, Partup.schemas.forms.startPartup);
 
         try {
             var newPartup = Partup.transformers.partup.fromFormStartPartup(fields);
             newPartup.uppers = [user._id];
             newPartup.creator_id = user._id;
             newPartup.created_at = new Date();
+
+            if (fields.privacy_type === 'public') {
+                newPartup.privacy_type = Partup.PUBLIC;
+            } else if (fields.privacy_type === 'private') {
+                newPartup.privacy_type = Partup.PRIVATE;
+            } else if (fields.privacy_type) {
+                var network = Networks.findOneOrFail(fields.privacy_type);
+                switch (network.privacy_type) {
+                    case Networks.NETWORK_PUBLIC:
+                        newPartup.privacy_type = Partup.NETWORK_PUBLIC;
+                        break;
+                    case Networks.NETWORK_INVITE:
+                        newPartup.privacy_type = Partup.NETWORK_INVITE;
+                        break;
+                    case Networks.NETWORK_CLOSED:
+                        newPartup.privacy_type = Partup.NETWORK_CLOSED;
+                        break;
+                }
+            }
 
             //check(newPartup, Partup.schemas.entities.partup);
 
