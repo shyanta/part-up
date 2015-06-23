c28f861fe065aca0190c0bf55e2a6140c5f721a3
Nick Koster <nick.koster@lifely.nl>
2015-06-23 14:57:49 +0200

    Added event handler for network accept

--- app/packages/partup:server/event_handlers/networks/networks_handler.js
+++ app/packages/partup:server/event_handlers/networks/networks_handler.js
@@ -28,7 +28,32 @@ Event.on('networks.invited', function(user, networkId, upperId) {
     var notificationOptions = {
         type: 'partups_networks_invited',
         typeData: {
-            partup: {
+            network: {
+                name: network.name,
+                image: network.image
+            }
+        }
+    };
+
+    notificationOptions.userId = upperId;
+
+    Partup.server.services.notifications.send(notificationOptions, function(error) {
+        if (error) return Log.error(error);
+
+        Log.debug('Notification generated for User [' + upperId + '] with type [' + notificationOptions.type + '].');
+    });
+});
+
+/**
+ * Generate a notification for an upper when getting accepted for a network
+ */
+Event.on('networks.accepted', function(userId, networkId, upperId) {
+    var network = Networks.findOneOrFail(networkId);
+
+    var notificationOptions = {
+        type: 'partups_networks_accepted',
+        typeData: {
+            network: {
                 name: network.name,
                 image: network.image
             }
