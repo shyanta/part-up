77b06c1031175198e3f0a0447bb411530760ce13
Chiel Kunkels <chiel@kunkels.me>
2015-06-23 14:02:59 +0200

    network-uppers: add package network-settings-uppers

---
+++ app/packages/partup:client-network-settings-uppers/NetworkSettingsUppers.js
@@ -0,0 +1,10 @@
+/**
+ * Render a widget to view/edit a single network's uppers
+ *
+ * @param {Number} networkId
+ */
+Template.NetworkSettingsUppers.onCreated(function() {});
+
+Template.NetworkSettingsUppers.helpers({});
+
+Template.NetworkSettingsUppers.events({});
