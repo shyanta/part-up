da4e4a00d54e4d1425a8d8a2515ba3f011b4a2fd
Chiel Kunkels <chiel@kunkels.me>
2015-06-24 11:32:37 +0200

    partup-tile: ensure translations are loaded on server

--- app/packages/partup:client-partup-tile/package.js
+++ app/packages/partup:client-partup-tile/package.js
@@ -24,4 +24,10 @@ Package.onUse(function(api) {
         'i18n/en.i18n.json',
         'i18n/nl.i18n.json'
     ], 'client');
+
+    api.addFiles([
+        'package-tap.i18n',
+        'i18n/en.i18n.json',
+        'i18n/nl.i18n.json'
+    ]);
 });
