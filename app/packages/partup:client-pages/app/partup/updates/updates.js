44283e818c12e902914b38a1e1f521cbf639f1c9
Jesse de Vries <jessedvrs@gmail.com>
2015-06-24 09:24:25 +0200

    Increased starting_limit and increment in updates.js

--- app/packages/partup:client-pages/app/partup/updates/updates.js
+++ app/packages/partup:client-pages/app/partup/updates/updates.js
@@ -8,8 +8,8 @@ Template.app_partup_updates.onCreated(function() {
     tpl.updates = {
 
         // Constants
-        STARTING_LIMIT: 10,
-        INCREMENT: 10,
+        STARTING_LIMIT: 20,
+        INCREMENT: 20,
 
         // States
         loading: false,
