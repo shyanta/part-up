662286bd460fc5036a2607259aa729e9a20268dd
Chiel Kunkels <chiel@kunkels.me>
2015-07-01 10:44:30 +0200

    subscriptions: updates: add some docs/comments

--- app/packages/partup:server/publications/updates.js
+++ app/packages/partup:server/publications/updates.js
@@ -35,20 +35,40 @@ Meteor.publishComposite('updates.one', function(updateId) {
     };
 });
 
+/**
+ * Publish all required data for updates in a part-up
+ *
+ * This subscription will first check if the current user is allowed to view the
+ * requested part-up. If so, the client will be allowed access to:
+ *
+ * - The part-up
+ * - Updates, filtered by given options and related:
+ *   - Activities
+ *   - Contributions
+ *   - Images
+ *   - Additionally, any update type will also be extended with any type of
+ *     addtitional related information such as users and their profile pictures
+ *
+ * @param {String} partupId - The part-up's id
+ * @param {Object} options  - Possible filtering options for updates
+ */
 Meteor.publishComposite('updates.from_partup', function(partupId, options) {
     var self = this;
 
     return {
+        // Use guarded find to check if current user has access to the part-up
         find: function() {
             return Partups.guardedFind(self.userId, {_id: partupId}, {limit:1});
         },
         children: [
             {
+                // Find all updates, filtered by given options
                 find: function(partup) {
                     return Updates.findForUpdates(partup._id, options);
                 },
                 children: [
                     {
+                        // Find the user related to the update, along with their profile picture
                         find: function(update) {
                             return Meteor.users.findSinglePublicProfile(update.upper_id);
                         },
@@ -61,6 +81,7 @@ Meteor.publishComposite('updates.from_partup', function(partupId, options) {
                         ]
                     },
                     {
+                        // Find any images required for the update
                         find: function(update) {
                             var images = [];
 
@@ -76,6 +97,7 @@ Meteor.publishComposite('updates.from_partup', function(partupId, options) {
                         }
                     },
                     {
+                        // Find activity related to the update
                         find: function(update) {
                             if (update.isActivityUpdate()) {
                                 return Activities.find({_id: update.type_data.activity_id}, {limit: 1});
@@ -83,22 +105,27 @@ Meteor.publishComposite('updates.from_partup', function(partupId, options) {
                         }
                     },
                     {
+                        // Find contribution related to the update
                         find: function(update) {
                             if (update.isContributionUpdate()) {
                                 return Contributions.find({_id: update.type_data.contribution_id}, {limit: 1});
                             }
                         },
                         children: [
+                            // Backtrack and find the contribution's activity as well
                             {
                                 find: function(contribution) {
                                     return Activities.find({_id: contribution.activity_id}, {limit: 1});
                                 }
                             },
+
+                            // Find ratings associated with the contribution
                             {
                                 find: function(contribution) {
                                     return Ratings.find({contribution_id: contribution._id});
                                 },
                                 children: [
+                                    // Find user associated with the rating, and their profile picture
                                     {
                                         find: function(rating) {
                                             return Meteor.users.findSinglePublicProfile(rating.upper_id);
