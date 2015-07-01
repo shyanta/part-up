6a7028b48d8d3fdfa780b9b1ea2c8a900e47c9f2
Chiel Kunkels <chiel@kunkels.me>
2015-07-01 13:35:07 +0200

    publications: activities: refactor
    - move activiyChildren to a separate array for easy re-use
    - don't expose the partup as part of this publication

--- app/packages/partup:server/publications/activities.js
+++ app/packages/partup:server/publications/activities.js
@@ -1,80 +1,79 @@
 /**
- * Publish all required data for activities in a part-up
+ * Children of an activity
  *
- * This subscription will first check if the current user is allowed to view the
- * requested part-up. If so, the client will be allowed access to:
+ * This will allow access to the following:
  *
- * - The part-up
- * - Updates, filtered by given options and related:
- *   - Activities
- *   - Contributions
- *   - Images
- *   - Additionally, any update type will also be extended with any type of
- *     addtitional related information such as users and their profile pictures
- *
- * @param {String} partupId - The part-up's id
+ * - Activity contributions
+ *   - Contribution user's public profile + profile picture
+ *   - Contribution ratings
+ *     - Contribution rating user's public profile + profile picture
  */
-Meteor.publishComposite('activities.from_partup', function(partupId) {
-    var self = this;
-
-    return {
-        // Use guarded find to check if current user has access to the part-up
-        find: function() {
-            return Partups.guardedFind(self.userId, {_id: partupId}, {limit:1});
+var activityChildren = [
+    // Find contributions for each activity
+    {
+        find: function(activity) {
+            return Contributions.find({activity_id: activity._id});
         },
         children: [
-            // Find all activities in this partup
+            // Find user's public profile + profile picture for each contribution
             {
-                find: function(partup) {
-                    return Activities.find({partup_id: partup._id});
+                find: function(contribution) {
+                    return Meteor.users.findSinglePublicProfile(contribution.upper_id);
                 },
                 children: [
-                    // Find contributions for each activity
                     {
-                        find: function(activity) {
-                            return Contributions.find({activity_id: activity._id});
+                        find: function(user) {
+                            return Images.find({_id: user.profile.image}, {limit: 1});
+                        }
+                    }
+                ]
+            },
+
+            // Find ratings for each contribution
+            {
+                find: function(contribution) {
+                    return Ratings.find({contribution_id: contribution._id});
+                },
+                children: [
+                    // Find each rating's user's public profile + profile picture
+                    {
+                        find: function(rating) {
+                            return Meteor.users.findSinglePublicProfile(rating.upper_id);
                         },
                         children: [
-                            // Find user's public profile + profile picture for each contribution
                             {
-                                find: function(contribution) {
-                                    return Meteor.users.findSinglePublicProfile(contribution.upper_id);
-                                },
-                                children: [
-                                    {
-                                        find: function(user) {
-                                            return Images.find({_id: user.profile.image}, {limit: 1});
-                                        }
-                                    }
-                                ]
-                            },
-
-                            // Find ratings for each contribution
-                            {
-                                find: function(contribution) {
-                                    return Ratings.find({contribution_id: contribution._id});
-                                },
-                                children: [
-                                    // Find each rating's user's public profile + profile picture
-                                    {
-                                        find: function(rating) {
-                                            return Meteor.users.findSinglePublicProfile(rating.upper_id);
-                                        },
-                                        children: [
-                                            {
-                                                find: function(user) {
-                                                    return Images.find({_id: user.profile.image}, {limit: 1});
-                                                }
-                                            }
-                                        ]
-                                    }
-                                ]
+                                find: function(user) {
+                                    return Images.find({_id: user.profile.image}, {limit: 1});
+                                }
                             }
                         ]
                     }
                 ]
             }
         ]
+    }
+];
+
+/**
+ * Publish all required data for activities in a part-up
+ *
+ * This subscription will first check if the current user is allowed to view the
+ * requested part-up. If so, the client will be allowed access to:
+ *
+ * - Activities, filtered by given options and related:
+ *
+ * @param {String} partupId - The part-up's id
+ */
+Meteor.publishComposite('activities.from_partup', function(partupId) {
+    var partupCursor = Partups.guardedFind(this.userId, {_id: partupId}, {limit:1});
+    if (!partupCursor.count()) return;
+
+    return {
+        // Find all activities in this partup
+        find: function(partup) {
+            return Activities.find({partup_id: partupId});
+        },
+        children: activityChildren
     };
 });
 
