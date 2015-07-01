a03d1b8f49cc270116440c58aef6a96393ef7350
Peter Peerdeman <peter@lifely.nl>
2015-07-01 15:57:50 +0200

    added preliminary join network code

--- app/packages/partup:client-pages/app/network/network.js
+++ app/packages/partup:client-pages/app/network/network.js
@@ -11,5 +11,13 @@ Template.app_network.helpers({
 /* Page events */
 /*************************************************************/
 Template.app_network.events({
-    //
+    'click [data-join]': function(event, template) {
+        Meteor.call('networks.join', template.data.networkId, function(error) {
+            if (error) {
+                Partup.client.notify.error(error.reason);
+            } else {
+                Partup.client.notify.success('joined network');
+            }
+        });
+    }
 });
