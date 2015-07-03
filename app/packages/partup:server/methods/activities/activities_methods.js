57e1852e7d6dcded037646a82fd0ed83ad024ba8
Nick Koster <nick.koster@lifely.nl>
2015-07-03 15:31:35 +0200

    Allow all partners to archive activity

--- app/packages/partup:server/methods/activities/activities_methods.js
+++ app/packages/partup:server/methods/activities/activities_methods.js
@@ -147,8 +147,9 @@ Meteor.methods({
     'activities.archive': function(activityId) {
         var upper = Meteor.user();
         var activity = Activities.findOneOrFail(activityId);
+        var partup = Partups.findOneOrFail({_id: activity.partup_id});
 
-        if (!upper || activity.creator_id !== upper._id) {
+        if (!upper || !partup.hasUpper(upper._id)) {
             throw new Meteor.Error(401, 'Unauthorized.');
         }
 
