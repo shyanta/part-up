387ba5f032b785321c3b3198a512af7371c368bb
Chiel Kunkels <chiel@kunkels.me>
2015-07-06 11:23:12 +0200

    network-requests: render requests

--- app/packages/partup:client-network-settings-requests/NetworkSettingsRequests.js
+++ app/packages/partup:client-network-settings-requests/NetworkSettingsRequests.js
@@ -4,9 +4,25 @@
  * @param {Number} networkId
  */
 Template.NetworkSettingsRequests.onCreated(function() {
-    this.subscribe('network.one', this.data.networkId);
+    this.subscribe('networks.one', this.data.networkId);
+    this.subscribe('networks.one.pending_uppers', this.data.networkId);
 });
 
-Template.NetworkSettingsUppers.helpers({});
+Template.NetworkSettingsRequests.helpers({
+    requests: function() {
+        var requests = [];
+        var network = Networks.findOne({_id: this.networkId});
+        if (!network) return;
+
+        var pending = network.pending_uppers || [];
+        for (var i = 0; i < pending.length; i++) {
+            requests.push({
+                user: Meteor.users.findOne({_id: pending[i]})
+            });
+        }
+
+        return requests;
+    }
+});
 
 Template.NetworkSettingsUppers.events({});
