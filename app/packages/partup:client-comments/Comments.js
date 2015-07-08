73f37d4c31438c5cf791b8c154d2f39d3f5438ca
Chiel Kunkels <chiel@kunkels.me>
2015-07-08 17:59:04 +0200

    comments: change how motivations are handled
    - no longer prefill comment field with "my motivation is" copy
    - prefix motivations in rendering

--- app/packages/partup:client-comments/Comments.js
+++ app/packages/partup:client-comments/Comments.js
@@ -77,11 +77,6 @@ Template.Comments.helpers({
     },
     isMotivation: function() {
         return this.type === 'motivation';
-    },
-    fieldsForComment: function() {
-        if (this.type === 'motivation') {
-            return {content: __('widgetcommentfield-comment-motivation-placeholder')};
-        }
     }
 });
 
