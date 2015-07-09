bce5d02595b19a3d60542a17f9be852391e36e60
Chiel Kunkels <chiel@kunkels.me>
2015-07-09 17:26:43 +0200

    activity-invite: get suggestions to template

--- app/packages/partup:client-pages/modal/invite_to_partup/invite_to_partup.js
+++ app/packages/partup:client-pages/modal/invite_to_partup/invite_to_partup.js
@@ -1,3 +1,34 @@
+Template.modal_invite_to_partup.onCreated(function() {
+    var self = this;
+    self.userIds = new ReactiveVar([]);
+    self.subscription = new ReactiveVar();
+
+    Meteor.call('activities.user_suggestions', this.data.activityId, function(err, userIds) {
+        if (err) {
+            Partup.client.notify.error(err.reason);
+            return;
+        }
+
+        self.userIds.set(userIds);
+        self.subscription.set(self.subscribe('users.by_ids', userIds));
+    });
+});
+
+Template.modal_invite_to_partup.helpers({
+    suggestions: function() {
+        var sub = Template.instance().subscription.get();
+        if (!sub || !sub.ready()) return;
+
+        var suggestions = [];
+        var userIds = Template.instance().userIds.get();
+        for (var i = 0; i < userIds.length; i++) {
+            suggestions.push(Meteor.users.findOne({_id: userIds[i]}));
+        }
+
+        return suggestions;
+    }
+});
+
 /*************************************************************/
 /* Page events */
 /*************************************************************/
