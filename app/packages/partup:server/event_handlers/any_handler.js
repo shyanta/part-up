858ddb3f13d70eb63d5cbeba63a22e90130c0fb7
Bryan te Beek <bryantebeek@gmail.com>
2015-07-10 14:43:57 +0200

    Only log all events when the LOG_EVENTS env variabele is set

--- app/packages/partup:server/event_handlers/any_handler.js
+++ app/packages/partup:server/event_handlers/any_handler.js
@@ -2,6 +2,8 @@
  * Log every event fired by the application including it's arguments
  */
 Event.onAny(function() {
-    Log.debug('Event fired: '.white + this.event.magenta, arguments);
+    if (process.env.LOG_EVENTS) {
+        Log.debug('Event fired: '.white + this.event.magenta, arguments);
+    }
 });
 
