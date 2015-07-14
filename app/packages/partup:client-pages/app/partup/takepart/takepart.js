6d12cfa70b605676deff689bd9962ffadd8d9fa0
Chiel Kunkels <chiel@kunkels.me>
2015-07-14 13:06:32 +0200

    takepart: ensure users are logged in before adding messages

--- app/packages/partup:client-pages/app/partup/takepart/takepart.js
+++ app/packages/partup:client-pages/app/partup/takepart/takepart.js
@@ -1,12 +1,25 @@
 Template.app_partup_takepart.events({
-    'click [data-newmessage]': function() {
+    'click [data-newmessage]': function(event) {
+        event.preventDefault();
         Partup.client.popup.close();
 
-        // Use a defer to execute code after the route has changed
-        Meteor.defer(function() {
-            if (Router.current().route.getName() === 'partup') {
-                Partup.client.popup.open('new-message');
-            }
-        });
+        var href = event.target.getAttribute('href');
+        var userId = Meteor.userId();
+        var proceed = function() {
+            Router.go(href);
+            Meteor.defer(function() {
+                if (Router.current().route.getName() === 'partup') {
+                    Partup.client.popup.open('new-message');
+                }
+            });
+        };
+
+        if (!userId) {
+            Intent.go({route: 'login'}, function(user) {
+                if (user) proceed();
+            });
+        } else {
+            proceed();
+        }
     }
 });
