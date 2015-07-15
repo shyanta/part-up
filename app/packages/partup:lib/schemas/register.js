093c7bd9d5c91e52fc2cfe33013996112d0061b3
Léon Smit <leonsmit@Leons-MacBook-Pro.local>
2015-07-15 17:15:04 +0200

    profileSettings schema

--- app/packages/partup:lib/schemas/register.js
+++ app/packages/partup:lib/schemas/register.js
@@ -101,3 +101,75 @@ Partup.schemas.forms.registerOptional = new SimpleSchema({
         regEx: Partup.services.validators.simpleSchemaUrlWithoutProtocol
     }
 });
+
+/**
+ * Register Form Optional
+ * @name profileSettings
+ * @memberof Partup.schemas.forms
+ */
+Partup.schemas.forms.profileSettings = new SimpleSchema({
+    name: {
+        type: String
+    },
+    description: {
+        type: String,
+        max: 650,
+        optional: true
+    },
+    facebook: {
+        type: String,
+        max: 255,
+        optional: true,
+        regEx: Partup.services.validators.facebookUsername
+    },
+    image: {
+        type: String,
+        max: 255,
+        optional: true
+    },
+    instagram: {
+        type: String,
+        max: 255,
+        optional: true,
+        regEx: Partup.services.validators.instagramUsername
+    },
+    linkedin: {
+        type: String,
+        max: 255,
+        optional: true,
+        regEx: Partup.services.validators.linkedinUsername
+    },
+    location_input: {
+        type: String,
+        max: 255,
+        optional: true
+    },
+    phonenumber: {
+        type: String,
+        max: 255,
+        optional: true
+    },
+    skype: {
+        type: String,
+        max: 255,
+        optional: true
+    },
+    tags_input: {
+        type: String,
+        max: 255,
+        optional: true,
+        regEx: Partup.services.validators.tagsSeparatedByComma
+    },
+    twitter: {
+        type: String,
+        max: 255,
+        optional: true,
+        regEx: Partup.services.validators.twitterUsername
+    },
+    website: {
+        type: String,
+        max: 255,
+        optional: true,
+        regEx: Partup.services.validators.simpleSchemaUrlWithoutProtocol
+    }
+});
