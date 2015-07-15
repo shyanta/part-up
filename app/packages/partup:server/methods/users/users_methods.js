5584430e3094186ba4a8894a1199cab425faf8ae
Léon Smit <leonsmit@Leons-MacBook-Pro.local>
2015-07-15 17:14:22 +0200

    added a method for register user

--- app/packages/partup:server/methods/users/users_methods.js
+++ app/packages/partup:server/methods/users/users_methods.js
@@ -1,12 +1,43 @@
 Meteor.methods({
     /**
+     * Register a user, it's the same ad update, only with a slightly different schema
+     *
+     * @param {mixed[]} fields
+     */
+    'users.register': function(fields) {
+
+        check(fields, Partup.schemas.forms.registerOptional);
+
+        var upper = Meteor.user();
+
+        if (!upper) {
+            throw new Meteor.Error(401, 'Unauthorized.');
+        }
+
+        try {
+            var profileFields = Partup.transformers.profile.fromFormRegisterOptional(fields);
+
+            var newProfile = _.extend(upper.profile, profileFields);
+
+            Meteor.users.update(upper._id, {$set:newProfile});
+            Event.emit('users.updated', upper._id, newProfile);
+
+            return {
+                _id: upper._id
+            }
+        } catch (error) {
+            Log.error(error);
+            throw new Meteor.Error(400, 'Upper [' + upper._id + '] could not be updated.');
+        }
+    },
+    /**
      * Update a user
      *
      * @param {mixed[]} fields
      */
     'users.update': function(fields) {
 
-        check(fields, Partup.schemas.forms.registerOptional);
+        check(fields, Partup.schemas.forms.profileSettings);
 
         var upper = Meteor.user();
 
