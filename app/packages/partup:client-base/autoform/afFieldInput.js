d521a721632bc8e1eeba7575de985911d5d6fd63
Léon Smit <leonsmit@Leons-MacBook-Pro.local>
2015-07-17 14:46:25 +0200

    fixed all the issues

--- app/packages/partup:client-base/autoform/afFieldInput.js
+++ app/packages/partup:client-base/autoform/afFieldInput.js
@@ -28,6 +28,32 @@ Meteor.startup(function() {
         template.visibleInput.attr('placeholder', placeholder);
         template.visibleInput.attr('style', '');
 
+        // magic to keep the input the correct width to prevent flickering
+        // LORDREEKUS
+        var setInputWidth = function(input) {
+            var font = input.css('fontFamily');
+            var fontSize = input.css('fontSize');
+            var text = input.val();
+            var div = $(document.createElement('div'));
+            div.css({
+                fontFamily: font,
+                fontSize: fontSize,
+                visibility: 'hidden',
+                position: 'absolute',
+                pointerEvents: 'none',
+                width: 'auto',
+                height: 'auto'
+            });
+            $('body').append(div);
+            div.text(text);
+            var width = div.width() + parseInt(fontSize);
+            input.attr('style', 'width: ' + width + 'px !important;');
+            input.attr('size', '');
+            lodash.defer(function() {
+                div.remove();
+            });
+        };
+
         template.changePlaceholder = function() {
             lodash.defer(function() {
                 if (template.visibleInput.val() !== '' || template.hiddenInput.val() !== '') {
@@ -35,6 +61,7 @@ Meteor.startup(function() {
                 } else {
                     template.visibleInput.attr('placeholder', placeholder);
                 }
+                setInputWidth(template.visibleInput);
             });
         };
 
