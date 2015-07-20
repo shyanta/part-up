26773dbf02e1cf41cd5035135aa1d2f18072cf3d
Chiel Kunkels <chiel@kunkels.me>
2015-07-20 17:41:21 +0200

    emails: fix jscs styling

--- app/server/emailconfiguration.js
+++ app/server/emailconfiguration.js
@@ -6,33 +6,33 @@ Accounts.emailTemplates.from = 'Part-up <noreply@part-up.com>';
 /**
  * Password Reset Email
  */
-Accounts.emailTemplates.resetPassword.subject = function (user) {
+Accounts.emailTemplates.resetPassword.subject = function(user) {
     var locale = getUserLocale(user);
 
     return TAPi18n.__('emails-reset-password-subject', {}, locale);
 };
-Accounts.emailTemplates.resetPassword.html = function (user, url) {
+Accounts.emailTemplates.resetPassword.html = function(user, url) {
     var locale = getUserLocale(user);
 
     SSR.compileTemplate('resetPasswordEmail', Assets.getText('emails/ResetPassword.' + locale + '.html'));
 
-    return SSR.render('resetPasswordEmail', { user: user, url: url.replace('/#', '') });
+    return SSR.render('resetPasswordEmail', {user: user, url: url.replace('/#', '')});
 };
 
 /**
  * Verify Email
  */
-Accounts.emailTemplates.verifyEmail.subject = function (user) {
+Accounts.emailTemplates.verifyEmail.subject = function(user) {
     var locale = getUserLocale(user);
 
     return TAPi18n.__('emails-verify-account-subject', {}, locale);
 };
-Accounts.emailTemplates.verifyEmail.html = function (user, url) {
+Accounts.emailTemplates.verifyEmail.html = function(user, url) {
     var locale = getUserLocale(user);
 
     SSR.compileTemplate('verifyAccountEmail', Assets.getText('emails/VerifyAccount.' + locale + '.html'));
 
-    return SSR.render('verifyAccountEmail', { user: user, url: url.replace('/#', '') });
+    return SSR.render('verifyAccountEmail', {user: user, url: url.replace('/#', '')});
 };
 
 /**
@@ -41,9 +41,9 @@ Accounts.emailTemplates.verifyEmail.html = function (user, url) {
 var getUserLocale = function(user) {
     var locale = mout.object.get(user, 'profile.settings.locale') || 'nl';
 
-    if (! mout.object.has(TAPi18n.getLanguages(), locale)) {
+    if (!mout.object.has(TAPi18n.getLanguages(), locale)) {
         locale = 'nl';
     }
 
     return locale;
-}
+};
