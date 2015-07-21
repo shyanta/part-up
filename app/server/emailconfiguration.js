036b64653dab88c985a7df245abf458adfd9a246
Chiel Kunkels <chiel@kunkels.me>
2015-07-21 14:35:28 +0200

    emails: use getLocale helper defined for user

--- app/server/emailconfiguration.js
+++ app/server/emailconfiguration.js
@@ -7,12 +7,12 @@ Accounts.emailTemplates.from = 'Part-up <noreply@part-up.com>';
  * Password Reset Email
  */
 Accounts.emailTemplates.resetPassword.subject = function(user) {
-    var locale = getUserLocale(user);
+    var locale = User(user).getLocale();
 
     return TAPi18n.__('emails-reset-password-subject', {}, locale);
 };
 Accounts.emailTemplates.resetPassword.html = function(user, url) {
-    var locale = getUserLocale(user);
+    var locale = User(user).getLocale();
 
     SSR.compileTemplate('resetPasswordEmail', Assets.getText('emails/ResetPassword.' + locale + '.html'));
 
@@ -23,12 +23,12 @@ Accounts.emailTemplates.resetPassword.html = function(user, url) {
  * Verify Email
  */
 Accounts.emailTemplates.verifyEmail.subject = function(user) {
-    var locale = getUserLocale(user);
+    var locale = User(user).getLocale();
 
     return TAPi18n.__('emails-verify-account-subject', {}, locale);
 };
 Accounts.emailTemplates.verifyEmail.html = function(user, url) {
-    var locale = getUserLocale(user);
+    var locale = User(user).getLocale();
 
     SSR.compileTemplate('verifyAccountEmail', Assets.getText('emails/VerifyAccount.' + locale + '.html'));
 
@@ -37,17 +37,4 @@ Accounts.emailTemplates.verifyEmail.html = function(user, url) {
         url: url.replace('/#', ''),
         count: Meteor.users.find().count()
     });
-};
-
-/**
- * Helpers
- */
-var getUserLocale = function(user) {
-    var locale = mout.object.get(user, 'profile.settings.locale') || 'nl';
-
-    if (!mout.object.has(TAPi18n.getLanguages(), locale)) {
-        locale = 'nl';
-    }
-
-    return locale;
 };
