97cf6f792cacdf7ac7d96b5a8cf0b2fd61287bea
Bryan te Beek <bryantebeek@gmail.com>
2015-07-23 14:03:50 +0200

    KIJK HEM FRONTENDEN DAN, TROLOL

--- app/packages/partup:client-usercard/templates/UserCardContent.js
+++ app/packages/partup:client-usercard/templates/UserCardContent.js
@@ -1,5 +1,17 @@
 Template.UserCardContent.helpers({
     data: function() {
-        return Meteor.users.findOne({_id: this._id});
+        var user = Meteor.users.findOne({_id: this._id});
+        var score = user.participation_score;
+
+        // For design purposes, we only want to display
+        // a max value of 99 and a min value of 10,
+        // every number should be a natural one
+        score = Math.min(99, score);
+        score = Math.max(10, score);
+        score = Math.round(score);
+
+        user.participation_score = score;
+
+        return user;
     }
 });
