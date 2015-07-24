258da6c618843dd8eb06b9bde9c79d8218e54a2a
Jesse de Vries <jessedvrs@gmail.com>
2015-07-24 11:25:47 +0200

    Beta homepage content & video (conditional to user)

--- app/packages/partup:client-pages/app/home/home.js
+++ app/packages/partup:client-pages/app/home/home.js
@@ -2,11 +2,26 @@
 /* Page helpers */
 /*************************************************************/
 Template.app_home.helpers({
-    firstPartup: function() {
-        return Partups.findOne();
-    },
     shrinkHeader: function() {
         return Partup.client.scroll.pos.get() > 40;
+    },
+    videoWatched: function() {
+        return Session.get('home.videowatched');
+    },
+    firstName: function() {
+        return User(Meteor.user()).getFirstname();
+    },
+    greeting: function() {
+        var daypart;
+        var hour = moment().hours();
+
+        if (hour < 6) daypart = 'night';
+        else if (hour < 12) daypart = 'morning';
+        else if (hour < 18) daypart = 'afternoon';
+        else if (hour < 24) daypart = 'evening';
+        else daypart = 'fallback';
+
+        return __('pages-app-home-loggedin-greeting-' + daypart);
     }
 });
 
@@ -14,8 +29,15 @@ Template.app_home.helpers({
 /* Page events */
 /*************************************************************/
 Template.app_home.events({
-    'click [data-translate]': function clickTranslate (event, template) {
-        var language = $(event.currentTarget).data('translate');
-        Partup.client.language.change(language);
+    'click [data-open-video-modal]': function clickTranslate (event, template) {
+        event.preventDefault();
+
+        // Open the video popup
+        Partup.client.popup.open('video', function() {
+
+            // We explicitely want to use localstorage here
+            Session.set('home.videowatched', true);
+
+        });
     }
 });
