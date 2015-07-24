df58ff08af29e0108ec41d3d1314d5a1205c5342
Jesse de Vries <jessedvrs@gmail.com>
2015-07-24 11:51:42 +0200

    Simplified Discover tribe selector

--- app/packages/partup:client-network-selector/NetworkSelector.js
+++ app/packages/partup:client-network-selector/NetworkSelector.js
@@ -1,73 +1,17 @@
-Template.NetworkSelector.onCreated(function() {
-    var tpl = this;
-
-    // When the value changes, notify the parent using the onSelect callback
-    tpl.currentNetwork = new ReactiveVar(false, function(a, network) {
-        if (!network) return;
-
-        if (tpl.data.onSelect) tpl.data.onSelect(network);
-    });
-
-    // Suggested networks
-    tpl.suggestedNetworks = new ReactiveVar();
-    tpl.autorun(function() {
-        var networks = Networks.find({}, {limit: 10}).fetch();
-        tpl.suggestedNetworks.set(networks);
-    });
-});
-
-Template.NetworkSelector.onRendered(function() {
-    var tpl = this;
-
-    // Reset the form when the networkselector closes
-    var form = tpl.find('form');
-    tpl.autorun(function() {
-        var activeVar = Template.currentData().isActive;
-        if (!activeVar) return;
-
-        if (!activeVar.get()) {
-            form.reset();
-        }
-    });
-
-});
-
 Template.NetworkSelector.helpers({
-    currentNetwork: function() {
-        return Template.instance().currentNetwork.get();
-    },
-    suggestedNetworks: function() {
-        return Template.instance().suggestedNetworks.get();
-    },
-    onAutocompleteQuery: function() {
-        return function(query, sync, async) {
-            Meteor.call('networks.autocomplete', query, function(error, networks) {
-                lodash.each(networks, function(n) {
-                    n.value = n.name; // what to show in the autocomplete list
-                });
-                async(networks);
-            });
-        };
-    },
-    onAutocompleteSelect: function() {
-        var tpl = Template.instance();
-
-        return function(network) {
-            tpl.currentNetwork.set(network);
-        };
+    networks: function() {
+        return Networks.find();
     }
 });
 
 Template.NetworkSelector.events({
-    'click [data-select-suggested-network]': function(event, template) {
-        var networks = template.suggestedNetworks.get();
+    'click [data-select-network]': function(event, template) {
+        var networks = Networks.find().fetch();
         if (!networks || !networks.length) return;
 
-        var networkId = event.currentTarget.getAttribute('data-select-suggested-network');
+        var networkId = event.currentTarget.getAttribute('data-select-network');
         var network = lodash.find(networks, {_id: networkId});
-        template.currentNetwork.set(network);
-    },
-    'submit form': function(event) {
-        event.preventDefault();
+
+        if (template.data.onSelect) template.data.onSelect(network);
     }
 });
