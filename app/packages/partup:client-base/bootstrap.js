16ea963a6b86a7a50facce0492e4f86daddf7399
Léon Smit <leonsmit@Leons-MacBook-Pro.local>
2015-07-24 16:20:28 +0200

    ie 10 and below fallbacks for pointer-events none for overlaying elements

--- app/packages/partup:client-base/bootstrap.js
+++ app/packages/partup:client-base/bootstrap.js
@@ -5,7 +5,19 @@
  * @name client-base
  */
 Meteor.startup(function() {
-
+    var version = -1;
+    if (navigator.appName == 'Microsoft Internet Explorer') {
+        var ua = navigator.userAgent;
+        var re  = new RegExp('MSIE ([0-9]{1,}[\.0-9]{0,})');
+        if (re.exec(ua) != null) {
+            version = parseFloat(RegExp.$1);
+        }
+    }
+    // var oldIE = /msie 8|msie 9|msie 10/i.test(navigator.userAgent);
+    if (version < 11 && version > -1) {
+        console.log('IE version: ' + version);
+        $('body').addClass('pu-no-pointer-events');
+    }
     /*************************************************************/
     /* Language configuration */
     /*************************************************************/
