d23c5e745d2cb6c9414393f61c03482d28fc8d72
Bryan te Beek <bryantebeek@gmail.com>
2015-07-27 15:06:32 +0200

    Refactor activity publication

--- app/packages/partup:server/publications/activities.js
+++ app/packages/partup:server/publications/activities.js
@@ -1,68 +1,29 @@
 /**
- * Publish all required data for activities in a part-up
+ * Publish all activities in a partup
  *
- * This subscription will first check if the current user is allowed to view the
- * requested part-up. If so, the client will be allowed access to:
- *
- * - Activities
- *   - Activity contributions
- *     - Contribution user's public profile + profile picture
- *     - Contribution ratings
- *       - Contribution rating user's public profile + profile picture
- *
- * @param {String} partupId - The part-up's id
+ * @param {String} partupId
  */
 Meteor.publishComposite('activities.from_partup', function(partupId) {
-    var partup = Partups.guardedFind(this.userId, {_id: partupId}, {limit:1}).fetch().pop();
-
     return {
         find: function() {
+            var partup = Partups.guardedFind(this.userId, {_id: partupId}, {limit:1}).fetch().pop();
+
+            if (!partup) return;
+
             return Activities.findForPartup(partup._id);
         },
         children: [
-            {
-                find: function(activity) {
-                    return Updates.find({_id: activity.update_id}, {limit: 1});
-                }
-            },
-            {
-                find: function(activity) {
-                    return Contributions.find({activity_id: activity._id});
-                },
-                children: [
-                    {
-                        find: function(contribution) {
-                            return Meteor.users.findSinglePublicProfile(contribution.upper_id);
-                        },
-                        children: [
-                            {
-                                find: function(user) {
-                                    return Images.find({_id: user.profile.image}, {limit: 1});
-                                }
-                            }
-                        ]
-                    },
-                    {
-                        find: function(contribution) {
-                            return Ratings.find({contribution_id: contribution._id});
-                        },
-                        children: [
-                            {
-                                find: function(rating) {
-                                    return Meteor.users.findSinglePublicProfile(rating.upper_id);
-                                },
-                                children: [
-                                    {
-                                        find: function(user) {
-                                            return Images.find({_id: user.profile.image}, {limit: 1});
-                                        }
-                                    }
-                                ]
-                            }
-                        ]
-                    }
-                ]
-            }
+            {find: Updates.findForActivity},
+            {find: Contributions.findForActivity, children: [
+                {find: Meteor.users.findForContribution, children: [
+                    {find: Images.findForUser}
+                ]},
+                {find: Ratings.findForContribution, children: [
+                    {find: Meteor.users.findForRating, children: [
+                        {find: Images.findForUser}
+                    ]},
+                ]}
+            ]}
         ]
     };
 });
