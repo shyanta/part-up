71a00eb406ca1c664ce1faba2a2e3563a04ab506
Léon Smit <leonsmit@Leons-MacBook-Pro.local>
2015-07-27 13:51:21 +0200

    tags-input works on IE9+

--- app/packages/partup:client-base/bootstrap.js
+++ app/packages/partup:client-base/bootstrap.js
@@ -5,17 +5,17 @@
  * @name client-base
  */
 Meteor.startup(function() {
-    var version = -1;
+    window.PU_IE_VERSION = -1;
     if (navigator.appName == 'Microsoft Internet Explorer') {
         var ua = navigator.userAgent;
         var re  = new RegExp('MSIE ([0-9]{1,}[\.0-9]{0,})');
         if (re.exec(ua) != null) {
-            version = parseFloat(RegExp.$1);
+            window.PU_IE_VERSION = parseFloat(RegExp.$1);
         }
     }
     // var oldIE = /msie 8|msie 9|msie 10/i.test(navigator.userAgent);
-    if (version < 11 && version > -1) {
-        console.log('IE version: ' + version);
+    if (window.PU_IE_VERSION < 11 && window.PU_IE_VERSION > -1) {
+        console.log('IE version: ' + window.PU_IE_VERSION);
         $('body').addClass('pu-no-pointer-events');
     }
 
