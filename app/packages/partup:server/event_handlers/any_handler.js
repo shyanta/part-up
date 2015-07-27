ce62d6620d24376baccfa878537a9244534bfc10
Bryan te Beek <bryantebeek@gmail.com>
2015-07-27 15:31:48 +0200

    Remove dot

--- app/packages/partup:server/event_handlers/any_handler.js
+++ app/packages/partup:server/event_handlers/any_handler.js
@@ -4,7 +4,7 @@ var d = Debug('event_handlers');
  * Log every event fired by the application including it's arguments
  */
 Event.onAny(function() {
-    d('Event fired.', this.event);
+    d('Event fired', this.event);
 
     if (process.env.LOG_EVENTS) {
         Log.debug('Event fired: '.white + this.event.magenta, arguments);
