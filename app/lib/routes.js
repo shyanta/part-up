17997fcdd58547bab26ee019b3b1c9a5edb7a0da
Léon Smit <leonsmit@Leons-MacBook-Pro.local>
2015-07-29 14:15:11 +0200

    disabled email settings and made profile details the main settings page

--- app/lib/routes.js
+++ app/lib/routes.js
@@ -101,21 +101,6 @@ Router.route('/profile/:_id/settings', {
     yieldRegions: {
         'modal':              {to: 'main'},
         'modal_profile_settings': {to: 'modal'},
-        'modal_profile_settings_account': {to: 'modal_profile_settings'}
-    },
-    data: function() {
-        return {
-            profileId: this.params._id
-        };
-    }
-});
-
-Router.route('/profile/:_id/settings/details', {
-    name: 'profile-settings-details',
-    where: 'client',
-    yieldRegions: {
-        'modal':              {to: 'main'},
-        'modal_profile_settings': {to: 'modal'},
         'modal_profile_settings_details': {to: 'modal_profile_settings'}
     },
     data: function() {
@@ -125,13 +110,13 @@ Router.route('/profile/:_id/settings/details', {
     }
 });
 
-Router.route('/profile/:_id/settings/email', {
-    name: 'profile-settings-email',
+Router.route('/profile/:_id/settings/general', {
+    name: 'profile-settings-account',
     where: 'client',
     yieldRegions: {
         'modal':              {to: 'main'},
         'modal_profile_settings': {to: 'modal'},
-        'modal_profile_settings_email': {to: 'modal_profile_settings'}
+        'modal_profile_settings_account': {to: 'modal_profile_settings'}
     },
     data: function() {
         return {
@@ -139,6 +124,22 @@ Router.route('/profile/:_id/settings/email', {
         };
     }
 });
+
+// DISABLED UNTILL IMPLEMENTATION
+// Router.route('/profile/:_id/settings/email', {
+//     name: 'profile-settings-email',
+//     where: 'client',
+//     yieldRegions: {
+//         'modal':              {to: 'main'},
+//         'modal_profile_settings': {to: 'modal'},
+//         'modal_profile_settings_email': {to: 'modal_profile_settings'}
+//     },
+//     data: function() {
+//         return {
+//             profileId: this.params._id
+//         };
+//     }
+// });
 
 /*************************************************************/
 /* Create Partup */
