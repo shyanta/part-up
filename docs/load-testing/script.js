cb6eb16a9f95a4b69110506b20c7b4aa670c4e8d
Bryan te Beek <bryantebeek@gmail.com>
2015-07-29 14:12:41 +0200

    Update load testing script

--- docs/load-testing/script.js
+++ docs/load-testing/script.js
@@ -1,10 +1,13 @@
 meteorDown.run({
-    concurrency: 10,
-    url: 'http://localhost:3000'
+    concurrency: 1000,
+    url: 'http://pu-staging.lifely.nl'
 });
 
 meteorDown.init(function(Meteor) {
-    Meteor.call('ping', function(error, result) {
+    Meteor.subscribe('users.loggedin');
+
+    Meteor.call('partups.discover', {}, function(error, result) {
+        Meteor.subscribe('partups.by_ids', result);
         Meteor.kill();
     });
 });
