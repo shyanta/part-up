c4a631da65cfbe23aa0e9af83d825345a71df341
Léon Smit <leonsmit@Leons-MacBook-Pro.local>
2015-07-30 16:06:24 +0200

    close notifications on notification click

--- app/packages/partup:client-dropdowns/notifications/notifications.js
+++ app/packages/partup:client-dropdowns/notifications/notifications.js
@@ -25,6 +25,7 @@ Template.DropdownNotifications.onDestroyed(function() {
 Template.DropdownNotifications.events({
     'click [data-toggle-menu]': ClientDropdowns.dropdownClickHandler,
     'click [data-notification]': function(event, template) {
+        template.dropdownOpen.set(false);
         var notificationId = $(event.currentTarget).data('notification');
         Meteor.call('notifications.clicked', notificationId, function(error, response) {
 
