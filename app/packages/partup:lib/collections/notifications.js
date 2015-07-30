46814fc7cc2665eaf2918a6c224442becd1986e2
Bryan te Beek <bryantebeek@gmail.com>
2015-07-30 09:22:38 +0200

    Refactor Notifications.findForUser to new convention

--- app/packages/partup:lib/collections/notifications.js
+++ app/packages/partup:lib/collections/notifications.js
@@ -8,15 +8,17 @@ Notifications = new Mongo.Collection('notifications');
  * Find the notifications for a user
  *
  * @param {User} user
- * @param {Object} parameters
- * @param {Object} parameters.sort
+ * @param {Object} selector
+ * @param {Object} options
  * @return {Mongo.Cursor}
  */
-Notifications.findForUser = function(user, parameters) {
-    var parameters = parameters || {};
-    var options = {};
+Notifications.findForUser = function(user, selector, options) {
+    if (!user) return;
 
-    if (parameters.sort) options.sort = parameters.sort;
+    var selector = selector || {};
+    var options = options || {};
 
-    return Notifications.find({for_upper_id: user._id}, options);
+    selector.for_upper_id = user._id;
+
+    return Notifications.find(selector, options);
 };
