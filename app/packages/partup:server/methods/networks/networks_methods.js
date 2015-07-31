df4ea253614fffb5f362f5e04c8b30c63fd677e9
Nick Koster <nick.koster@lifely.nl>
2015-07-31 15:47:31 +0200

    u-u-u-u-u-u-u-ulllltraa failll-fail-fail-ail-ail-il-l

--- app/packages/partup:server/methods/networks/networks_methods.js
+++ app/packages/partup:server/methods/networks/networks_methods.js
@@ -331,7 +331,7 @@ Meteor.methods({
             throw new Meteor.Error(400, 'user_is_not_a_member_of_network');
         }
 
-        if (!network.isAdmin(user._id)) {
+        if (network.isAdmin(user._id)) {
             throw new Meteor.Error(401, 'unauthorized');
         }
 
