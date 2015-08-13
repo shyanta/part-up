70494687eedbf018d00b52da9300ddfe38ec93fe
Jesse de Vries <jessedvrs@gmail.com>
2015-08-13 17:37:27 +0200

    Integrated popular_partups in home

--- app/packages/partup:client-pages/app/home/home.js
+++ app/packages/partup:client-pages/app/home/home.js
@@ -1,4 +1,39 @@
+var partupsToColumnTiles = function(partups) {
+    return lodash.map(partups, function(partup) {
+        return {
+            partup: partup,
+            HIDE_TAGS: true
+        };
+    });
+};
+
+Template.app_home.onCreated(function() {
+    var tpl = this;
+
+    tpl.popular_partups = {
+        layout: {}
+    };
+
+    // Call first four discover part-ups and add them to the UI
+    Meteor.call('partups.discover', {sort: 'popular', limit: 4}, function(error, partupIds) {
+        if (tpl.view.isDestroyed) return;
+
+        tpl.subscribe('partups.by_ids', partupIds, {
+            onReady: function() {
+                var popular_partups = Partups.find({_id: {$in: partupIds}}).fetch();
+
+                if (popular_partups.length > 0) {
+                    tpl.popular_partups.layout.add(partupsToColumnTiles(popular_partups));
+                }
+            }
+        });
+    });
+});
+
 Template.app_home.helpers({
+    featured_partup: function() {
+        // return Partups.findFeaturedForHome();
+    },
     videoWatched: function() {
         return Session.get('home.videowatched');
     },
@@ -16,6 +51,45 @@ Template.app_home.helpers({
     },
     firstName: function() {
         return User(Meteor.user()).getFirstname();
+    },
+
+    // For the columnLayout for popular partups
+    amountOfColumns: function() {
+        var width = Partup.client.screensize.current.get('width');
+
+        var template = Template.instance();
+        Meteor.defer(function() {
+            template.popular_partups.layout.rerender();
+        });
+
+        if (width >= 992) return 4;
+        if (width >= 600) return 2;
+        return 1;
+    },
+
+    // We use this trick to be able to call a function in a child template.
+    // The child template directly calls 'addToLayoutHook' with a callback.
+    // We save that callback, so we can call it later and the child template can react to it.
+    addToLayoutHook: function() {
+        var tpl = Template.instance();
+
+        return function registerCallback(callback) {
+            tpl.popular_partups.layout.add = callback;
+        };
+    },
+    clearLayoutHook: function() {
+        var tpl = Template.instance();
+
+        return function registerCallback(callback) {
+            tpl.popular_partups.layout.clear = callback;
+        };
+    },
+    rerenderLayoutHook: function() {
+        var tpl = Template.instance();
+
+        return function registerCallback(callback) {
+            tpl.popular_partups.layout.rerender = callback;
+        };
     }
 });
 
