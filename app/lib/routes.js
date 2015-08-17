800294d34ffe01f37a3cfa93dabec6c8bb48d90d
Peter Peerdeman <peter@lifely.nl>
2015-08-17 18:03:50 +0200

    refactored tribe admin to admin panel

--- app/lib/routes.js
+++ app/lib/routes.js
@@ -408,6 +408,16 @@ Router.route('/admin/featured-partups', {
     }
 });
 
+Router.route('/admin/tribes', {
+    name: 'admin-createtribe',
+    where: 'client',
+    yieldRegions: {
+        'modal':                   {to: 'main'},
+        'modal_admin':             {to: 'modal'},
+        'modal_create_tribe':      {to: 'modal_admin'}
+    }
+});
+
 /*************************************************************/
 /* Networks */
 /*************************************************************/
@@ -504,16 +514,6 @@ Router.route('/:slug/settings/requests', {
 });
 
 /*************************************************************/
-/* Create network (super mega ultra admin) */
-/*************************************************************/
-Router.route('/tribes/create', {
-    name: 'create-network',
-    where: 'client',
-    yieldRegions: {
-        'modal':                   {to: 'main'},
-        'modal_create_network':    {to: 'modal'}
-    }
-});
 
 /*************************************************************/
 /* All other routes */
