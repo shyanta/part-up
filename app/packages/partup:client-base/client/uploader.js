0cff3e910e076be8578c7ab8ff2f2d739c8505ba
Bryan te Beek <bryantebeek@gmail.com>
2015-08-25 12:08:20 +0200

    Let the image uploader resize images before uploading (uploading is now done with a Meteor.call using data:uri, base64 encoded)

--- app/packages/partup:client-base/client/uploader.js
+++ app/packages/partup:client-base/client/uploader.js
@@ -8,19 +8,57 @@ Partup.client.uploader = {
      * @param {Function} callback
      */
     uploadImage: function(file, callback) {
-        Images.insert(file, function(error, dbImage) {
-            if (error) return callback(error);
-            Meteor.subscribe('images.one', dbImage._id);
-            Meteor.autorun(function(computation) {
-                var image = Images.findOne({_id: dbImage._id});
-                if (image && image.isUploaded() && image.url()) {
-                    computation.stop();
-                    Tracker.nonreactive(function() {
-                        callback(null, image);
-                    });
+        var img = document.createElement('img');
+        var canvas = document.createElement('canvas');
+
+        var reader = new FileReader();
+        reader.readAsDataURL(file);
+
+        reader.onload = function(e) {
+            img.src = e.target.result;
+
+            var ctx = canvas.getContext('2d');
+            ctx.drawImage(img, 0, 0);
+
+            var MAX_WIDTH = 1500;
+            var MAX_HEIGHT = 1500;
+            var width = img.width;
+            var height = img.height;
+
+            if (width > height) {
+                if (width > MAX_WIDTH) {
+                    height *= MAX_WIDTH / width;
+                    width = MAX_WIDTH;
                 }
+            } else {
+                if (height > MAX_HEIGHT) {
+                    width *= MAX_HEIGHT / height;
+                    height = MAX_HEIGHT;
+                }
+            }
+
+            canvas.width = width;
+            canvas.height = height;
+
+            var ctx = canvas.getContext('2d');
+            ctx.drawImage(img, 0, 0, width, height);
+
+            var dataUrl = canvas.toDataURL('image/jpeg', 0.8);
+
+            Meteor.call('images.insertByDataUrl', dataUrl, function(error, dbImage) {
+                if (error) return callback(error);
+                Meteor.subscribe('images.one', dbImage._id);
+                Meteor.autorun(function(computation) {
+                    var image = Images.findOne({_id: dbImage._id});
+                    if (image && image.isUploaded() && image.url()) {
+                        computation.stop();
+                        Tracker.nonreactive(function() {
+                            callback(null, image);
+                        });
+                    }
+                });
             });
-        });
+        };
     },
 
     /**
--- app/packages/partup:server/methods/images/images_methods.js
+++ app/packages/partup:server/methods/images/images_methods.js
@@ -24,6 +24,39 @@ Meteor.methods({
         return {
             _id: image._id
         };
-    }
+    },
+
+    /**
+     * Insert an image by data url
+     *
+     * @param {String} dataUrl
+     *
+     * @return {String} imageId
+     */
+    'images.insertByDataUrl': function(dataUrl) {
+        check(dataUrl, String);
+
+        this.unblock();
+
+        var matches = dataUrl.match(/data:(.*);(.+),(.*)/);
+
+        if (matches && matches.length === 4) {
+            var data = matches.pop();
+            var encoding = matches.pop();
+            var mime = matches.pop();
+
+            var buffer = new Buffer(data, 'base64');
+
+            var ref = new FS.File();
+            ref.attachData(buffer, {type: mime});
+            ref.name(Random.id() + '.jpg');
+
+            var image = Images.insert(ref);
+
+            return {
+                _id: image._id
+            };
+        }
+    },
 
 });
