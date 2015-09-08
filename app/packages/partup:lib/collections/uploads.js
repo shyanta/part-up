f01083d442d296fae260e166b540524d650ee27a
Nick Koster <nick.koster@lifely.nl>
2015-09-08 17:29:48 +0200

    Simplified uploads collection because I couldn't get it to work otherwise. This works for now

--- app/packages/partup:lib/collections/uploads.js
+++ app/packages/partup:lib/collections/uploads.js
@@ -1,82 +1,31 @@
-var stores = [];
-
-if (Meteor.isServer) {
-    FS.TempStore.Storage = new FS.Store.FileSystem('_tempUploadStore', {
-        internal: true,
-        path: process.env.CLOUD_DIR
-    });
-
-    var Store = null;
-    var args = {
-        'uploads': {}
-    };
-
-    if (process.env.NODE_ENV.match(/staging|acceptance|production/)) {
-        console.log('Creating Upload store with S3');
-        Store = FS.Store.S3;
-
-        mout.object.deepFillIn(args, {
-            'uploads': {
-                //region: process.env.AWS_BUCKET_REGION,
-                //bucket: process.env.AWS_BUCKET_NAME
-            }
-        });
-    } else if (process.env.NODE_ENV.match(/development/)) {
-        console.log('Creating Upload store with Filesystem');
-        Store = FS.Store.FileSystem;
-
-        mout.object.deepFillIn(args, {
-            'uploads': {
-                path: process.env.PWD + '/uploads'
-            }
-        });
-    }
-
-    if (!Store) throw new Error('A store for CFS has not been defined.');
-
-    stores.push(new Store('uploads', args['uploads']));
-} else {
-
-    /**
-     * Dummy stores so the client knows which stores are available (names)
-     */
-    stores.push(new FS.Store.FileSystem('uploads'));
-}
-
+// @TODO Needs to be enhanced, but at least this works
 /**
- * Uploads are entities stored under each object that contains an uploaded file
+ * Uploads collection
  *
  * @namespace Uploads
  * @memberOf Collection
  */
 Uploads = new FS.Collection('uploads', {
-    stores: stores,
-    filter: {
-        maxSize: 1000 * 1000 * 10, // 10 mb
-        allow: {
-            contentTypes: ['text/*']
-        }
-    },
-    onInvalid: function(message) {
-        if (Meteor.isClient) {
-            Partup.client.notify.error(__('uploads-error_' + message));
-        } else {
-            //backend error handling
-        }
-    }
+    stores: [
+        new FS.Store.FileSystem(
+            'uploads', {
+                path: '/tmp/uploads/csv'
+            }
+        )
+    ]
 });
 
 Uploads.allow({
     insert: function() {
-        var user = Meteor.user();
-        return User(user).isAdmin() || User(user).isNetworkAdmin();
+        return true;
+    },
+    update: function() {
+        return true;
     },
     remove: function() {
-        var user = Meteor.user();
-        return User(user).isAdmin() || User(user).isNetworkAdmin();
+        return true;
     },
     download: function() {
-        var user = Meteor.user();
-        return User(user).isAdmin() || User(user).isNetworkAdmin();
+        return true;
     }
 });
