92efa2bc877092f51a67c908cf1a0db831c9af4a
Chiel Kunkels <chiel@kunkels.me>
2015-09-17 13:40:28 +0200

    profile: allow underscores/dashes in facebook url

--- app/packages/partup:lib/services/validators.js
+++ app/packages/partup:lib/services/validators.js
@@ -31,7 +31,7 @@ Partup.services.validators = {
     simpleSchemaUrlWithoutProtocol: /^(?:(?:https?|ftp):\/\/)?(?:\S+(?::\S*)?@)?(?:(?!10(?:\.\d{1,3}){3})(?!127(?:\.\d{1,3}){3})(?!169\.254(?:\.\d{1,3}){2})(?!192\.168(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]+-?)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]+-?)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:\/[^\s]*)?$/i,
 
     // facebookUrl: /^http[s]?:\/\/(www\.)?facebook\.com\/\w+(\?.*)?$/,     //old
-    facebookUrl: /^http[s]?:\/\/(www\.)?facebook\.com\/[a-zA-Z0-9.]+(\?.*)?$/, //new
+    facebookUrl: /^http[s]?:\/\/(www\.)?facebook\.com\/[a-zA-Z0-9._-]+(\?.*)?$/, //new
     instagramUrl: /^http[s]?:\/\/(www\.)?instagram\.com\/[a-zA-Z._]+\/?(\?.*)?$/,
     linkedinUrl: /^http[s]?:\/\/([a-zA-Z]+\.)?linkedin\.com\/(in\/[a-zA-Z0-9]+|pub\/.*|profile\/view)(\?.*)?$/,
     twitterUrl: /^http[s]?:\/\/(www\.)?twitter\.com\/[A-Za-z0-9_]+(\?.*)?$/,
