53871ab81a0b93154f95890690c3fcf2594e2182
Jesse de Vries <jessedvrs@gmail.com>
2015-09-17 11:07:15 +0200

    style(contents): lots of style fixes for homepage-philosophy, about, pricing

--- app/packages/partup:client-pages/app/content_pages/pricing/pricing.js
+++ app/packages/partup:client-pages/app/content_pages/pricing/pricing.js
@@ -1,61 +1,50 @@
-var individualProducts = function(namespace) {
-    return {
-        name: __('pages-app-pricing-' + namespace + '-name'),
-        summary: __('pages-app-pricing-' + namespace + '-summary'),
-        price: __('pages-app-pricing-' + namespace + '-price'),
-        price_info: __('pages-app-pricing-' + namespace + '-price-info'),
-        features: [
-            {
-                name: __('pages-app-pricing-' + namespace + '-feature'),
-                value: __('pages-app-pricing-' + namespace + '-feature-value')
-            }
-        ],
-        type: 'individual'
-    };
+var UpperProduct = function(type) {
+    this.name = __('pages-app-pricing-upper-' + type + '-name');
+    this.summary = __('pages-app-pricing-upper-' + type + '-summary');
+    this.price = __('pages-app-pricing-upper-' + type + '-price');
+    this.price_info = __('pages-app-pricing-upper-' + type + '-price-info');
+    this.features = [
+        {
+            name: __('pages-app-pricing-upper-' + type + '-feature'),
+            value: __('pages-app-pricing-upper-' + type + '-feature-value')
+        }
+    ];
+    this.type = type;
 };
 
-var tribeProducts = function(namespace) {
-    return {
-        name: __('pages-app-pricing-' + namespace + '-name'),
-        summary: __('pages-app-pricing-' + namespace + '-summary'),
-        price: __('pages-app-pricing-' + namespace + '-price'),
-        price_info: __('pages-app-pricing-' + namespace + '-price-info'),
-        features: [
-            {
-                name: __('pages-app-pricing-' + namespace + '-feature1'),
-                value: __('pages-app-pricing-' + namespace + '-feature1-value')
-            }, {
-                name: __('pages-app-pricing-' + namespace + '-feature2'),
-                value: __('pages-app-pricing-' + namespace + '-feature2-value')
-            }
-        ],
-        type: 'network'
-    };
+var TribeProduct = function(type) {
+    this.name = __('pages-app-pricing-tribe-' + type + '-name');
+    this.summary = __('pages-app-pricing-tribe-' + type + '-summary');
+    this.price = __('pages-app-pricing-tribe-' + type + '-price');
+    this.price_info = __('pages-app-pricing-tribe-' + type + '-price-info');
+    this.features = [
+        {
+            name: __('pages-app-pricing-tribe-' + type + '-feature1'),
+            value: __('pages-app-pricing-tribe-' + type + '-feature1-value')
+        }, {
+            name: __('pages-app-pricing-tribe-' + type + '-feature2'),
+            value: __('pages-app-pricing-tribe-' + type + '-feature2-value')
+        }
+    ];
+    this.type = type;
 };
+
 Template.app_pricing.helpers({
     shrinkHeader: function() {
         return Partup.client.scroll.pos.get() > 40;
     },
-    products: function() {
-        return {
-            upper: function() {//individuals-upper
-                return individualProducts('individuals-upper');
-            },
-            premium_upper: function() { //individuals-premiumupper
-                return individualProducts('individuals-premiumupper');
-            },
-            social_impact: function() {
-                return tribeProducts('tribes-socialimpact');
-            },
-            small: function() {
-                return tribeProducts('tribes-small');
-            },
-            medium: function() {
-                return tribeProducts('tribes-medium');
-            },
-            large: function() {
-                return tribeProducts('tribes-large');
-            }
-        }
+    upper_products: function() {
+        return [
+            new UpperProduct('upper'),
+            new UpperProduct('premiumupper')
+        ];
+    },
+    tribe_products: function() {
+        return [
+            new TribeProduct('socialimpact'),
+            new TribeProduct('small'),
+            new TribeProduct('medium'),
+            new TribeProduct('large')
+        ];
     }
 });
