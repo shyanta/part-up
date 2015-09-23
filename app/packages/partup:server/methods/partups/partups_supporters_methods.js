52f956f60a4caef3d39faf1fc626ca09c49db461
Nick Koster <nick.koster@lifely.nl>
2015-09-23 14:48:14 +0200

    refactor(add supporter): make use of helper function instead of retyping

--- app/packages/partup:server/methods/partups/partups_supporters_methods.js
+++ app/packages/partup:server/methods/partups/partups_supporters_methods.js
@@ -13,22 +13,9 @@ Meteor.methods({
         var partup = Partups.findOneOrFail(partupId);
 
         try {
-            var supporters = partup.supporters || [];
-            var isAlreadySupporter = !!(supporters.indexOf(upper._id) > -1);
+            partup.makeSupporter(upper._id);
 
-            if (!isAlreadySupporter && partup.creator_id !== upper._id) {
-                Partups.update(partupId, {$addToSet: {'supporters': upper._id}});
-                Meteor.users.update(upper._id, {$addToSet: {'supporterOf': partupId}});
-
-                // Create the data object that holds the meta data
-                partup.createUpperDataObject(upper._id);
-
-                Event.emit('partups.supporters.inserted', partup, upper);
-
-                return true;
-            }
-
-            return false;
+            return true;
         } catch (error) {
             Log.error(error);
             throw new Meteor.Error(400, 'user_could_not_be_marked_as_a_supporter');
