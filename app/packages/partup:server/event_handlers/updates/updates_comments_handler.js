ca0a3dabcfe44774f47414e81e569e03a36988de
Nick Koster <nick.koster@lifely.nl>
2015-10-01 17:24:33 +0200

    performance(new comment): decrease loading time when adding comment

--- app/packages/partup:server/event_handlers/updates/updates_comments_handler.js
+++ app/packages/partup:server/event_handlers/updates/updates_comments_handler.js
@@ -61,11 +61,10 @@ Event.on('updates.comments.inserted', function(upper, partup, update, comment) {
 
     // Add the comment to new comment list for all users of this partup
     partup.getUsers().forEach(function(upperId) {
-        // Exclude the upper that wrote the comment
-        if (upperId === comment.creator._id) return;
+        // Make sure all users have an upper_data object
         update.createUpperDataObject(upperId);
-        update.addNewCommentForUpper(upperId, comment._id);
     });
+    update.addNewCommentToUpperData(comment);
 
     // We only want to continue if the update currently has
     // no comments which means that it's the first comment
