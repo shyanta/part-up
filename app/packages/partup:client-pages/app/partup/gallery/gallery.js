931a2c0657e35a8bde5c7a3c7a7636b16d0e9fbc
Léon Smit <lee_smit@live.nl>
2015-10-14 14:46:32 +0200

    refactor(update-gallery): cleaned up the code

--- app/packages/partup:client-pages/app/partup/gallery/gallery.js
+++ app/packages/partup:client-pages/app/partup/gallery/gallery.js
@@ -1,21 +1,5 @@
 Template.ImageGallery.onCreated(function() {
     var template = this;
-
-    template.setNewCurrent = function(relativePos) {
-        var pictures = template.data.pictures;
-        var current = template.data.current;
-        var newCurrent = current + relativePos;
-
-        if (relativePos > 0 && newCurrent > pictures.length - 1) {
-            newCurrent = 0;
-        } else if (relativePos < 0 && newCurrent < 0) {
-            newCurrent = pictures.length - 1;
-        } else if (newCurrent < 0 || newCurrent > pictures.length - 1) {
-            newCurrent = 0;
-        }
-
-        template.data.setter(newCurrent);
-    };
 });
 Template.ImageGallery.helpers({
     popupId: function() {
@@ -27,22 +11,11 @@ Template.ImageGallery.events({
     'click [data-open-gallery]': function(event, template) {
         var popupId = $(event.currentTarget).data('open-gallery');
         var imageId = $(event.target).closest('[data-image]').data('image');
-
         Partup.client.popup.open({
             id: popupId,
-            type: 'gallery'
+            type: 'gallery',
+            totalImages: this.images.length
         });
 
-    },
-    'click [data-previous]': function eventsClickNext (event, template) {
-        event.preventDefault();
-        template.setNewCurrent(-1);
-        if (template.focuspoint) template.focuspoint.reset();
-    },
-
-    'click [data-next]': function eventsClickNext (event, template) {
-        event.preventDefault();
-        template.setNewCurrent(1);
-        if (template.focuspoint) template.focuspoint.reset();
     }
 });
