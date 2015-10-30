c4372c52752e436ba5fdcacedfcf36e49d347837
Nick Koster <nick.koster@lifely.nl>
2015-10-30 17:04:01 +0100

    feat(meurs api): added notifications endpoint

---
+++ app/packages/partup:server/routes/meurs/meurs_routes.js
@@ -0,0 +1,30 @@
+Router.route('/meurs/notifications', {where: 'server'}).post(function() {
+    var data = this.request.body;
+    var response = this.response;
+
+    // We are going to respond in JSON format
+    response.setHeader('Content-Type', 'application/json');
+
+    if (!data || data.eventType !== 10 || !data.candidateId) {
+        // Nothing to do here
+        return response.end();
+    }
+
+    // Get Upper
+    var upper = Meteor.users.findOne({_id: data.candidateId});
+    if (!upper) {
+        return response.end();
+    }
+
+    // Get results from Meurs
+    // Authenticate
+    var token = Partup.server.services.meurs.getToken();
+    var results = Partup.server.services.meurs.getResults(token, upper.profile.meurs_id);
+
+    if (results) {
+        Meteor.users.update()
+    }
+
+    // This POST request was automated and they don't expect anything in return
+    return response.end();
+});
