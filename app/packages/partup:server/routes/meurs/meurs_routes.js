9db0b401e6c6ff63754b785dae3ff488defd8848
Nick Koster <nick.koster@lifely.nl>
2015-11-02 17:58:28 +0100

    feat(meurs api): finalized notification endpoint

--- app/packages/partup:server/routes/meurs/meurs_routes.js
+++ app/packages/partup:server/routes/meurs/meurs_routes.js
@@ -1,30 +1,30 @@
-Router.route('/meurs/notifications', {where: 'server'}).post(function() {
+Router.route('/meurs/finished', {where: 'server'}).post(function() {
     var data = this.request.body;
     var response = this.response;
 
     // We are going to respond in JSON format
     response.setHeader('Content-Type', 'application/json');
 
-    if (!data || data.eventType !== 10 || !data.candidateId) {
+    if (!data || data.eventType !== 10 || !data.candidateId || !data.programSessionId) {
         // Nothing to do here
-        return response.end();
+        return response.end('Not enough data');
     }
 
     // Get Upper
-    var upper = Meteor.users.findOne({_id: data.candidateId});
-    if (!upper) {
-        return response.end();
-    }
+    var upper = Meteor.users.findOne({'profile.meurs_id': data.candidateId});
+    if (!upper) return response.end('No Upper found with given candidateId');
 
     // Get results from Meurs
     // Authenticate
     var token = Partup.server.services.meurs.getToken();
-    var results = Partup.server.services.meurs.getResults(token, upper.profile.meurs_id);
+    var results = Partup.server.services.meurs.getResults(token, upper.profile.meurs_id, data.programSessionId);
+
+    //return response.end(results); // Test
 
     if (results) {
         Meteor.users.update({_id: upper._id}, {$set: {'profile.results': results}});
     }
 
     // This POST request was automated and they don't expect anything in return
-    return response.end();
+    return response.end('Done.');
 });
