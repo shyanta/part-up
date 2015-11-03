03d34b8b5f2e95b02b274ac9ed1b565f5037b47d
Nick Koster <nick.koster@lifely.nl>
2015-11-03 17:22:31 +0100

    feat(meurs api): create test flow update

--- app/packages/partup:server/methods/services/meurs_methods.js
+++ app/packages/partup:server/methods/services/meurs_methods.js
@@ -10,38 +10,42 @@ Meteor.methods({
 
         // Authenticate
         var token = Partup.server.services.meurs.getToken();
-        //console.log(token);
 
         // Create user if needed
-        if (!upper.profile.meurs_id) {
+        if (!upper.profile.meurs._id && !upper.profile.meurs.portal) {
+            // Get portal
+            //var portal = Partup.server.services.meurs.getPortal(upper.profile.settings.locale);
+            // Add user
             var q4youId = Partup.server.services.meurs.addUser(token, upper._id, User(upper).getEmail());
-            //console.log(q4youId);
-            Meteor.users.update(upper._id, {$set: {'profile.meurs_id': q4youId}});
+            // Activate user
+            var isUserActivated = Partup.server.services.meurs.activateUser(token, q4youId);
+            if (!isUserActivated) return false;
+            // Update user
+            Meteor.users.update(upper._id, {$set: {'profile.meurs._id': q4youId}});
         }
-
-        // Activate user
-        var isUserActivated = Partup.server.services.meurs.activateUser(token, q4youId);
-        if (!isUserActivated) return false;
 
         // Get Program Templates
         //var programTemplates = Partup.server.services.meurs.getProgramTemplates(token);
         //console.log(programTemplates);
 
-        // Create Program Session
-        var programSessionId = Partup.server.services.meurs.createProgramSessionId(token, q4youId);
-        //console.log(programSessionId);
-
-        // Activate Program Session
-        var isProgramSessionActivated = Partup.server.services.meurs.setActiveProgramSession(token, q4youId, programSessionId);
-        if (!isProgramSessionActivated) return false;
+        // Create Program Session if needed
+        if (!upper.profile.meurs.program_session_id) {
+            // Create session
+            var programSessionId = Partup.server.services.meurs.createProgramSessionId(token, q4youId);
+            // Activate Program Session
+            var isProgramSessionActivated = Partup.server.services.meurs.setActiveProgramSession(token, q4youId, programSessionId);
+            if (!isProgramSessionActivated) return false;
+            // Update user
+            Meteor.users.update(upper._id, {$set: {'profile.meurs.program_session_id': programSessionId}});
+        }
 
         // Get Program Session Content
         //var programSessionContent = Partup.server.services.meurs.getProgramSessionContent(token, q4youId, programSessionId);
-        //console.log(programSessionContent);
 
         // Create return URL
         var returnUrl = Meteor.absoluteUrl() + 'profile/' + upper._id + '/about?results_ready=true';
 
+        // Generate browser token and return generated URL to FE
         return Partup.server.services.meurs.getBrowserToken(token, q4youId, returnUrl);
     },
 
