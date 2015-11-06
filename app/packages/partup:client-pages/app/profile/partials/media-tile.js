f67504c52bc3fcb45f667a4c793389b411be1784
Léon Smit <lee_smit@live.nl>
2015-11-06 12:02:18 +0100

    feat(meurs-profile): media placeholder

--- app/packages/partup:client-pages/app/profile/partials/media-tile.js
+++ app/packages/partup:client-pages/app/profile/partials/media-tile.js
@@ -1,5 +1,28 @@
 Template.MediaTile.helpers({
     embedSettings: function() {
         return this.embedSettings();
+    },
+
+    isCurrentusersMediaTile: function() {
+        return this.upper_id === Meteor.userId();
+    }
+});
+
+Template.MediaTile.events({
+    'click [data-delete]': function(event, template) {
+        var tileId = template.data._id;
+        Partup.client.prompt.confirm({
+            title: __('pages-app-profile-about-tile-prompt-title'),
+            message: __('pages-app-profile-about-tile-prompt-message'),
+            onConfirm: function() {
+                Meteor.call('tiles.remove', tileId, function(error, result) {
+                    if (error) {
+                        Partup.client.notify.error(__(error));
+                        return;
+                    }
+                    Partup.client.notify.success('Tile removed');
+                });
+            }
+        });
     }
 });
