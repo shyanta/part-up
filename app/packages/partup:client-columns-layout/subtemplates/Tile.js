4c2fdf5980302b0afe6db80e209cad36f7164dcb
Jesse de Vries <jessedvrs@gmail.com>
2015-12-01 14:04:00 +0100

    refactor(depricated-column-layout): remove

--- app/packages/partup:client-columns-layout/subtemplates/Tile.js
+++
@@ -1,9 +0,0 @@
-/**
- * Run onRendered callback when the template is rendered
- */
-Template.Columnslayout_Tile.onRendered(function() {
-    var template = this;
-    if (mout.lang.isFunction(this.data.onRendered)) {
-        this.data.onRendered();
-    }
-});
