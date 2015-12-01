18e2c0fe33b2dd9ca18b4d6c72df3c51e903e9ef
Jesse de Vries <jessedvrs@gmail.com>
2015-12-01 11:44:51 +0100

    feat(profile): create http routes for count calls

--- app/packages/partup:server/package.js
+++ app/packages/partup:server/package.js
@@ -151,6 +151,7 @@ Package.onUse(function(api) {
         'routes/images/images_routes.js',
         'routes/partups/partups_routes.js',
         'routes/networks/networks_routes.js',
+        'routes/users/users_routes.js',
         'methods/updates/updates_comments_methods.js',
         'methods/updates/updates_messages_methods.js',
         'methods/activities/activities_methods.js',
