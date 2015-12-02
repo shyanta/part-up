c9226282a6b23817d2a1dc9d94df6b24f6a9e4c2
Léon Smit <lee_smit@live.nl>
2015-12-02 16:28:06 +0100

    feat(message): implemented edit message and emotions

--- app/packages/partup:client-pages/app/partup/updates/newmessage/newmessage.js
+++ app/packages/partup:client-pages/app/partup/updates/newmessage/newmessage.js
@@ -8,20 +8,36 @@ Template.app_partup_updates_newmessage.onCreated(function() {
     var template = this;
 
     template.uploadingPhotos = new ReactiveVar(false);
-    template.uploadedPhotos = new ReactiveVar([]);
+    var images = this.data._id ? this.data.type_data.images : [];
+    template.uploadedPhotos = new ReactiveVar(images);
     template.totalPhotos = new ReactiveVar(0);
     template.maxPhotos = 4;
     template.submitting = new ReactiveVar(false);
+    template.partupId = this.data.partup_id || this.data.partupId
 });
 
-Template.app_partup_updates_newmessage.onRendered(function() {
-    this.input = this.find('[name=text]');
-    this.mentionsInput = Partup.client.forms.MentionsInput(this.input, this.data.partupId);
+Template.afFieldInput.onRendered(function() {
+    // makes sure it only goes on if it's a mentions input
+    if (!this.data.hasOwnProperty('data-message-input')) return;
+
+    // comment template
+    var template = this.parent();
+    var currentValue = template.data._id ? template.data.type_data.new_value : undefined;
+
+    // destroy other edit mentionsinputs to prevent conflicts
+    if (template.mentionsInput) template.mentionsInput.destroy();
+
+    var input = template.find('[data-message-input]');
+    template.mentionsInput = Partup.client.forms.MentionsInput(input, template.partupId, {
+        autoFocus: true,
+        autoAjustHeight: true,
+        prefillValue: currentValue
+    });
 });
 
 Template.app_partup_updates_newmessage.onDestroyed(function() {
     var tpl = this;
-    tpl.mentionsInput.destroy();
+    if (tpl.mentionsInput) tpl.mentionsInput.destroy();
 });
 
 // helpers
@@ -39,6 +55,25 @@ Template.app_partup_updates_newmessage.helpers({
     },
     submitting: function() {
         return Template.instance().submitting.get();
+    },
+    state: function() {
+        var self = this;
+        var template = Template.instance();
+        return {
+            edit: function() {
+                return self._id ? true : false;
+            },
+            formId: function() {
+                return self._id ? 'editMessageForm' : 'newMessageForm';
+            }
+        };
+    },
+    formDoc: function() {
+        if (!this._id) return;
+        return {
+            text: this.type_data.new_value,
+            images: this.type_data.images || []
+        };
     }
 });
 
@@ -83,6 +118,29 @@ Template.app_partup_updates_newmessage.events({
         var total = Template.instance().totalPhotos.get();
         total--;
         Template.instance().totalPhotos.set(total);
+    },
+    'click [data-submit]': function(event, template) {
+        // var input = template.find('[data-message-input]');
+        // var valueWithoutWhitespace = input.value.split(' ').join('');
+        // if (!valueWithoutWhitespace.length) {
+        //     var updateId = this._id || false;
+        //     Partup.client.prompt.confirm({
+        //         onConfirm: function() {
+        //             Partup.client.popup.close();
+        //             Meteor.call('updates.messages.remove', updateId, function(error, result) {
+        //                 if (error) {
+        //                     Partup.client.notify.error(error.reason);
+        //                     self.done(new Error(error.message));
+        //                     return;
+        //                 }
+        //                 template.submitting.set(false);
+        //                 AutoForm.resetForm('editMessageForm');
+        //                 template.mentionsInput.reset();
+        //                 template.uploadedPhotos.set([]);
+        //             });
+        //         }
+        //     });
+        // }
     }
 });
 
@@ -90,6 +148,43 @@ Template.app_partup_updates_newmessage.events({
 /* Widget form hooks */
 /*************************************************************/
 AutoForm.hooks({
+    editMessageForm: {
+        onSubmit: function(insertDoc, updateDoc, currentDoc) {
+            var self = this;
+            var parent = Template.instance().parent();
+            var template = self.template.parent();
+
+            parent.submitting.set(true);
+
+            var updateId = parent.data._id;
+
+            var uploadedPhotos = parent.uploadedPhotos.get();
+            insertDoc.images = uploadedPhotos;
+            insertDoc.text = parent.mentionsInput.getValue();
+
+            // close popup before call is made, an error notifier
+            // will be the feedback when it fails
+            Partup.client.popup.close();
+
+            Meteor.call('updates.messages.update', updateId, insertDoc, function(error, result) {
+                parent.submitting.set(false);
+
+                // Error
+                if (error) {
+                    Partup.client.notify.error(error.reason);
+                    self.done(new Error(error.message));
+                    return;
+                }
+
+                AutoForm.resetForm('editMessageForm');
+                template.mentionsInput.reset();
+                self.done();
+                parent.uploadedPhotos.set([]);
+            });
+
+            return false;
+        }
+    },
     newMessageForm: {
         onSubmit: function(insertDoc, updateDoc, currentDoc) {
             var self = this;
