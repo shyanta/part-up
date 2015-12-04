8dd81123384720e4d808be430ac4d71c65d2f66c
Nick Koster <nick.koster@lifely.nl>
2015-12-04 17:06:26 +0100

    feat(notifications): moved grouping logic to collection hooks

---
+++ app/packages/partup:server/event_handlers/notifications/notifications_handler.js
@@ -0,0 +1,93 @@
+/**
+ * Check if notifications need to be grouped after inserted
+ */
+Event.on('partups.notifications.inserted', function(upperId, notification) {
+    // Define the notification groups
+    var updateGroup = [
+        'partups_activities_inserted',
+        'partups_messages_inserted',
+        'partups_contributions_inserted',
+        'partups_supporters_added'
+    ];
+    var conversationGroup = [
+        'partups_new_comment_in_involved_conversation'
+    ];
+
+    // Check if notification belongs to a group
+    var notificationGroupType = null;
+    var group = [];
+    if (updateGroup.indexOf(notification.type) > -1) {
+        notificationGroupType = 'partups_multiple_updates_since_visit';
+        group = updateGroup;
+    } else if (conversationGroup.indexOf(notification.type) > -1) {
+        notificationGroupType = 'multiple_comments_in_conversation_since_visit';
+        group = conversationGroup;
+    }
+
+    if (notificationGroupType) {
+        // Set latest upper data
+        var latestUpper = {};
+        if (notification.type_data.supporter) latestUpper = notification.type_data.supporter;
+        if (notification.type_data.creator) latestUpper = notification.type_data.creator;
+        if (notification.type_data.commenter) latestUpper = notification.type_data.commenter;
+
+        // Get unread notifications
+        var query = {
+            for_upper_id: notification.for_upper_id,
+            type: {$in: group},
+            new: true
+        };
+        if (group === conversationGroup) query['type_data.update._id'] = notification.type_data.update._id;
+        if (group === updateGroup) query['type_data.partup._id'] = notification.type_data.partup._id;
+        var unreadNotifications = Notifications.find(query, {fields: {_id: 1, type_data: 1}}).fetch();
+        var notificationIds = [];
+        var upperIds = [];
+        unreadNotifications.forEach(function(notification) {
+            if (notification.type_data.supporter) upperIds.push(notification.type_data.supporter._id);
+            if (notification.type_data.creator) upperIds.push(notification.type_data.creator._id);
+            if (notification.type_data.commenter) upperIds.push(notification.type_data.commenter._id);
+            notificationIds.push(notification._id);
+        });
+
+        // Now check if there is already a grouped notification for this user
+        query = {
+            for_upper_id: notification.for_upper_id,
+            type: notificationGroupType,
+            new: true
+        };
+        if (group === conversationGroup) query['type_data.update._id'] = notification.type_data.update._id;
+        if (group === updateGroup) query['type_data.partup._id'] = notification.type_data.partup._id;
+        var groupNotification = Notifications.findOne(query);
+        if (groupNotification) {
+            // There is, so we only need to update that notification, and set grouped property to created notification
+            Notifications.update(notification._id, {$set: {grouped: true}});
+            Notifications.update(groupNotification._id, {
+                $set: {
+                    'type_data.latest_upper': latestUpper,
+                    'type_data.others_count': lodash.unique(upperIds).length - 1 // All uppers minus latest one
+                }
+            });
+        } else {
+            // There is no group notification, so check if we need to create one
+            console.log(unreadNotifications.length);
+            if (unreadNotifications.length < 2) return;
+
+            // We need to create a group notification at this point, so flag the single notifications
+            Notifications.update({_id: {$in: notificationIds}}, {$set: {grouped: true}});
+
+            // Set the data
+            var newNotification = {
+                for_upper_id: notification.for_upper_id,
+                type: notificationGroupType,
+                type_data: notification.type_data,
+                created_at: new Date(),
+                new: true,
+                clicked: false
+            };
+            newNotification.type_data.latest_upper = latestUpper;
+            newNotification.type_data.others_count = lodash.unique(upperIds).length - 1;
+
+            Notifications.insert(newNotification);
+        }
+    }
+});
--- app/packages/partup:server/services/notifications_service.js
+++ app/packages/partup:server/services/notifications_service.js
@@ -21,10 +21,16 @@ Partup.server.services.notifications = {
         if (!options.type) throw new Meteor.Error('Required argument [options.type] is missing for method [Partup.server.services.notifications::send]');
         if (!options.typeData) throw new Meteor.Error('Required argument [options.typeData] is missing for method [Partup.server.services.notifications::send]');
 
-        options = Partup.server.services.notifications.group(options);
-        if (!options) {
-            // The grouped notification is updated, nothing more to do here
-            return;
+        // If conversation notification, check if there is already an unread notification from the same user on this topic
+        if (options.type === 'partups_new_comment_in_involved_conversation') {
+            var isAlreadyNotified = Notifications.findOne({
+                for_upper_id: options.userId,
+                new: true,
+                'type_data.update._id': options.typeData.update._id,
+                'type_data.commenter._id': options.typeData.commenter._id
+            });
+            // Nothing else to do here
+            //if (isAlreadyNotified) return;
         }
 
         notification.for_upper_id = options.userId;
@@ -37,102 +43,5 @@ Partup.server.services.notifications = {
         d('Notification created for user [' + notification.for_upper_id + '] with type [' + notification.type + ']');
 
         Notifications.insert(notification);
-    },
-
-    /**
-     * Group notifications of the same type
-     *
-     * @param  {Object} options
-     *
-     * @return {Object} options
-     */
-    group: function(options) {
-        // Define the notification groups
-        var updateGroup = [
-            'partups_activities_inserted',
-            'partups_messages_inserted',
-            'partups_contributions_inserted',
-            'partups_supporters_added'
-        ];
-        var conversationGroup = [
-            'partups_new_comment_in_involved_conversation'
-        ];
-
-        // Check if notification to be sent belongs to a group
-        var notificationGroupType = null;
-        var group = [];
-        if (updateGroup.indexOf(options.type) > -1) {
-            notificationGroupType = 'partups_multiple_updates_since_visit';
-            group = updateGroup;
-        } else if (conversationGroup.indexOf(options.type) > -1) {
-            // Check if there is already an unread notification from the same user on this topic
-            var isAlreadyNotified = Notifications.findOne({
-                for_upper_id: options.userId,
-                new: true,
-                'type_data.update._id': options.typeData.update._id,
-                'type_data.commenter._id': options.typeData.commenter._id
-            });
-            // Nothing else to do here
-            if (isAlreadyNotified) return false;
-
-            notificationGroupType = 'multiple_comments_in_conversation_since_visit';
-            group = conversationGroup;
-        }
-
-        if (notificationGroupType) {
-            // Set latest upper data
-            var latestUpper = {};
-            if (options.typeData.supporter) latestUpper = options.typeData.supporter;
-            if (options.typeData.creator) latestUpper = options.typeData.creator;
-            if (options.typeData.commenter) latestUpper = options.typeData.commenter;
-
-            // Now check if there is already a grouped notification for this user
-            var groupNotification = Notifications.findOne({
-                for_upper_id: options.userId,
-                type: notificationGroupType,
-                new: true
-            });
-
-            var countOthers = function() {
-                // Something
-            };
-
-            if (groupNotification) {
-                // There is, so we only need to update that notification
-                Notifications.update(groupNotification._id, {
-                    $set: {
-                        'type_data.latest_upper': latestUpper,
-                        'type_data.others_count': countOthers()
-                    }
-                });
-
-                // Our work here is done, return false to escape
-                return false;
-            } else {
-                // There is no group notification, so check if we need to create one
-                var query = {
-                    for_upper_id: options.userId,
-                    type: {$in: group},
-                    new: true
-                };
-                if (group === conversationGroup) query['type_data.update._id'] = options.typeData.update._id;
-                var unreadNotifications = Notifications.find(query, {fields: {_id: 1}}).fetch();
-                if (unreadNotifications.length > 1) {
-                    // We need to create a group notification at this point, so alter the provided options
-                    options.type = notificationGroupType;
-                    options.typeData.latest_upper = latestUpper;
-                    options.typeData.others_count = countOthers();
-
-                    // And delete the single notifications
-                    var notificationIds = [];
-                    unreadNotifications.forEach(function(notification) {
-                        notificationIds.push(notification._id);
-                    });
-                    Notifications.remove({_id: {$in: notificationIds}});
-                }
-
-                return options;
-            }
-        }
     }
 };
