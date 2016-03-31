42dcb231b5e5f873825cac046a0b554d85e4dd6e
Wouter Danes <wouter.danes@gmail.com>
2016-03-31 18:01:44 +0200

    fix(bootstrap): all images are available again.

--- app/packages/partup:server/fixtures/images.js
+++ app/packages/partup:server/fixtures/images.js
@@ -13,47 +13,47 @@ Meteor.startup(function() {
 
         if (Images.find().count() === 27) {
             // Networks - Lifely (open)
-            downloadAndSaveImage('raaNx9aqA6okiqaS4', 'https://i.vimeocdn.com/portrait/7543891_300x300.jpg');
-            downloadAndSaveImage('SEswZsYiTTKTTdnN5', 'http://lifely.nl/bundles/lifelywebsite/img/icons/icon152.png');
+            downloadAndSaveImage('raaNx9aqA6okiqaS4', 'https://s3-eu-west-1.amazonaws.com/partup-dev-images/7543891_300x300.jpg');
+            downloadAndSaveImage('SEswZsYiTTKTTdnN5', 'https://s3-eu-west-1.amazonaws.com/partup-dev-images/icon152.png');
 
             // Networks - ING (public)
-            downloadAndSaveImage('T8pfWebTJmvbBNJ2g', 'http://www.integrand.nl/asset/partner/ing.jpeg');
-            downloadAndSaveImage('f7yzkqh9J9JvxCCqN', 'https://www.ingsprinters.nl/v287/img/authors/ing.jpg');
+            downloadAndSaveImage('T8pfWebTJmvbBNJ2g', 'https://s3-eu-west-1.amazonaws.com/partup-dev-images/ing_logo.jpeg');
+            downloadAndSaveImage('f7yzkqh9J9JvxCCqN', 'https://s3-eu-west-1.amazonaws.com/partup-dev-images/ing-no-text.jpg');
 
             // Networks - ING (invite)
-            downloadAndSaveImage('efDuvuTzpqH65P9DF', 'http://www.integrand.nl/asset/partner/ing.jpeg');
-            downloadAndSaveImage('fReGXG4qkNXb4K8wp', 'https://www.ingsprinters.nl/v287/img/authors/ing.jpg');
+            downloadAndSaveImage('efDuvuTzpqH65P9DF', 'https://s3-eu-west-1.amazonaws.com/partup-dev-images/ing_logo.jpeg');
+            downloadAndSaveImage('fReGXG4qkNXb4K8wp', 'https://s3-eu-west-1.amazonaws.com/partup-dev-images/ing-no-text.jpg');
 
             // Networks - ING (closed)
-            downloadAndSaveImage('PnYAg3EX5dKfEnkdn', 'http://www.integrand.nl/asset/partner/ing.jpeg');
-            downloadAndSaveImage('4rymNTA3jFfTRKtFJ', 'https://www.ingsprinters.nl/v287/img/authors/ing.jpg');
+            downloadAndSaveImage('PnYAg3EX5dKfEnkdn', 'https://s3-eu-west-1.amazonaws.com/partup-dev-images/ing_logo.jpeg');
+            downloadAndSaveImage('4rymNTA3jFfTRKtFJ', 'https://s3-eu-west-1.amazonaws.com/partup-dev-images/ing-no-text.jpg');
 
             // Users - Default User
-            downloadAndSaveImage('oQeqgwkdd44JSBSW5', 'http://img.faceyourmanga.com/mangatars/0/2/2797/normal_3810.png');
+            downloadAndSaveImage('oQeqgwkdd44JSBSW5', 'https://s3-eu-west-1.amazonaws.com/partup-dev-images/normal_3810.png');
 
             // Users - John Partup
-            downloadAndSaveImage('cHhjpWKo9DHjXQQjy', 'http://img.faceyourmanga.com/mangatars/0/6/6110/normal_6861.png');
+            downloadAndSaveImage('cHhjpWKo9DHjXQQjy', 'https://s3-eu-west-1.amazonaws.com/partup-dev-images/normal_6861.png');
 
             // Users - Judy Partup
-            downloadAndSaveImage('bMTGT9oSDGzxCL3r4', 'http://img.faceyourmanga.com/mangatars/0/0/37/normal_253.png');
+            downloadAndSaveImage('bMTGT9oSDGzxCL3r4', 'https://s3-eu-west-1.amazonaws.com/partup-dev-images/normal_253.png');
 
             // Users - Admin User
-            downloadAndSaveImage('CxEprGKNWo6HdrTdq', 'http://img.faceyourmanga.com/mangatars/0/0/39/normal_511.png');
+            downloadAndSaveImage('CxEprGKNWo6HdrTdq', 'https://s3-eu-west-1.amazonaws.com/partup-dev-images/normal_511.png');
 
             // Partups - Crowd funding Part-up organiseren
-            downloadAndSaveImage('FTHbg6wbPxjiA4Y8w', 'http://www.quick2finance.nl/wp-content/uploads/2014/04/crowdfunding2701.jpg');
+            downloadAndSaveImage('FTHbg6wbPxjiA4Y8w', 'https://s3-eu-west-1.amazonaws.com/partup-dev-images/Premium.jpg');
 
             // Partups - Super secret closed ING partup
-            downloadAndSaveImage('D3zGxajTjWCLhXokS', 'http://www.bloomberg.com/ss/08/09/0918_best_brands/image/86-ing.jpg');
+            downloadAndSaveImage('D3zGxajTjWCLhXokS', 'https://s3-eu-west-1.amazonaws.com/partup-dev-images/86-ing.jpg');
 
             // Partups - A semisecret ING partup, plus ones are ok
-            downloadAndSaveImage('ComeF2exAjeKBPAf8', 'http://thewshopping.be/2013/wp-content/uploads/2013/08/ing.jpg');
+            downloadAndSaveImage('ComeF2exAjeKBPAf8', 'https://s3-eu-west-1.amazonaws.com/partup-dev-images/ing-wide-logo-wall.jpg');
 
             // Partups - Organise a Meteor Meetup
-            downloadAndSaveImage('J2KxajXMcqiKwrEBu', 'https://camo.githubusercontent.com/c3e0a56600e69a7730b5caf79959459c96c08c89/687474703a2f2f636c2e6c792f59366a6e2f76656761732e706e672532303d32303078313032');
+            downloadAndSaveImage('J2KxajXMcqiKwrEBu', 'https://s3-eu-west-1.amazonaws.com/partup-dev-images/687474703a2f2f636c2e6c792f59366a6e2f76656761732e706e672532303d32303078313032');
 
             // Partups - Partup Premium Part-up
-            downloadAndSaveImage('xfYreAouRFh4mnctk', 'http://datasymphony.com.au/wp-content/uploads/2013/11/Premium.jpg');
+            downloadAndSaveImage('xfYreAouRFh4mnctk', 'https://s3-eu-west-1.amazonaws.com/partup-dev-images/Premium.jpg');
         }
 
     }
