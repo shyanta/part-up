9a4536ced2dbc8219391ae4271436664859c4c59
Theuy Limpanont <theuy.limpanont@gmail.com>
2016-04-12 15:34:35 +0200

    refactor(document-renderer): check which client previewlink to render

--- app/packages/partup:client-pages/app/partup/document-renderer/document-renderer.js
+++ app/packages/partup:client-pages/app/partup/document-renderer/document-renderer.js
@@ -2,6 +2,14 @@ Template.DocumentRenderer.helpers({
     getSvgIcon: Partup.helpers.getSvgIcon,
     bytesToSize: Partup.helpers.bytesToSize,
     previewLink: function(file) {
-        return Partup.helpers.DropboxRenderer().createPreviewLinkFromDirectLink(file.link, file.name);
+        var client = new URL(file.link).host;
+
+        if(client.indexOf('dropbox') > -1) {
+            return Partup.helpers.DropboxRenderer().createPreviewLinkFromDirectLink(file.link, file.name);
+        }
+        else if(client.indexOf('gstatic') > -1) {
+
+        }
+        else { /* perhaps partup 404 no file found */ return ''; }
     }
 });
