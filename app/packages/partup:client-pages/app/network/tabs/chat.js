d4673ad04fd35435bc85ed5e9bbe08f171fe4deb
Léon Smit <lee_smit@live.nl>
2016-05-10 11:54:28 +0200

    feat(chat): reverse scroll behaviour

--- app/packages/partup:client-pages/app/network/tabs/chat.js
+++ app/packages/partup:client-pages/app/network/tabs/chat.js
@@ -1,7 +1,63 @@
-Template.app_network_chat.onCreated(function() {
-
-});
-
 Template.app_network_chat.events({
     'DOMMouseScroll [data-preventscroll], mousewheel [data-preventscroll]': Partup.client.scroll.preventScrollPropagation
 });
+
+Template.app_network_chat.onRendered(function() {
+    Meteor.setTimeout(function() {
+        $('[data-reversed-scroller]')[0].scrollTop = $('[data-reversed-scroller]')[0].scrollHeight;
+        $('[data-reversed-scroller-wrapper]').addClass('pu-state-active');
+    }, 500);
+});
+/*
+'use strict';
+
+import { throttle } from 'lodash';
+
+export default class ReversedScroller {
+    constructor(props) {
+
+        // Setting the 'stick' flag to true/false will respectively
+        // enable/disable automatic-scroll-to-bottom when a
+        // new comment comes in.
+        // This flag will be set to false when the user
+        // starts to scroll and will be set back to true
+        // when the user scrolls close to the bottom.
+        this.stick = true;
+
+        // The actual scrolling container
+        this.scroller;
+
+        // Defer and throttle _updateStickFlag
+        this._updateStickFlag = throttle(this._updateStickFlag, 100);
+    }
+
+    contentPossiblyUpdated(scroller) {
+        this._setScroller(scroller);
+
+        if (this.stick) {
+            this.scroller.scrollTop = this.scroller.scrollHeight;
+        }
+    }
+
+    destroy() {
+        if (this.scroller) {
+            this.scroller.removeEventListener('scroll', this._updateStickFlag.bind(this));
+            window.removeEventListener('resize', this.contentPossiblyUpdated.bind(this, this.scroller));
+        }
+    }
+
+    _setScroller(scroller) {
+        if (!this.scroller) {
+            this.scroller = scroller;
+            this.scroller.addEventListener('scroll', this._updateStickFlag.bind(this));
+            window.addEventListener('resize', this.contentPossiblyUpdated.bind(this, this.scroller));
+        }
+    }
+
+    _updateStickFlag() {
+        const clientRect = this.scroller.getBoundingClientRect();
+        const distanceToBottom = this.scroller.scrollHeight - clientRect.height - this.scroller.scrollTop;
+        this.stick = distanceToBottom === 0;
+    }
+}
+*/
