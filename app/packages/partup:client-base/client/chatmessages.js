cc278e144e7eb3e4df2db2de966af909b9719630
Léon Smit <lee_smit@live.nl>
2016-05-17 16:56:27 +0200

    feat(chat): group messages by day

--- app/packages/partup:client-base/client/chatmessages.js
+++ app/packages/partup:client-base/client/chatmessages.js
@@ -1,4 +1,21 @@
 Partup.client.chatmessages = {
+    groupByCreationDay: function(messages) {
+        var outputArray = [];
+
+        messages.forEach(function(item, index) {
+            var outputLastIndex = outputArray.length - 1;
+            if (outputArray[outputLastIndex] && outputArray[outputLastIndex].day === new Date(item.created_at).setHours(0, 0, 0, 0, 0)) {
+                outputArray[outputLastIndex].messages.push(item);
+            } else {
+                outputArray.push({
+                    day: new Date(item.created_at).setHours(0, 0, 0, 0, 0),
+                    messages: [item]
+                });
+            }
+        });
+
+        return outputArray;
+    },
     groupByCreatorId: function(messages) {
         var outputArray = [];
         messages.forEach(function(item, index) {
