9a5b2ae55c73c538471db267135eb5f569e38cd5
Léon Smit <lee_smit@live.nl>
2016-05-17 16:56:50 +0200

    fix(chatmessage): chat message rendering

--- app/packages/partup:client-network-chat/ChatMessage.js
+++ app/packages/partup:client-network-chat/ChatMessage.js
@@ -1,13 +1,14 @@
 Template.ChatMessage.onCreated(function() {
     var template = this;
+    template.subscribe('users.one', template.data.data.creator_id);
 });
 Template.ChatMessage.helpers({
     data: function() {
         var data = Template.currentData().data;
         var user = Meteor.users.findOne(data.creator_id);
         return {
-            creator_id: function() {
-                return data.creator_id;
+            creator: function() {
+                return user;
             },
             messages: function() {
                 data.messages[0].creator = user;
