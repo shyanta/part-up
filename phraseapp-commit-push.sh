c81199b82bc2dbce2a455a6a8e3ca6813db33668
Theuy Limpanont <theuy.limpanont@gmail.com>
2016-05-24 11:42:05 +0200

    fix(phraseapp-custom-cli): improve commit command for automatic commit and push

--- phraseapp-commit-push.sh
+++ phraseapp-commit-push.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+git checkout .
+git clean -fd
+git reset --hard HEAD
+git pull
 git add app/i18n/phraseapp.de.i18n.json app/i18n/phraseapp.en.i18n.json app/i18n/phraseapp.fr.i18n.json app/i18n/phraseapp.nl.i18n.json
 git commit -am "update(translation): update phraseapp translation"
 git push
