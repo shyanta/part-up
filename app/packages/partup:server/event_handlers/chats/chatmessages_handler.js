caf5ae74f649697f87dc5328d9e75f0b6123927a
Peter Peerdeman <peter@lifely.nl>
2016-06-14 17:25:28 +0200

    fix(linkscraper): temporarily disable linkscraper until feedback is processed

--- app/packages/partup:server/event_handlers/chats/chatmessages_handler.js
+++ app/packages/partup:server/event_handlers/chats/chatmessages_handler.js
@@ -1,11 +1,13 @@
 /**
  * Check for scrapable content
  */
+
+/* disabled scraper until feedback is implemented
 Event.on('chats.messages.inserted', function(userId, chatMessageId, content) {
     if (!userId || !chatMessageId) return;
 
     // Check if an URL is present
-    var regex = new RegExp("(http[s]?:\\/\\/(www\\.)?|(www\\.)?){1}([0-9A-Za-z-\\.@:%_\‌​+~#=]+)+((\\.[a-zA-Z]{2,3})+)(/(.)*)?(\\?(.)*)?");
+    var regex = new RegExp('(http[s]?:\\/\\/(www\\.)?|(www\\.)?){1}([0-9A-Za-z-\\.@:%_\‌​+~#=]+)+((\\.[a-zA-Z]{2,3})+)(/(.)*)?(\\?(.)*)?');
     var url = content.match(regex);
     if (url && url.length > 0) {
         var result = Partup.server.services.scrape.website(url[0]);
@@ -24,3 +26,4 @@ Event.on('chats.messages.inserted', function(userId, chatMessageId, content) {
         ChatMessages.update(chatMessageId, {$set: {preview_data: data}});
     }
 });
+*/
