86cc253e4f7ee1b851c36ef049fb5579aff8315c
Theuy Limpanont <theuy.limpanont@gmail.com>
2016-06-14 21:46:31 +0200

    refactor(windowTitle): refactor windowTitle feature with jshint check

--- app/packages/partup:client-base/client/windowtitle.js
+++ app/packages/partup:client-base/client/windowtitle.js
@@ -1,73 +1,39 @@
-/**
- * Window Title function for global use
- *
- * @class window-title
- * @memberof Partup.client
- */
+var appName = 'Part-up';
+var contextName = '';
+var defaultDelimiter = '' - '';
+var notificationsCount = 0;
+var windowTitle = '';
 
-
-Partup.client.windowTitle = (function() {
-
-  var appName,
-  contextName,
-  defaultDelimiter,
-  isDisplayName,
-  notificationsCount,
-  windowTitle = "";
-
-  init = function () {
-    appName = "Part-up";
-    contextName = ""
-    isDisplayName = true;
-    defaultDelimiter = " - ";
-    notificationsCount = 0;
-    windowTitle = "";
-
-    composeWindowTitle();
-  },
-  setAppName = function(isDisplayIn, nameIn) {
-    	names[numberIn] = nameIn;
-
-      composeWindowTitle();
-  },
-  setNotificationsCount = function(notificationsCountIn) {
-    notificationsCount = notificationsCountIn;
-
-    composeWindowTitle();
-  	return;
-  },
-  setContextName = function(contextNameIn) {
-  	contextName = contextNameIn;
-
-    composeWindowTitle();
-  	return;
-  },
-  composeWindowTitle = function() {
+var composeWindowTitle = function () {
     if (notificationsCount > 0) {
         windowTitle = '(' + notificationsCount + ') ' + appName;
     } else {
-        windowTitle = appName + "";
+        windowTitle = appName + '';
     }
 
-    if (contextName !== "") {
-
-      if (windowTitle == "") {
-        windowTitle = contextName;
-      } else {
-        windowTitle = windowTitle + defaultDelimiter + contextName;
-      }
-
+    if (contextName !== '') {
+        if (windowTitle === '') {
+            windowTitle = contextName;
+        } else {
+            windowTitle = windowTitle + defaultDelimiter + contextName;
+        }
     }
-
     document.title = windowTitle;
-    return;
-  };
-  return {
-  	init: init,
-    setAppName: setAppName,
+};
+
+var setNotificationsCount = function (notificationsCountIn) {
+    notificationsCount = notificationsCountIn;
+    composeWindowTitle();
+};
+
+var setContextName = function (contextNameIn) {
+    contextName = contextNameIn;
+    composeWindowTitle();
+};
+
+composeWindowTitle();
+
+Partup.client.windowTitle = {
     setNotificationsCount: setNotificationsCount,
     setContextName: setContextName
-  };
-}());
-
-Partup.client.windowTitle.init();
+};
