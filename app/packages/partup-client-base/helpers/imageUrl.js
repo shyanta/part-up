c020b09b45f100e8fb658923c514d020387dfdd8
Léon Smit <lee_smit@live.nl>
2016-07-11 13:29:25 +0200

    fix(partup): tribe partup start styled like discussed with pim

--- app/packages/partup-client-base/helpers/imageUrl.js
+++ app/packages/partup-client-base/helpers/imageUrl.js
@@ -1,4 +1,5 @@
 Template.registerHelper('partupImageUrl', function(options) {
+
     if (!options || !options.hash || !options.hash.id) return '';
 
     var image = Images.findOne({_id: options.hash.id});
@@ -6,6 +7,13 @@ Template.registerHelper('partupImageUrl', function(options) {
 
     var store = options.hash.store || '360x360';
     return Partup.helpers.url.getImageUrl(image, store);
+});
+Template.registerHelper('partupImageById', function(id) {
+
+    var image = Images.findOne({_id: id});
+    if (!image) return;
+
+    return Partup.helpers.url.getImageUrl(image, '360x360');
 });
 
 Template.registerHelper('partupImageObjectUrl', function(options) {
