4a18cc881cf295ce2d9dd5c391f0bc791bde60f6
Theuy Limpanont <info@theuy.nl>
2016-07-24 14:35:23 +0200

    test(discover): rewrite steps

--- app/tests/cucumber/features/step_definitions/discover_steps.js
+++ app/tests/cucumber/features/step_definitions/discover_steps.js
@@ -1,31 +1,33 @@
-// (function() {
-//     'use strict';
-//
-//     var assert = require('assert');
-//
-//     module.exports = function() {
-//
-//         /*
-//         this.Then(/^I should see some partup tiles$/, function(expectedUsername, callback) {
-//             this.browser.
-//             waitForExist('.pu-partuptile').
-//             getText('.pu-partuptile').should.eventually.contain('Benefiet');
-//         });
-//
-//         this.When(/^I enter a discover keyword$/, function(expectedUsername, callback) {
-//             this.browser.
-//             waitForExist('[name="search_query"]').
-//             setValue('[name="search_query"]', 'Benefiet').
-//             submit('#discoverQuery').
-//             waitForExist('.pu-partuptile').
-//             getText('.pu-partuptile').
-//             then(function(tiles) {
-//                 assert(tiles.length, 1);
-//                 callback();
-//             });
-//         });
-//         */
-//
-//     };
-//
-// })();
+module.exports = function () {
+
+  this.Given(/^I navigate to discover$/, function() {
+    browser.url(process.env.ROOT_URL + '/discover');
+  });
+
+  this.Then(/^I should see some partup tiles$/, function () {
+    browser.waitForExist('.pu-partuptile');
+  });
+
+  this.When(/^I enter a discover keyword$/, function () {
+    browser.waitForExist('[name="textSearch"]');
+    browser.setValue('[name="textSearch"]', 'Organise');
+    browser.submitForm('#discoverQueryForm');
+  });
+
+  this.When(/^I select a tribe$/, function () {
+    browser.element('[data-open-networkbox]').click();
+    browser.element('#networkSelector').waitForVisible();
+    browser.element('[data-select-network="ibn27M3ePaXhmKzWq"]').click();
+  });
+
+  this.When(/^I select a location$/, function () {
+    browser.element('[data-open-locationbox]').click();
+    browser.element('#locationSelector').waitForVisible();
+    browser.element('[data-select-suggested-location="ChIJNy3TOUNvxkcR6UqvGUz8yNY"]').click();
+  });
+
+  this.Then(/^I should see filtered partups$/, function () {
+    expect(browser.element('.pu-partuptile header h2 a').getText()).toBe('Organise a Meteor Meetup');
+  });
+
+};
