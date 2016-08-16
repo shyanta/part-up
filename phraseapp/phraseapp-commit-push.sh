f38099769ba980bc9570a0c3ba6f76a58bb0f322
Theuy Limpanont <info@theuy.nl>
2016-08-16 21:13:28 +0200

    chore(phraseapp-commit-push): change commit message

--- phraseapp/phraseapp-commit-push.sh
+++ phraseapp/phraseapp-commit-push.sh
@@ -4,5 +4,5 @@
 # this script will run by the prompt application in phraseapp.js
 
 git add ../app/i18n/phraseapp.de.i18n.json ../app/i18n/phraseapp.en.i18n.json ../app/i18n/phraseapp.fr.i18n.json ../app/i18n/phraseapp.nl.i18n.json
-git commit -am "update(translation): update phraseapp translation"
+git commit -am "chore(translation): update phraseapp translation"
 git push
