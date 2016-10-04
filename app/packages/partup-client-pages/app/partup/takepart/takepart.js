d1489dd6b8a4d6bebcf0587db7d28c738181f774
Léon Smit <lee_smit@live.nl>
2016-10-04 15:00:13 +0200

    feat(partner-popup): partner popup for new partner flow

--- app/packages/partup-client-pages/app/partup/takepart/takepart.js
+++ app/packages/partup-client-pages/app/partup/takepart/takepart.js
@@ -1,33 +1,19 @@
 Template.app_partup_takepart.helpers({
     partup: function() {
         return Partups.findOne({_id: this.partupId});
+    },
+    form: function() {
+        return {
+            schema: Partup.schemas.entities.updateComment
+        };
     }
 });
 
-Template.app_partup_takepart.events({
-    'click [data-newmessage]': function(event) {
-        event.preventDefault();
-        Partup.client.popup.close();
-
-        var href = event.currentTarget.getAttribute('href');
-        var userId = Meteor.userId();
-        var proceed = function() {
-            Router.go(href);
-            Meteor.defer(function() {
-                if (Router.current().route.getName() === 'partup') {
-                    Partup.client.popup.open({
-                        id: 'new-message'
-                    });
-                }
-            });
-        };
-
-        if (!userId) {
-            Intent.go({route: 'login'}, function(user) {
-                if (user) proceed();
-            });
-        } else {
-            proceed();
+AutoForm.hooks({
+    becomePartnerForm: {
+        onSubmit: function(insertDoc, updateDoc, currentDoc) {
+            console.log('dhjsfhdfkhdasfjkh');
         }
     }
 });
+
