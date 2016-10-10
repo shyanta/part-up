8b0cb9a7425660bf5807d922455d5190c5289c02
Léon Smit <lee_smit@live.nl>
2016-10-10 17:14:43 +0200

    fix(becomepartner): pending approval button

--- app/packages/partup-client-pages/app/partup/partup-sidebar.js
+++ app/packages/partup-client-pages/app/partup/partup-sidebar.js
@@ -72,6 +72,14 @@ Template.app_partup_sidebar.helpers({
         return partup.hasUpper(user._id);
     },
 
+    isPendingPartner: function() {
+        var user = Meteor.user();
+        if (!user) return false;
+        var partup = Partups.findOne(this.partupId);
+        if (!partup) return false;
+        return (partup.pending_partners || []).indexOf(user._id) > -1;
+    },
+
     partupUppers: function() {
         var partup = Partups.findOne(this.partupId);
         if (!partup) return;
