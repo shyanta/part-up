3b6b340b9822876e30958760520515311a643b78
Léon Smit <lee_smit@live.nl>
2017-01-25 17:48:38 +0100

    feat(home): header

--- app/packages/partup-client-pages/app/home/blocks/Home_Header.js
+++ app/packages/partup-client-pages/app/home/blocks/Home_Header.js
@@ -1,3 +1,15 @@
+// Meteor.startup(function() {
+//     Session.set('partup-fullScreenHome', true);
+// });
+
+Template.Home_Header.onCreated(function() {
+    $('body').addClass('pu-prevent-scrolling');
+});
+
+Template.Home_Header.onDestroyed(function() {
+    $('body').removeClass('pu-prevent-scrolling');
+});
+
 Template.Home_Header.helpers({
     greeting: function() {
         var daypart;
@@ -13,5 +25,19 @@ Template.Home_Header.helpers({
     },
     firstName: function() {
         return User(Meteor.user()).getFirstname();
+    }
+});
+
+Template.Home_Header.events({
+    'click [data-what-is-partup]': function(event, template) {
+        event.preventDefault();
+
+        // Session.set('partup-fullScreenHome', false);
+
+        $(event.currentTarget).addClass('pu-home-header__button--is-clicked');
+
+        $('body').removeClass('pu-prevent-scrolling');
+
+        $('[data-header]').addClass('pu-home-header--is-collapsed');
     }
 });
