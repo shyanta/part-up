eba0d5ec959b0265f9c3c1942acc521e583abc53
Nick Koster <nick.koster@lifely.nl>
2017-02-02 13:45:31 +0100

    feat(chat): temporarily disabled url scraper for personal chats #818

--- app/packages/partup-server/event_handlers/chats/chatmessages_handler.js
+++ app/packages/partup-server/event_handlers/chats/chatmessages_handler.js
@@ -71,50 +71,50 @@ Event.on('chats.messages.inserted', function(upper, chatMessage, network) {
                 }
             });
         }
-    }
 
-    // Check if an URL is present
-    var url = chatMessage.content.match(/[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/);
+        // Check if an URL is present
+        var url = chatMessage.content.match(/[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/);
 
-    if (url && url.length > 0) {
-        var matchedUrl = url[0];
-        var data = Partup.server.services.scrape.website(matchedUrl);
+        if (url && url.length > 0) {
+            var matchedUrl = url[0];
+            var data = Partup.server.services.scrape.website(matchedUrl);
 
-        // Stop if there is no scraped data
-        if (!data) return;
+            // Stop if there is no scraped data
+            if (!data) return;
 
-        // Scrape again with seo snippets available for partup routes
-        if (data.host == 'part-up.com') {
-            data = Partup.server.services.scrape.website(matchedUrl + '?_escaped_fragment_');
-            // If still not available (like on a /chat route), scrape the part-up root page
-            if (!data || !data.title) return;
+            // Scrape again with seo snippets available for partup routes
+            if (data.host == 'part-up.com') {
+                data = Partup.server.services.scrape.website(matchedUrl + '?_escaped_fragment_');
+                // If still not available (like on a /chat route), scrape the part-up root page
+                if (!data || !data.title) return;
 
-            if (!data.description) {
-                data = Partup.server.services.scrape.website('https://part-up.com/?_escaped_fragment_');
+                if (!data.description) {
+                    data = Partup.server.services.scrape.website('https://part-up.com/?_escaped_fragment_');
+                }
+                // Change the URL to link to back to the original URL
+                data.url = matchedUrl;
             }
-            // Change the URL to link to back to the original URL
-            data.url = matchedUrl;
+
+            var preview_data = {};
+
+            var isImage = function(url) {
+                return (url.match(/\.(jpeg|jpg|gif|png)$/) != null);
+            };
+
+            if (data.title) {
+                preview_data.url = data.url;
+                preview_data.title = data.ogTitle ? data.ogTitle : data.title;
+                preview_data.description = data.ogDescription ? data.ogDescription : data.description ? data.description : undefined;
+                preview_data.image = data.image ? data.image : data.images[0];
+                preview_data.domain = data.host ? data.host : null;
+            } else if (isImage(matchedUrl)) {
+                preview_data.url = data.url;
+                preview_data.type = 'image';
+            } else {
+                return;
+            }
+
+            ChatMessages.update(chatMessage._id, {$set: {preview_data: preview_data}});
         }
-
-        var preview_data = {};
-
-        var isImage = function(url) {
-            return (url.match(/\.(jpeg|jpg|gif|png)$/) != null);
-        };
-
-        if (data.title) {
-            preview_data.url = data.url;
-            preview_data.title = data.ogTitle ? data.ogTitle : data.title;
-            preview_data.description = data.ogDescription ? data.ogDescription : data.description ? data.description : undefined;
-            preview_data.image = data.image ? data.image : data.images[0];
-            preview_data.domain = data.host ? data.host : null;
-        } else if (isImage(matchedUrl)) {
-            preview_data.url = data.url;
-            preview_data.type = 'image';
-        } else {
-            return;
-        }
-
-        ChatMessages.update(chatMessage._id, {$set: {preview_data: preview_data}});
     }
 });
