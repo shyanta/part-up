97b696bede2f87d9ae2ef3a84748cc742192980a
Léon Smit <lee_smit@live.nl>
2017-02-03 14:32:37 +0100

    fix(board): added translation files

--- app/packages/partup-client-partupsettings/Partupsettings.js
+++ app/packages/partup-client-partupsettings/Partupsettings.js
@@ -668,23 +668,3 @@ var ImageSystem = function(template) {
         }
     });
 };
-
-Template.BoardSwitch.onRendered(function() {
-    var template = this;
-
-    if (template.data.enabled) {
-        template.$('[data-board-input]').val(true);
-        template.$('[data-enable] input').attr('checked', 'checked');
-    } else {
-        template.$('[data-disable] input').attr('checked', 'checked');
-    }
-});
-
-Template.BoardSwitch.events({
-    'click [data-enable]': function(event, template) {
-        template.$('[data-board-input]').val(true);
-    },
-    'click [data-disable]': function(event, template) {
-        template.$('[data-board-input]').val(false);
-    }
-});
