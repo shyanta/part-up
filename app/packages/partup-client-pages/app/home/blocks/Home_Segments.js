09fb677341a63fc380951e5a27ad9012aa8b4992
Léon Smit <lee_smit@live.nl>
2017-02-06 10:17:36 +0100

    feat(home): added appstore buttons, also the “start” button now has an URL translation key

--- app/packages/partup-client-pages/app/home/blocks/Home_Segments.js
+++ app/packages/partup-client-pages/app/home/blocks/Home_Segments.js
@@ -1,7 +1,4 @@
 Template.Home_Segments.events({
-    'click [data-segment-start-button]': function(event, template) {
-
-    },
     'click [data-segment-search-button]': function(event, template) {
         event.preventDefault();
         Router.go('discover');
