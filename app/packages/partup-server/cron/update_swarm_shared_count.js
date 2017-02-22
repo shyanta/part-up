4ed66ee2144a430363eff88dc5f4170f2e1c1b52
Wouter Danes <wouter.danes@gmail.com>
2017-02-22 22:01:21 +0100

    fix(cron): Removed Twitter share counts because api is no longer available

--- app/packages/partup-server/cron/update_swarm_shared_count.js
+++ app/packages/partup-server/cron/update_swarm_shared_count.js
@@ -1,4 +1,4 @@
-if(process.env.PARTUP_CRON_ENABLED) {
+if (process.env.PARTUP_CRON_ENABLED) {
     SyncedCron.add({
         name: 'Update the shared counts of a swarm from different socials',
         schedule: function(parser) {
@@ -6,17 +6,15 @@ if(process.env.PARTUP_CRON_ENABLED) {
         },
         job: function() {
             if (!process.env.NODE_ENV.match(/development/)) {
-                Swarms.find({}, {sort: {refreshed_at: 1}, limit: 10}).forEach(function(swarm) {
+                Swarms.find({}, { sort: { refreshed_at: 1 }, limit: 10 }).forEach(function(swarm) {
                     var counts = {
                         facebook: Partup.server.services.shared_count.facebook(Meteor.absoluteUrl() + swarm.slug),
-                        twitter: Partup.server.services.shared_count.twitter(Meteor.absoluteUrl() + swarm.slug),
                         linkedin: Partup.server.services.shared_count.linkedin(Meteor.absoluteUrl() + swarm.slug)
                     };
 
                     Swarms.update(swarm._id, {
                         $set: {
                             'shared_count.facebook': counts.facebook,
-                            'shared_count.twitter': counts.twitter,
                             'shared_count.linkedin': counts.linkedin,
                             refreshed_at: new Date()
                         }
--- app/packages/partup-server/services/shared_count_service.js
+++ app/packages/partup-server/services/shared_count_service.js
@@ -20,20 +20,6 @@ Partup.server.services.shared_count = {
         return data.shares ? data.shares : 0;
     },
 
-    twitter: function(url) {
-        var response = HTTP.get('https://cdn.api.twitter.com/1/urls/count.json?url=' + url);
-
-        if (response.statusCode !== 200) {
-            Log.error('Twitter shared count API resulted in status code [' + response.statusCode + ']', response);
-            return false;
-        }
-
-        var data = mout.object.get(response, 'data');
-        if (!data) return false;
-
-        return data.count;
-    },
-
     linkedin: function(url) {
         var response = HTTP.get('https://www.linkedin.com/countserv/count/share?format=json&url=' + url);
 
