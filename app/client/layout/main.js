aee9fc3794207271d7c81ff979bbb2f65e4d317b
Wouter Danes <wouter.danes@gmail.com>
2017-03-15 00:05:48 +0100

    Revert "fix(dropbox): loading github and dropbox stuff in head after render"
    This reverts commit 125657ad6041e7698b8f7fc5cd5fe0d845b2764a.

--- app/client/layout/main.js
+++ app/client/layout/main.js
@@ -2,10 +2,6 @@
 /* main rendered */
 /*************************************************************/
 Template.main.onRendered(function() {
-    $('head').append('<link rel="stylesheet" href="/css/highlight.js-github.min.css">');
-    $('head').append('<script src="https://cdnjs.cloudflare.com/ajax/libs/js-cookie/2.1.3/js.cookie.min.js"></script>');
-    $('head').append('<script src="https://cdnjs.cloudflare.com/ajax/libs/dropbox.js/2.5.0/Dropbox-sdk.min.js"></script>');
-    $('head').append('<script src="https://www.dropbox.com/static/api/2/dropins.js" id="dropboxjs" data-app-key="le3ovpnhqs4qy9g"></script>');
     try {
         var mainContainer = this.find('.pu-main');
         if (!mainContainer) throw 'Could not find ".pu-main" element to initialize Bender with.';
