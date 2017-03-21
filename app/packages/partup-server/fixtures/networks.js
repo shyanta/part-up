d994376297ffa2517de0dd30211b217960ee4708
Noel Heesen <noelheesen@gmail.com>
2017-03-21 10:31:42 +0100

    fix(fixture): Update fixtures

--- app/packages/partup-server/fixtures/networks.js
+++ app/packages/partup-server/fixtures/networks.js
@@ -2,19 +2,21 @@ Meteor.startup(function() {
     if (process.env.NODE_ENV.match(/development|staging/)) {
         if (!Networks.find().count()) {
 
+            /* ING */
             /* 1 */
             Networks.insert({
                 '_id' : 'nqBnE8nSLasaapXXS',
+                'sector_id' : 'FhapMhLSOHcCCbPfO',
                 'name' : 'ING (public)',
-                'privacy_type' : 1,
                 'slug' : 'ing-public',
+                'description': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus veniam illo inventore excepturi architecto ut, numquam enim est assumenda ex doloremque quos ratione. Repellendus blanditiis, tempora fugit velit est deleniti.',
                 'image': 'T8pfWebTJmvbBNJ2g',
                 'icon': 'f7yzkqh9J9JvxCCqN',
-                'description': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus veniam illo inventore excepturi architecto ut, numquam enim est assumenda ex doloremque quos ratione. Repellendus blanditiis, tempora fugit velit est deleniti.',
                 'uppers' : [
                     'q63Kii9wwJX3Q6rHS'
                 ],
                 'admins' : ['q63Kii9wwJX3Q6rHS'],
+                'privacy_type' : 1,
                 'created_at' : new Date('2015-07-21T15:47:33.225Z'),
                 'updated_at' : new Date('2015-07-21T15:47:33.225Z'),
                 'language': 'en',
@@ -25,9 +27,10 @@ Meteor.startup(function() {
             /* 2 */
             Networks.insert({
                 '_id' : 'kRCjWDBkKru3KfsjW',
+                'sector_id' : 'FhapMhLSOHcCCbPfO',
                 'name' : 'ING (invite)',
-                'privacy_type' : 2,
                 'slug' : 'ing-invite',
+                'privacy_type' : 2,
                 'image': 'efDuvuTzpqH65P9DF',
                 'icon': 'fReGXG4qkNXb4K8wp',
                 'description': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Modi architecto consequatur unde dolorem fuga laboriosam non alias blanditiis odit vero!',
@@ -52,9 +55,10 @@ Meteor.startup(function() {
             /* 3 */
             Networks.insert({
                 '_id' : 'wfCv4ZdPe5WNT4xfg',
+                'sector_id' : 'FhapMhLSOHcCCbPfO',
                 'name' : 'ING (closed)',
-                'privacy_type' : 3,
                 'slug' : 'ing-closed',
+                'privacy_type' : 3,
                 'image': 'PnYAg3EX5dKfEnkdn',
                 'icon': '4rymNTA3jFfTRKtFJ',
                 'description': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nemo nesciunt tempora accusamus temporibus ipsam modi.',
@@ -84,15 +88,18 @@ Meteor.startup(function() {
                 'chat_id': '9nTogbMy6Ddjfh6NP'
             });
 
+            /* Lifely */
             /* 4 */
             Networks.insert({
                 '_id' : 'ibn27M3ePaXhmKzWq',
+                'sector_id' : 'xPqmVC0HXZSonoTXB',
                 'name' : 'Lifely (open)',
-                'privacy_type' : 1,
                 'slug' : 'lifely-open',
+                'description': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur, delectus.',
+                'language': 'nl',
+                'privacy_type' : 1,
                 'image': 'raaNx9aqA6okiqaS4',
                 'icon': 'SEswZsYiTTKTTdnN5',
-                'description': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur, delectus.',
                 'uppers' : [
                     'q63Kii9wwJX3Q6rHS',
                     'K5c5M4Pbdg3B82wQH',
@@ -101,10 +108,44 @@ Meteor.startup(function() {
                 'admins' : ['q63Kii9wwJX3Q6rHS'],
                 'created_at' : new Date('2015-07-21T15:52:04.548Z'),
                 'updated_at' : new Date('2015-07-21T15:52:04.548Z'),
-                'language': 'nl',
                 'stats': {},
                 'chat_id': 'wioZDD9bTkT2eDF4c'
             });
+
+            // Networks.insert({
+            //     '_id' : '...',
+            //     'name' : 'OpenSource development (open)',
+            //     'slug' : 'opensource-developement',
+            //     'sector_id' : '...',
+            //     'privacy_type' : 1,
+            //     'image': 'raaNx9aqA6okiqaS4',
+            //     'icon': 'SEswZsYiTTKTTdnN5',
+            //     'description': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur, delectus.',
+            //     'tags' : [
+            //         'software',
+            //         'opensource',
+            //         'developement'
+            //     ],
+            //     'uppers' : [
+            //         'q63Kii9wwJX3Q6rHS',
+            //         'K5c5M4Pbdg3B82wQH',
+            //         'a7qcp5RHnh5rfaeW9'
+            //     ],
+            //     'upper_count' : 3,
+            //     'admins' : ['q63Kii9wwJX3Q6rHS'],
+            //     'created_at' : new Date('2015-07-21T15:52:04.548Z'),
+            //     'updated_at' : new Date('2015-07-21T15:52:04.548Z'),
+            //     'language': 'nl',
+            //     'stats': {},
+            //     'location': {
+            //         'lat': 52.0704978, 
+            //         'country': 'Netherlands', 
+            //         'lng': 4.300699899999999,
+            //         'place_id': 'ChIJcb2YQi-3xUcREGwejVreAAQ', 
+            //         'city': 'The Hague'
+            //     },
+            //     'chat_id': 'wioZDD9bTkT2eDF4c'
+            // });
         }
     }
 });
